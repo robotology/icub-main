c3e7722906db43cd6ea142045cec13a2a9f3d5df
Lorenzo Natale <lorenzo.natale@iit.it>
2009-08-05 15:20:53 +0000

    new repository
    svn path=/trunk/iCub/; revision=208

---
+++ ICUBOptions.cmake
@@ -0,0 +1,8 @@
+
+# Create Qt dependent modules?
+SET(CREATE_GUIS_QT FALSE CACHE BOOL "Do you want to compile the Qt graphical user interfaces?")
+SET(CREATE_GUIS_GTKMM FALSE CACHE BOOL "Do you want to compile the GtkMM graphical user interfaces?")
+SET(CREATE_GUIS_GTK FALSE CACHE BOOL "Do you want to compile the Gtk graphical user interfaces?")
+
+
+
