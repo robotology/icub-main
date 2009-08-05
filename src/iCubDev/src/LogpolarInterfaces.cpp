45d4cfe90f2f143138b9a2ffc58c0c9709ced36e
Lorenzo Natale <lorenzo.natale@iit.it>
2009-08-05 13:36:10 +0000

    new repository
    svn path=/trunk/iCub/; revision=92

---
+++ src/iCubDev/src/LogpolarInterfaces.cpp
@@ -0,0 +1,12 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Author: Giorgio Metta
+ * Copyright (C) 2009 RobotCub Consortium
+ * CopyPolicy: Released under the terms of the GNU GPL v2.0.
+ *
+ */
+
+#include <iCub/LogpolarInterfaces.h>
+
+// put here a proper interface to the logpolar library.
