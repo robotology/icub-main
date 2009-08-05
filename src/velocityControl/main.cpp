db7db46c42fb68b2172597fe87e7d8918ec4cee3
Lorenzo Natale <lorenzo.natale@iit.it>
2009-08-05 14:07:31 +0000

    new repository
    svn path=/trunk/iCub/; revision=180

---
+++ src/velocityControl/main.cpp
@@ -0,0 +1,326 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+/**
+*
+@ingroup icub_module
+\defgroup icub_velocityControl velocityControl
+
+Perform position control using a velocity control loop.
+
+\section intro_sec Description
+This module performs joint space position control using a velocity loop. 
+It basically implements a pid control converting a position error into a 
+velocity signal that sent to the control board. It allows commanding 
+trajectories at fine temporal scale (~10-20ms).
+
+Note: for safey reasons the module initially starts with 0 gain and 
+0 max velocity, so you first have to set them to correct values.
+
+Use yarp rpc and type for example:
+\code
+gain 0 10
+svel 0 10
+\endcode
+
+This set gain and maximum velocity to 10 for the joint 0.
+
+There are maximum values for gain and velocity set in the code 
+(check velControlThread.cpp). At the moment they are set to 10 and 50 deg/sec respectively.
+
+\section lib_sec Libraries
+YARP
+
+\section parameters_sec Parameters
+
+--robot: specifies the name of the robot. It will be used to form 
+the names of the ports created and accessed by module.
+
+--part: part to control (e.g. head,arm_right, arm_left, lef_right...), it 
+will be used to form the names of the ports created and accessed by the 
+module.
+
+--period: the periodicity of the velocity control loop, in milliseconds 
+(possible values are 20 or 10).
+
+\section portsa_sec Ports Accessed
+It assumes \ref icub_iCubInterface runs. It accesses velocity and 
+encoder ports created by iCubInterface.
+
+\section portsc_sec Ports Created
+The module instantiates a control_board device, which opens the
+usual ports. The pattern of the name is as follow:
+- /robot/vc/part/client/rpc:o
+- /robot/vc/part/client/command:o
+- /robot/vc/part/client/state:i
+
+where robot is the name of the robot as specified with --robot and 
+part is the required part as specified with --part (see below).
+
+- /robot/vc/part/input: input port of the module
+    -	[susp]: suspend the controller (command zero velocity)
+    -	[run]: resume the controller
+    -	[quit]: quit the module (exit)
+    -   [set] j p: move joint j to p (degrees)
+    -   [svel] j v: set maximum speed for joint j to v (deg/sec)
+    -   [gain] j k: set gain for joint j to k
+
+Note: commands to the module through /robot/vc/part/input are not
+fast, use /robot/vc/part/fastCommand or /robot/vc/part/command instead.
+
+- /robot/vc/part/fastCommand: accept a vector of positions as a bottle 
+of int-double pairs, in the form j,p (where j is the joint and p is the 
+requested position). Messages in this format are deprecated, 
+use /robot/vc/part/command instead.
+
+- /robot/vc/part/command: a a vector of positions which specifies the references
+for each joint.
+
+\section in_files_sec Input Data Files
+None.
+
+\section out_data_sec Output Data Files
+None.
+
+\section conf_file_sec Configuration Files
+None.
+
+\section tested_os_sec Tested OS
+Linux and Windows.
+
+\section example_sec Example Instantiation of the Module
+
+velocityControl --robot icub --part head --period 10
+
+Starts the velocityControl module using the robot icub to control
+the head. The period of the velocity control loop will be 10ms.
+
+\author Lorenzo Natale
+
+Copyright (C) 2008 RobotCub Consortium
+ 
+CopyPolicy: Released under the terms of the GNU GPL v2.0.
+
+This file can be edited at src/velocityControl/main.cpp.
+**/
+
+#include <stdio.h>
+
+#include <yarp/os/Network.h>
+#include <yarp/os/Property.h>
+#include <yarp/os/Time.h>
+#include <yarp/dev/PolyDriver.h>
+#include <yarp/os/BufferedPort.h>
+#include "velControlThread.h"
+#include "yarp/os/Module.h"
+#include <string.h>
+
+/*default rate for the control loop*/
+const int CONTROL_RATE=20;
+
+using namespace yarp::os;
+using namespace yarp::dev;
+
+class VelControlModule: public Module {
+private:
+    PolyDriver driver;
+    velControlThread *vc;
+    char partName[255];
+  
+    //added by ludovic
+    BufferedPort<Bottle> input_port;
+    ////
+public:
+  
+    virtual bool respond(const yarp::os::Bottle &command, yarp::os::Bottle &reply)
+    {
+        fprintf(stderr,"receiving command from port\n");
+        int index = 0;
+        int cmdSize = command.size();
+    
+        while(cmdSize>0)
+            {
+                switch(command.get(index).asVocab())  {
+                case  VOCAB4('s','u','s','p'):
+                    {
+                        reply.addVocab(Vocab::encode("ack"));
+                        vc->halt();
+                        cmdSize--;
+                        index++;
+                        break;
+                    }
+                case VOCAB3('r','u','n'):
+                    {
+                        reply.addVocab(Vocab::encode("ack"));
+                        vc->go();
+                        cmdSize--;
+                        index++;
+                        break;
+                    }
+                case VOCAB3('s','e','t'):
+                    {
+                        if (command.size()>=3)
+                            {
+                                int i=command.get(index+1).asInt();
+                                double pos=command.get(index+2).asDouble();
+                                vc->setRef(i, pos);
+                                index +=3;
+                                cmdSize-=3;
+                            }
+                        else
+                            {
+                                cmdSize--;
+                                index++;
+                                fprintf(stderr, "Invalid set message, ignoring\n");
+                            }
+                        reply.addVocab(Vocab::encode("ack"));
+                        break;
+                    }
+                case VOCAB4('s','v','e','l'):
+                    {
+                        if(command.size()>=3)
+                            {
+                                int i=command.get(index+1).asInt();
+                                double vel = command.get(index+2).asDouble();
+                                vc->setVel(i,vel);
+                                index += 3;
+                                cmdSize-=3;;
+                                reply.addVocab(Vocab::encode("ack"));
+                            }
+                        else
+                            {
+                                cmdSize--;
+                                index++;
+                                fprintf(stderr,"Invalid set vel message, ignoring\n");
+                                reply.addVocab(Vocab::encode("fail"));
+                            }
+                        break;
+                    }
+                case VOCAB4('g','a','i','n'):
+                    {
+                        if(command.size()>=3)
+                            {
+                                int i=command.get(index+1).asInt();
+                                double gain = command.get(index+2).asDouble();
+                                vc->setGain(i,gain);
+                                index+=3;
+                                cmdSize-=3;
+                                reply.addVocab(Vocab::encode("ack"));
+                            }
+                        else
+                            {
+                                cmdSize--;
+                                index++;
+                                fprintf(stderr,"Invalid set gain message, ignoring\n");
+                                reply.addVocab(Vocab::encode("fail"));
+                            }
+                        break;
+                    }
+                default:
+                    {
+                        cmdSize--;
+                        index++;
+                        return Module::respond(command, reply); // call default
+                    }
+                }
+                return true;
+            }
+
+        return false;
+    }
+  
+    virtual bool open(yarp::os::Searchable &s)
+    {
+        Property options(s.toString());
+        char robotName[255];
+        Time::turboBoost();    
+        options.put("device", "remote_controlboard");
+        if(options.check("robot"))
+            strncpy(robotName, options.find("robot").asString().c_str(),sizeof(robotName));
+        else
+            strncpy(robotName, "icub", sizeof(robotName));
+	
+        if(options.check("part"))
+            {
+                char tmp[255];
+                sprintf(tmp, "/%s/vc/%s/client",
+                        robotName,
+                        options.find("part").asString().c_str());
+                options.put("local",tmp);
+	    
+                sprintf(tmp, "/%s/%s", 
+                        robotName,
+                        options.find("part").asString().c_str());
+                options.put("remote", tmp);
+	    
+                sprintf(tmp,"/%s/vc/%s/input",
+                        robotName,
+                        options.find("part").asString().c_str());
+                input_port.open(tmp);
+            
+                options.put("carrier", "mcast");
+            
+                attach(input_port,true);
+            }
+        else
+            {
+                fprintf(stderr, "Please specify part (e.g. --part head)\n");
+                return false;
+            }
+        ////end of the modif////////////
+    
+        if (!driver.open(options))
+            {
+                fprintf(stderr, "Error opening device, check parameters\n");
+                return false;
+            }
+
+        ///we start the thread
+        int period = CONTROL_RATE;
+        if(options.check("period"))
+            period = options.find("period").asInt();
+        
+        printf("control rate is %d ms",period);
+
+        if (!options.check("part"))
+            return false;
+        
+        sprintf(partName, "%s", options.find("part").asString().c_str());
+
+        vc=new velControlThread(period);
+        vc->init(&driver, partName,
+                 robotName);
+
+        vc->start();
+        return true;
+    }
+
+    virtual bool close()
+    {
+        fprintf(stderr, "Closing module [%s]\n", partName);
+        vc->stop();
+        delete vc;
+        fprintf(stderr, "Thead [%s] stopped\n", partName);
+
+        driver.close();
+        input_port.close();
+
+        fprintf(stderr, "Module [%s] closed\n", partName);
+
+        return true;
+    }
+
+    virtual double getPeriod()
+    { return 5.0; } // module period
+
+    virtual bool updateModule()
+    {
+        return true;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    Network yarp;
+    VelControlModule mod;
+    
+    return mod.runModule(argc, argv);
+}
---
+++ src/velocityControl/velControlThread.cpp
@@ -0,0 +1,276 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+#include "velControlThread.h"
+#include <string.h>
+#include <string>
+#include <math.h>
+
+#include <iostream>
+
+using namespace yarp::dev;
+using namespace yarp::os;
+using namespace yarp::sig;
+
+const double MAX_SPEED=200;
+const double AVERAGE=50;
+const double ACCELERATIONS=1000000;
+const double MAX_GAIN = 10.0;
+
+velControlThread::velControlThread(int rate):
+yarp::os::RateThread(rate)
+{
+    control_rate = rate;
+    first_command = 0;
+}
+
+velControlThread::~velControlThread()
+{}
+
+void velControlThread::run()
+{
+    double t_start = yarp::os::Time::now();
+    if (getIterations()>100)
+        {
+            fprintf(stderr, "Thread ran %d times, est period %lf[ms], used %lf[ms]\n",
+                    getIterations(),
+                    getEstPeriod(),
+                    getEstUsed());
+            resetStat();
+        }
+    _mutex.wait();
+  
+    ////getting new commands
+    yarp::os::Bottle *bot = command_port.read(false);
+    if(bot!=NULL)
+        {
+            //fprintf(stderr, "\n Receiving command: \t");
+            int size = bot->size()/2;
+            for(int i=0;i<size;i++)
+                {
+                    int ind = bot->get(2*i).asInt();
+                    targets(ind) = bot->get(2*i+1).asDouble();
+                    //fprintf(stderr, "for joint *%d, received %f, \t", ind, targets(ind));
+                }
+            first_command++;
+        }
+
+    yarp::sig::Vector *vec = command_port2.read(false);
+    if (vec!=0)
+    {
+        targets=*vec;
+        first_command++;
+    }
+  
+    static int count=0;
+    count++;
+
+    ienc->getEncoders(encoders.data());
+  
+    //ienc->getEncoderSpeeds(encoders_speed.data());
+    //fprintf(stderr, "printing to file \n");
+#if 0
+    for(int i=0;i<nJoints;i++)
+        fprintf(currentSpeedFile,"%f ",encoders(i));
+    fprintf(currentSpeedFile,"%f\n",t_start-time_watch);
+#endif
+
+   Kd=0.0;
+  
+    for(int k=0;k<nJoints;k++)
+        {
+            double current_err = targets(k)-encoders(k);
+            error_d(k) = (current_err - error(k))/((double)control_rate)*1000.0;
+            error(k) = current_err;
+      
+            command(k) = Kp(k)*error(k) + Kd(k)*error_d(k);
+        }
+
+    //    std::cout << command.toString() << std::endl;
+
+    limitSpeed(command);
+    
+    if (suspended)
+        command=0;
+
+#if 0
+    for(int i=0;i<nJoints;i++)
+        fprintf(targetSpeedFile,"%f ",command(i));
+    fprintf(targetSpeedFile,"%f\n",t_start-time_watch);
+#endif
+
+    if(first_command)
+        ivel->velocityMove(command.data());
+    _mutex.post();
+}
+
+bool velControlThread::threadInit()
+{
+    suspended=false;
+    ienc->getEncoders(encoders.data());
+    //ienc->getEncoderSpeeds(encoders_speed.data());
+    targets=encoders;
+    count = 0;
+    time_watch = Time::now();
+    time_loop = 0.0;
+    first_command=0;
+
+    return true;
+}
+
+void velControlThread::threadRelease()
+{
+   for(int k=0;k<nJoints;k++)
+    {
+        command(k)=0;
+        ivel->velocityMove(k, command[k]);
+    }
+
+   command_port.close();
+   command_port2.close();
+   
+#if 0
+   fclose(targetSpeedFile);
+   fclose(currentSpeedFile);
+#endif
+}
+
+bool velControlThread::init(PolyDriver *d, ConstString partName, ConstString robotName)
+{
+  char tmp[255];
+
+  yarp::os::Time::turboBoost();
+
+  ///opening port for fast transfer of position command
+  sprintf(tmp,"/%s/vc/%s/fastCommand", robotName.c_str(), partName.c_str());
+  fprintf(stderr,"opening port for part %s\n",tmp);
+  command_port.open(tmp);
+
+  std::string tmp2;
+  tmp2="/";
+  tmp2+=robotName.c_str();
+  tmp2+="/vc/";
+  tmp2+=partName.c_str();
+  tmp2+="/command";
+  command_port2.open(tmp2.c_str());
+  
+  if (d==0)
+    return false;
+  
+  driver=d;
+  
+  driver->view(ivel);
+  driver->view(ienc);
+  
+  if ( (ivel==0)||(ienc==0))
+    return false;
+  
+  ivel->getAxes(&nJoints);
+  
+  fprintf(stderr,"controlling %d DOFs\n",nJoints);
+
+  Vector accs;
+  accs.resize(nJoints);
+  ivel->getRefAccelerations(accs.data());
+  accs=ACCELERATIONS;
+  ivel->setRefAccelerations(accs.data());
+  
+  encoders.resize(nJoints);
+  encoders_speed.resize(nJoints);
+  command.resize(nJoints);
+  targets.resize(nJoints);
+  command=0;
+  targets=0;
+  Kp.resize(nJoints);
+  Kp=0;
+  Kd.resize(nJoints);
+  Kd=0;
+  error.resize(nJoints);
+  error=0;
+  error_d.resize(nJoints);
+  error_d=0;
+  
+  maxVel.resize(nJoints);
+  maxVel = 0.0;
+  
+#if 0
+  sprintf(tmp,"%s_target_speed.dat",partName.c_str());
+  targetSpeedFile = fopen(tmp,"w");
+  sprintf(tmp,"%s_current_speed.dat",partName.c_str());
+  currentSpeedFile = fopen(tmp,"w");
+#endif
+
+  return true;
+}
+
+void velControlThread::halt()
+{
+    suspended=true;
+    fprintf(stderr, "Suspended\n");
+    command=0;
+
+    targets=encoders;
+}
+
+void velControlThread::go()
+{
+    suspended=false;
+    fprintf(stderr, "Run\n");
+    targets=encoders;
+}
+
+void velControlThread::setRef(int i, double pos)
+{
+    fprintf(stderr, "Setting new target %d to %lf\n", i, pos);
+    
+    _mutex.wait();
+    targets(i)=pos;
+    first_command++;
+    _mutex.post();
+}
+
+void velControlThread::setVel(int i, double vel)
+{
+  _mutex.wait();
+  
+  if((vel > 0.0) && (vel < MAX_SPEED) && (i>=0) && (i<nJoints))
+    {
+      maxVel(i) = vel;
+      fprintf(stderr,"setting max vel of joint %d to %f\n",i,maxVel(i));
+    }
+  else
+    fprintf(stderr,"impossible to set max vel higher than %f\n",MAX_SPEED);
+  
+  _mutex.post();
+}
+
+void velControlThread::setGain(int i, double gain)
+{
+  _mutex.wait();
+
+  if (gain>MAX_GAIN)
+          gain=MAX_GAIN;
+
+  if((gain >= 0.0) && (i>=0) && (i<nJoints))
+    {
+      Kp(i) = gain;
+      fprintf(stderr,"setting gain for joint %d to %lf\n", i, Kp(i));
+    }
+    else
+      fprintf(stderr,"cannot set gain of joint %d\n",i);
+
+  _mutex.post();
+}
+
+
+void velControlThread::limitSpeed(Vector &v)
+{
+  for(int k=0; k<nJoints;k++)
+        {
+            if (fabs(command(k))>maxVel(k))
+                {
+                    if (command(k)>0)
+                        command(k)=maxVel(k);
+                    else
+                        command(k)=-maxVel(k);
+                }
+        }
+}
