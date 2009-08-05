62e33f38902397c0ac895490babf3707dd348d02
Lorenzo Natale <lorenzo.natale@iit.it>
2009-08-05 13:14:37 +0000

    new repository
    svn path=/trunk/iCub/; revision=59

---
+++ src/cvBlobsLib/BlobLibraryConfiguration.h
@@ -0,0 +1,20 @@
+/************************************************************************
+  			BlobLibraryConfiguration.h
+  			
+FUNCIONALITAT: Configuraci�del comportament global de la llibreria
+AUTOR: Inspecta S.L.
+MODIFICACIONS (Modificaci� Autor, Data):
+
+FUNCTIONALITY: Global configuration of the library
+AUTHOR: Inspecta S.L.
+MODIFICATIONS (Modification, Author, Date):
+LICENSE: See README.TXT
+
+**************************************************************************/
+
+//! Indica si es volen fer servir les MatrixCV o no
+//! Use/Not use the MatrixCV class
+//#define MATRIXCV_ACTIU
+
+// Uses/not use the blob object factory
+//#define BLOB_OBJECT_FACTORY
