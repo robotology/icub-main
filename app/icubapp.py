fc2275effca768d3727775ca38b21f3afecf6143
Lorenzo Natale <lorenzo.natale@iit.it>
2009-09-24 14:01:36 +0000

    No commit message
    svn path=/trunk/iCub/; revision=832

--- app/icubapp.py
+++ app/icubapp.py
@@ -16,6 +16,7 @@
 ##MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
 ##Public License for more details
 
+
 import fnmatch
 import os
 import sys
@@ -38,86 +39,94 @@ class appEntry:
 
 class App:
     def __init__(self, master):
-        frame = Frame(master)
-        frame.pack()
-        self.master=frame
+	frame = Frame(master)
+	frame.pack()
+	self.master=frame
 
     def setManager(self, manager):
-        self.manager=manager
-        
+	self.manager=manager
+
     def setAppList(self, appList):
-        self.appList=appList;
+	self.appList=appList
 
-        tmpFrame=Frame(self.master, relief=SUNKEN)
+	tmpFrame=Frame(self.master, relief=SUNKEN)
 
-        tmpFrame.columnconfigure(0, minsize=150)
-        tmpFrame.columnconfigure(1, minsize=150)
-        tmpFrame.columnconfigure(2, minsize=40)
-             
-        Label(tmpFrame, text="Context:").grid(row=0,column=0, sticky=N+W)
-        Label(tmpFrame, text="Detected xmls:").grid(row=0,column=1, sticky=N+W)
+	tmpFrame.columnconfigure(0, minsize=150)
+	tmpFrame.columnconfigure(1, minsize=150)
+	tmpFrame.columnconfigure(2, minsize=40)
 
-        r=1
-        for app in appList:
-             tmpFrame.grid(row=r, column=0, sticky=W)
-             tmpApp=Label(tmpFrame, text=app.context, relief=SUNKEN).grid(row=r, column=0, sticky=N+W+E)
+	Label(tmpFrame, text="Context:").grid(row=0,column=0, sticky=N+W)
+	Label(tmpFrame, text="Detected xmls:").grid(row=0,column=1, sticky=N+W)
+
+	r=1
+	c=1
+
+	for app in appList:
+
+	    tmpFrame.grid(row=r, column=c, sticky=W)
+	    print app.context
+	    tmpApp=Label(tmpFrame, text=app.context, relief=SUNKEN).grid(row=r, column=0, sticky=N+W+E)
 ##             tmpFrame.columnconfigure(0, minsize=150)
 ##             tmpFrame.columnconfigure(1, minsize=150)
 ##             tmpFrame.columnconfigure(2, minsize=40)
-##        
-             rr=r
-             #tmpFrame=Frame(tmpFrame)
-             #tmpFrame.grid(row=1, column=1, sticky=N)
-             for xml in app.xmls:
-                 tmpXml=Label(tmpFrame,text=xml, relief=SUNKEN)
-                 tmpXml.grid(row=r, column=1, sticky=N+W)
-                 tmp=Button(tmpFrame, text="Run Manager", command=lambda i=os.path.join(app.path, xml):self.runManager(i))
-                 tmp.grid(row=r, column=2, sticky=N)
-                 r=r+1
-             r=r+1
+##
+	    rr=r
+	    #tmpFrame=Frame(tmpFrame)
+	    #tmpFrame.grid(row=1, column=1, sticky=N)
+	    for xml in app.xmls:
+		tmpXml=Label(tmpFrame,text=xml, relief=SUNKEN)
+		tmpXml.grid(row=r, column=c+1, sticky=N+W)
+		tmp=Button(tmpFrame, text="Run Manager", command=lambda i=os.path.join(app.path, xml):self.runManager(i))
+		tmp.grid(row=r, column=c+2, sticky=N)
+		r=r+1
+	    r=r+1
 
-                      
+	    if (r==10):
+		r=1
+		c=c+1
+
+
     def runManager(self, fullPathToXml):
-        print "running",
-        print fullPathToXml
-        
-        cmd=['python', self.manager, fullPathToXml]
-        #print cmd
-        ret=subprocess.Popen(cmd);
-        
+	print "running",
+	print fullPathToXml
+
+	cmd=['python', self.manager, fullPathToXml]
+	#print cmd
+	ret=subprocess.Popen(cmd);
+
 def searchManager():
     contexts=os.listdir('.')
     for c in contexts:
-        if (c=='default'):
-            tmp=os.listdir(c)
-            if 'scripts' in tmp:
-                files=os.listdir(os.path.join(c, 'scripts'))
-                for f in files:
-                    if isManagerPy(f):
-                        ret=os.path.join(c, 'scripts', f)
-                        print ret
-                        return ret
-    
+	if (c=='default'):
+	    tmp=os.listdir(c)
+	    if 'scripts' in tmp:
+		files=os.listdir(os.path.join(c, 'scripts'))
+		for f in files:
+		    if isManagerPy(f):
+			ret=os.path.join(c, 'scripts', f)
+			print ret
+			return ret
+
 def searchApplications():
     contexts=os.listdir('.')
 
     applications=[]
 
     for c in contexts:
-        if (os.path.isdir(c)):
-            #print c
-            tmp=os.listdir(c)
-            if 'scripts' in tmp:
-                #print c
-                tmpApp=appEntry()
-                files=os.listdir(os.path.join(c,'scripts'))
-                tmpApp.path=os.path.join(c,'scripts')
-                tmpApp.context=c
-                tmpApp.xmls=filter(isXML, files)
+	if (os.path.isdir(c)):
+	    #print c
+	    tmp=os.listdir(c)
+	    if 'scripts' in tmp:
+#                print c
+		tmpApp=appEntry()
+		files=os.listdir(os.path.join(c,'scripts'))
+		tmpApp.path=os.path.join(c,'scripts')
+		tmpApp.context=c
+		tmpApp.xmls=filter(isXML, files)
 
-                #print tmpApp.xmls
-                if (tmpApp.xmls!=[]):
-                    applications.append(tmpApp)
+#                print tmpApp.xmls
+		if (tmpApp.xmls!=[]):
+		    applications.append(tmpApp)
 
     return applications
 
@@ -131,13 +140,5 @@ if __name__ == '__main__':
 
     app.setManager(managerPath)
     app.setAppList(applications)
-    
+
     root.mainloop()
-
-
-  
-
-                    
-
-            
-        
