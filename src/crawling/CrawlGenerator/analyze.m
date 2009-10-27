121362a69eef8ce85b5abaf51ed419c77e517fad
Sarah Degallier <sarah.degallier@a3.epfl.ch>
2009-10-27 20:35:59 +0000

    No commit message
    svn path=/trunk/iCub/; revision=1205

--- src/crawling/CrawlGenerator/analyze.m
+++ src/crawling/CrawlGenerator/analyze.m
@@ -3,10 +3,10 @@ load right_arm_target_position.dat;
 load right_leg_target_position.dat;
 load left_leg_target_position.dat;
 
-t1 = left_arm_target_position(:,5);
-t2 = right_arm_target_position(:,5);
-t3 = left_leg_target_position(:,5);
-t4 = right_leg_target_position(:,5);
+t1 = left_arm_target_position(:,1);
+t2 = right_arm_target_position(:,1);
+t3 = left_leg_target_position(:,1);
+t4 = right_leg_target_position(:,1);
 
 t_base = min([t1;t2;t3;t4]);
 
@@ -18,30 +18,30 @@ t4 = t4-t_base;
 
 
 figure;
-plot(t1,left_arm_target_position(:,1));
+plot(t1,left_arm_target_position(:,2));
 hold on;
-plot(t2,right_arm_target_position(:,1),'k');
-plot(t3,left_leg_target_position(:,1),'g');
-plot(t4,right_leg_target_position(:,1),'r');
+plot(t2,right_arm_target_position(:,2),'k');
+plot(t3,left_leg_target_position(:,2),'g');
+plot(t4,right_leg_target_position(:,2),'r');
 
 
 figure;
 
 %subplot(4,1,1);
-plot(t1,left_arm_target_position(:,1:4),'x');
+plot(t1,left_arm_target_position(:,2:5),'x');
 xlim([t1(1) t1(end)]);
 
 %subplot(4,1,2);
 figure;
-plot(t2,right_arm_target_position(:,1:4),'x');
+plot(t2,right_arm_target_position(:,2:5),'x');
 xlim([t1(1) t1(end)]);
 
 %subplot(4,1,3);
 figure;
-plot(t3,left_leg_target_position(:,1:4),'x');
+plot(t3,left_leg_target_position(:,2:5),'x');
 xlim([t1(1) t1(end)]);
 
 %subplot(4,1,4);
 figure;
-plot(t4,right_leg_target_position(:,1:4),'x');
+plot(t4,right_leg_target_position(:,2:5),'x');
 xlim([t1(1) t1(end)]);
