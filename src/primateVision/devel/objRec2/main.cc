614be9dbcaf6691c113a4119975b1e1c4229493b
Andrew Dankers <andrew.dankers@honda-ri.de>
2009-11-02 18:11:19 +0000

    No commit message
    svn path=/trunk/iCub/; revision=1293

--- src/primateVision/devel/objRec2/main.cc
+++ src/primateVision/devel/objRec2/main.cc
@@ -72,14 +72,14 @@ void cart2polar_32f(Ipp32f*cart,int psb_cart,IppiSize cart_size,  Ipp32f* pol,in
 
   for(int x=0;x<cart_size.width;x++){
     for(int y=0;y<cart_size.height;y++){
-      if (cart[y*psb_cart + x] != 0.0){ //only if occupied
+      if (cart[y*psb_cart/4 + x] != 0.0){ //only if occupied
 	//radius = dist of this pixel from cog pixel (cx+cart_size.width/2,cy+cart_size.height/2):
 	r = (int) sqrt((x-(cx+cart_size.width/2.0))*(x-(cx+cart_size.width/2.0)) + (y-(cy+cart_size.height/2.0))*(y-(cy+cart_size.height/2.0)));
 	//theta = 180 + atan(y/x) * rows/360
 	t = (int) ( ( 180.0 + atan2(y-(cy+cart_size.height/2.0),x-(cx+cart_size.width/2.0))*(180.0/IPP_PI))  *  pol_size.height/360.0);
 
 	if (r<pol_size.width && r>=0 && t<pol_size.height && t>=0){
-	  pol[t*psb_pol + r] = cart[y*psb_cart + x];
+	  pol[t*psb_pol/4 + r] = cart[y*psb_cart/4 + x];
 	}
 	//else{printf("p:%d,%d <=> c:%d,%d\n",r,t,x,y);}
       }
@@ -91,40 +91,39 @@ void cart2polar_32f(Ipp32f*cart,int psb_cart,IppiSize cart_size,  Ipp32f* pol,in
 
 
 
-void rollup_8u(int n,Ipp8u*in,int psb_in, int w, int h,Ipp8u*out,int psb_out){
+void rollup_8u(int n,Ipp8u*in,int psb_in, IppiSize sz,Ipp8u*out,int psb_out){
   //roll image up by n rows:
   
-  IppiSize su = {w,h-n};
-  IppiSize sl = {w,n};
+  IppiSize su = {sz.width,sz.height-n};
+  IppiSize sl = {sz.width,n};
   
   //copy nth row downwards of 'in' to 0th row of 'out':
   ippiCopy_8u_C1R(&in[n*psb_in],psb_in,out,psb_out,su);
 
   //copy 0 to nth row of 'in' to (h-n)th row of 'out':
-  ippiCopy_8u_C1R(in,psb_in,&out[(h-n)*psb_out],psb_out,sl);
+  ippiCopy_8u_C1R(in,psb_in,&out[(sz.height-n)*psb_out],psb_out,sl);
 
 }
 
 
 
-void stretchright_8u(Ipp8u* in, int psb_in,int w,int h, Ipp8u* out, int psb_out){
+void stretchright_8u(Ipp8u* in, int psb_in,IppiSize sz_o, Ipp8u* out, int psb_out){
 
-  IppiSize sz_o = {w,h};
   //find rightmost occupied pixel, stretch image right by that many cols.
 
   //loop over image to find rightmost pixel:
   int max_right = 0;
-  for (int y=0;y<h;y++){
-    for (int x=0;x<w;x++){
+  for (int y=0;y<sz_o.height;y++){
+    for (int x=0;x<sz_o.width;x++){
       if (in[y*psb_in+x]!=0 && x>max_right){
 	max_right = x;
       }
     }
   }
 
-  IppiSize sz_i = {w-max_right,h};
-  IppiRect r_i = {0,0,w-max_right,h};
-  double xf = (w-max_right)/w;
+  IppiSize sz_i = {sz_o.width-max_right,sz_o.height};
+  IppiRect r_i = {0,0,sz_o.width-max_right,sz_o.height};
+  double xf = (sz_o.width-max_right)/((double)sz_o.width);
 
   //stretch remaining image right:
   ippiResize_8u_C1R(in,sz_i,psb_in,r_i,  out,psb_out,sz_o,  xf,1.0,IPPI_INTER_LINEAR);
@@ -162,7 +161,6 @@ int main( int argc, char **argv )
   IppiSize zdf_size={zdf_width,zdf_height};
 
 
-#if 0
   //Probe OCSServer:
   Port inPort_o;
   inPort_o.open("/objRec2/input/ocs_serv_params");     // Give it a name on the network.
@@ -177,9 +175,6 @@ int main( int argc, char **argv )
   int ocs_psb       = osp.psb;
   int ocs_psb_32f   = osp.psb_32f;
   IppiSize ocs_size = {ocs_width,ocs_height};
-#endif
-
-
 
 
   //For getting zdf data:
@@ -188,48 +183,37 @@ int main( int argc, char **argv )
   Network::connect("/zdfserver/output/data" , "/objRec2/input/zdfdata");
   ZDFServerData *zdfData;
 
-#if 0
   //For getting orientation data:
   BufferedPort<Bottle> inPort_or;  
   inPort_or.open("/objRec2/input/or");  
   Network::connect("/ocsserver_0/output/phaseOr" , "/objRec2/input/or");
   Bottle *inBot_or;
   Ipp32f* orient;
-#endif
 
   //max_rad = (cent to corner) + (max_cog = cent to corner) = full diag, worst case.
   int polar_width = 128;//(int) sqrt(zdf_width*zdf_width + zdf_height*zdf_height); 
   int polar_height = 128;
   IppiSize polar_size = {polar_width,polar_height};
 
-  int pol_seg_psb;
-  Ipp8u*pol_seg              = ippiMalloc_8u_C1(polar_width,polar_height,&pol_seg_psb);
-
-#if 0
-  int pol_or_psb;
-  Ipp32f*pol_or              = ippiMalloc_32f_C1(polar_width,polar_height,&pol_or_psb);
-  Ipp8u*pol_seg_rolled       = ippiMalloc_8u_C1(polar_width,polar_height,&pol_seg_psb);
-  Ipp8u*pol_seg_rolled_right = ippiMalloc_8u_C1(polar_width,polar_height,&pol_seg_psb);
-
-  int orient_fov_psb;
-  Ipp32f* orient_fov         = ippiMalloc_32f_C1(zdf_width,zdf_height,&orient_fov_psb);
-#endif
-  int out_psb;
-  Ipp8u* out                 = ippiMalloc_8u_C1(zdf_width,zdf_height,&out_psb);
-
+  int pol_seg_psb,out_psb,orient_fov_psb,pol_or_psb;
+  Ipp32f *pol_or               = ippiMalloc_32f_C1(polar_width,polar_height,&pol_or_psb);
+  Ipp8u  *pol_seg              = ippiMalloc_8u_C1(polar_width,polar_height,&pol_seg_psb);
+  Ipp8u  *pol_seg_rolled       = ippiMalloc_8u_C1(polar_width,polar_height,&pol_seg_psb);
+  Ipp8u  *pol_seg_rolled_right = ippiMalloc_8u_C1(polar_width,polar_height,&pol_seg_psb);
+  Ipp32f *orient_fov           = ippiMalloc_32f_C1(zdf_width,zdf_height,&orient_fov_psb);
+  Ipp8u  *out                  = ippiMalloc_8u_C1(zdf_width,zdf_height,&out_psb);
 
   Ipp8u* seg;
 
 
-
-
+  iCub::contrib::primateVision::Display *d_in  = new iCub::contrib::primateVision::Display(zdf_size,zdf_psb,D_8U_NN,"IN");
   iCub::contrib::primateVision::Display *d_out = new iCub::contrib::primateVision::Display(zdf_size,out_psb,D_8U_NN,"ORIENT INV");
-  iCub::contrib::primateVision::Display *d_pol = new iCub::contrib::primateVision::Display(polar_size,pol_seg_psb,D_8U_NN,"POLAR");
 
 
 
 
-  int n,nn;
+
+  int n,nc;
   double av,av_col;
   int cog_x,cog_y;
 
@@ -238,6 +222,8 @@ int main( int argc, char **argv )
   while (1){
 
 
+    printf("ObjRec2: acquiring data..\n");
+
     // get seg and CoG from ZDFSERVER:
     zdfData = inPort_zdf.read(); //blocking
     seg = (Ipp8u*) zdfData->tex.getRawImage();
@@ -245,93 +231,92 @@ int main( int argc, char **argv )
     cog_y = zdfData->cog_y;
 
 
-#if 0
     // get orientation map from OCSSERVER:
     inBot_or = inPort_or.read(); //blocking
 
-    // make copy of orient fov:
+    // make copy of orientation data in fovea:
     ippiCopy_32f_C1R((Ipp32f*) &inBot_or->get(0).asBlob()[ocs_psb_32f*(ocs_height-zdf_height)/2 + (ocs_width-zdf_width)/2],
 		     ocs_psb_32f,orient_fov,orient_fov_psb,zdf_size);
-#endif
 
 
-    //***[1]*******POLAR TRANSFORM:
+
+    printf("ObjRec2: polar trans..\n");
+
     // polar transform seg about CoG:
-    ippiSet_8u_C1R(0, pol_seg,pol_seg_psb,polar_size);
+    ippiSet_8u_C1R(0,pol_seg,pol_seg_psb,polar_size);
     cart2polar_8u(seg,zdf_psb,zdf_size, 
 		  pol_seg,pol_seg_psb,polar_size, 
 		  cog_x,cog_y);
 
-   ippiSet_8u_C1R(0, out,out_psb,zdf_size);
-   polar2cart_8u(pol_seg,pol_seg_psb,polar_size,
-		 out,out_psb,zdf_size);
-
-
-    d_out->display(out);
-    d_pol->display(pol_seg);
-
-
-#if 0
-
     // polar transform orient fov about same point as seg:
-    cart2polar_32f(orient_fov,orient_fov_psb,zdf_size, pol_or,pol_or_psb,polar_size, cog_x,cog_y); 
-    //*************POLAR TRANSFORM.
+    ippiSet_32f_C1R(0.0,pol_or,pol_or_psb,polar_size);
+    cart2polar_32f(orient_fov,orient_fov_psb,zdf_size, 
+		   pol_or,pol_or_psb,polar_size, 
+		   cog_x,cog_y); 
 
 
 
-    //***[2]***********IMPOSE INVARIANCE:
+    printf("ObjRec2: imposing invariance..\n");
+
     // av_col = av of all non-zero entries in col c.
-    av =0.0;
-    nn = 0;
+    av = 0.0;
+    nc = 0;
     for (int col=0;col<polar_width;col++){
       av_col = 0.0; 
       n = 0;
-      for (int row =0;row<polar_height;row++){
-	if (pol_seg[row*pol_seg_psb + col]!=0){
-	  av_col += pol_or[row*pol_or_psb + col];
-	  n++;
+      //get av orient for this col:
+      for (int row=0;row<polar_height;row++){
+	if (pol_seg[row*pol_seg_psb + col]!=0){ //if occupied
+	  if (pol_or[row*pol_or_psb/4 + col]!=0.0){
+	    av_col += pol_or[row*pol_or_psb/4 + col];
+	    n++;
+	  }
 	}
       }
+      //got sum of entries in this col in av_col.
       if (n!=0){
-	av_col/=n;
+	//conv to av. for this col
+	av_col /= n; 
+	//add to overall av.
 	av += av_col;
-	nn++;
+	nc++;
       }
     }
-    // av = av. of all av_cols
-    if (nn!=0){
-      av /= nn;
+    // got sum of cols_av.s in av
+    if (nc!=0){
+      //conv to av.
+      av /= nc;
     }
     else{av = 0.0;}
     
-
-    printf("%f\n",av); 
+    printf("av: %f\n",av); 
 
     //ROLL POLAR IMAGE UP BY "AV" (ORIENT INVARIANCE):
-    rollup_8u((int)av,pol_seg,pol_seg_psb, polar_width, polar_height, pol_seg_rolled, pol_seg_rolled_psb);
+    rollup_8u((int)av,pol_seg,pol_seg_psb,polar_size, pol_seg_rolled, pol_seg_psb);
     
     //stretch polar map fully right (SCALING INVARIANCE):
-    stretchright_8u(pol_seg_rolled, pol_seg_rolled_psb, polar_width,polar_height,  pol_seg_rolled_right, pol_seg_rolled_right_psb);
+    stretchright_8u(pol_seg_rolled, pol_seg_psb, polar_size,  pol_seg_rolled_right, pol_seg_psb);
  
-    //**************INVARIANCE.
 
 
 
+    printf("ObjRec2: converting back to cartesian..\n");
 
     //convert result back to cartesian space:
-    polar2cart_8u(pol_seg_rolled_right,pol_seg_rolled_right_psb,  polar_width,polar_height,  out,out_psb);
+    ippiSet_8u_C1R(0,out,out_psb,zdf_size);
+    polar2cart_8u(pol_seg_rolled_right,pol_seg_psb,polar_size,  out,out_psb,zdf_size);
 
 
 
 
     // display orient & scale invarient output:
-    // d_out->display(seg);
+    d_in->display(seg);
+    d_out->display(out);
 
 
 
     //NOW SEND THE ORIENTED SEG TO CLASSIFIER!!
 
-#endif
 
   }
   
