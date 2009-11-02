d3a9b0056fa3d7b1d086fafe02d4dee6c195d966
Andrew Dankers <andrew.dankers@honda-ri.de>
2009-11-02 16:52:11 +0000

    No commit message
    svn path=/trunk/iCub/; revision=1285

---
+++ src/primateVision/objRec/objRec.cc
@@ -0,0 +1,261 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+
+//MY INCLUDES
+//client of:
+#include <zdfio.h>
+#include "objRec.h"
+
+#define NCLASSES 4
+#define NN_WIDTH 50
+#define NN_HEIGHT 50
+
+using namespace nnfw;
+using namespace std;
+using namespace iCub::contrib::primateVision;
+
+
+
+iCub::contrib::primateVision::ObjRecServer::ObjRecServer( string*cfg_ )
+{
+  
+  cfg=cfg_;
+  start();
+
+}
+
+iCub::contrib::primateVision::ObjRecServer::~ObjRecServer()
+{
+  
+}
+
+void iCub::contrib::primateVision::ObjRecServer::run()
+{
+
+  Property prop;
+  prop.fromConfigFile(cfg->c_str());
+  double threshold = prop.findGroup("OBJREC").find("THRESHOLD").asDouble();
+  momentum  = prop.findGroup("OBJREC").find("MOMENTUM").asDouble();
+  learnRate = prop.findGroup("OBJREC").find("LEARNRATE").asDouble();
+  int max_unknowns = prop.findGroup("OBJREC").find("MAX_UNKNOWNS").asInt();
+
+  //probe ZDFServer:
+  Port inPort_s;
+  inPort_s.open("/objRecServer/input/serv_params"); 
+  Network::connect("/objRecServer/input/serv_params", "/zdfserver/output/serv_params");
+  Network::connect("/zdfserver/output/serv_params", "/objRecServer/input/serv_params");
+  BinPortable<ZDFServerParams> server_response; 
+  Bottle empty;
+  inPort_s.write(empty,server_response);
+  ZDFServerParams zsp = server_response.content();
+  std::cout << "ZDFServer Probe Response: " << zsp.toString() << std::endl;
+  int width = zsp.width;
+  int height = zsp.height;
+  int psb  = zsp.psb;
+  IppiSize msize={width,height};
+  
+  
+
+  //To get data from zdfserver:
+  BufferedPort<ZDFServerData > inPort_zdfdata;  
+  inPort_zdfdata.open("/objRecServer/input/zdfdata");
+  Network::connect("/zdfserver/output/data" , "/objRecServer/input/zdfdata");
+  ZDFServerData *zdfData;
+ 
+
+  //set up NN:
+  //initialise the network with a random see
+  int seed =time(0);
+  nnfw::Random::setSeed(seed); 
+  //load the saved model
+  net = loadXML( "/home/andrew/src/iCub/src/primateVision/objRec/data/learnedModel.xml" ); 
+  const ClusterVec& cl = net->clusters();  //configure the NN clusters
+  for( nnfw::u_int i=0; i<cl.size(); i++ ){                
+    cl[i]->inputs().zeroing();
+    cl[i]->outputs().zeroing();                                
+  }
+  //configure network
+  loadNet();
+  
+  //For resizing befor NN query:
+  temp = cvCreateImage(cvSize(NN_WIDTH,NN_HEIGHT), IPL_DEPTH_8U , 1 );
+  
+
+  const char *label;
+  const char *oldlabel;
+  double bestval = 0.0;
+  double radius = 0.03;
+  int unknowns = 0;
+  
+  
+  
+  //make server probe replier:
+  //Server params:
+  BufferedPort<Bottle> outPort_s;
+  outPort_s.open("/objRecServer/output/serv_params");
+  ObjRecServerParams rsp;
+  rsp.width = width;
+  rsp.height = height;
+  rsp.mos_width = zsp.mos_width;
+  rsp.mos_height = zsp.mos_height;
+  rsp.nclasses = NCLASSES;
+  rsp.psb = psb;
+  //Replier:
+  ObjRecServerReplyParamProbe server_replier;
+  server_replier.reply=rsp;
+  outPort_s.setReplier(server_replier); 
+
+
+  //make online output port:
+  BufferedPort<ObjRecServerData> outPort_objData; 
+  outPort_objData.open("/objRecServer/output/objData"); 
+  
+  
+
+  label = "unknown";
+
+
+  
+  printf("ObjRecServer: begin..\n");
+  //main event loop:
+  while (1){
+    
+    //get data from zdf server
+    zdfData = inPort_zdfdata.read(); //blocking
+
+    //resize dog image into openCV container "temp" for NN query:
+    cvResize((IplImage*) zdfData->dog.getIplImage(), temp, CV_INTER_LINEAR);
+
+
+    
+    //**************************************
+    //CLASSIFY:
+    //set image as inputs to NN:
+    extractPixels(temp);
+    //run the NN:
+    RealVec outputs(out->numNeurons());
+    for (int input = 0; input< (int)in->numNeurons(); input++ )
+      in->setInput( input, pixelValNorm[input][0] ); 
+    net->step();
+
+
+    //get best output:
+    outputs = out->outputs();	
+    bestval = 0.0;    
+    oldlabel = label;
+    if (outputs[0] > bestval){
+      label = "bottle";
+      bestval = outputs[0];
+    }
+    if (outputs[1] > bestval){
+      label = "fags";
+      bestval = outputs[1];
+    }
+    if (outputs[2] > bestval ){
+      label = "coke";
+      bestval = outputs[2];
+    }
+
+    //if not very sure (unknown)
+    if (bestval<threshold){
+      //revert to previous label.
+      label = oldlabel;
+      //only pass 'unknown' if we've had lots of low-conf classifications.
+      unknowns++;
+      if (unknowns>=max_unknowns){
+	label = "unknown";
+      }
+    }
+    else{
+      //classification was strong
+      unknowns = 0;
+    }
+
+    
+    printf("ObjRecServer: Mos:[%d,%d][%d,%d] 3D:(%f,%f,%f):%f %s \n",
+	   zdfData->mos_xl,zdfData->mos_yl,zdfData->mos_xr,zdfData->mos_yr,
+	   zdfData->x,zdfData->y,zdfData->z,bestval,label);
+    //**************************************
+    
+    
+    
+
+    //OUTPUT
+
+    //SEND as portable <ObjRecServerData>:
+    ObjRecServerData& objData = outPort_objData.prepare();
+    objData.resize(width,height);
+    ippiCopy_8u_C1R((Ipp8u*)zdfData->tex.getRawImage(),zdfData->tex.getRowSize(),
+		    (Ipp8u*)objData.tex.getRawImage(),objData.tex.getRowSize(), msize);
+    objData.label = label;        
+    objData.radius = radius;      
+    objData.confidence = bestval; 
+    objData.x = zdfData->x;
+    objData.y = zdfData->y;
+    objData.z = zdfData->z;
+    objData.mos_xl = zdfData->mos_xl;
+    objData.mos_yl = zdfData->mos_yl;
+    objData.mos_xr = zdfData->mos_xr;
+    objData.mos_yr = zdfData->mos_yr;
+    //send:
+    outPort_objData.write();
+    
+
+    //}//if update
+    
+    
+  } //while
+  
+  //never here! 
+}
+
+
+
+
+
+
+
+void iCub::contrib::primateVision::ObjRecServer::loadNet(){
+  //load the net
+  
+  in = (BiasedCluster*)net->getByName("in");
+  out = (BiasedCluster*)net->getByName("out");
+  numInputs = in->numNeurons();
+  numOutputs = out->numNeurons();
+  hid = (BiasedCluster*)net->getByName("hid");
+  in2hid = (DotLinker*)net->getByName("in2hid");
+  hid2out = (DotLinker*)net->getByName("hid2out");
+  numHiddens = hid->numNeurons();
+  //reverse the order	
+  UpdatableVec rev_ord(net->order().size());
+  rev_ord.assign_reverse(net->order());
+  learnNet = new BackPropagationAlgo (net, rev_ord, learnRate);
+  learnNet->enableMomentum();
+  learnNet->setMomentum(momentum);
+}
+
+
+void iCub::contrib::primateVision::ObjRecServer::extractPixels(IplImage* img){
+
+  int height,width;
+  char path[100] ;
+  CvScalar s;
+  for (int i=0; i<img->height; i++){
+    for (int j=0; j<img->width; j++){	
+
+      //get the (i,j) pixel intensity
+      s=cvGet2D(img,i,j); 
+      //convert to range 0.0->1.0:
+      pixelValNorm[inc][0] = s.val[0]/255;		
+      //printf("intensity = %lf  %d\n", pixelValNorm[inc][0], inc);
+      inc ++;
+    }
+  }
+  inc = 0;
+
+}
+
+
+
