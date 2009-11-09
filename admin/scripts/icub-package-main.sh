837a53a6aa91960334c7b419e3ddce95b4eb876e
Lorenzo Natale <lorenzo.natale@iit.it>
2009-11-09 23:32:13 +0000

    No commit message
    svn path=/trunk/iCub/; revision=1371

--- admin/scripts/icub-package-main.sh
+++ admin/scripts/icub-package-main.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-SOURCE_TMP_DIR=./package-tmp
-MODULE=iCub
-REL=1.0.1
-TARFILE=$MODULE-src-$REL.tar.gz
-DEPFILE=$MODULE-dep-$REL.txt
-URL=https://robotcub.svn.sourceforge.net/svnroot/robotcub/trunk/iCub
+export SOURCE_TMP_DIR=./package-tmp
+export MODULE=iCub
+export REL=1.0.1
+export TARFILE=$MODULE-src-$REL.tar.gz
+export DEPFILE=$MODULE-dep-$REL.txt
+export URL=https://robotcub.svn.sourceforge.net/svnroot/robotcub/trunk/iCub
 ./admin/scripts/icub-package-make-tar.sh
 scp $TARFILE babybot@eris.liralab.it:/var/www/html/iCub/downloads/src/
 rm $TARFILE
 
-SOURCE_TMP_DIR=./package-tmp
-MODULE=yarp
-TARFILE=$MODULE-src-$REL.tar.gz
-DEPFILE=$MODULE-dep-$REL.txt
-URL=https://yarp0.svn.sourceforge.net/svnroot/yarp0/trunk/yarp2
+export SOURCE_TMP_DIR=./package-tmp
+export MODULE=yarp2
+export TARFILE=$MODULE-src-$REL.tar.gz
+export DEPFILE=$MODULE-dep-$REL.txt
+export URL=https://yarp0.svn.sourceforge.net/svnroot/yarp0/trunk/yarp2
 ./admin/scripts/icub-package-make-tar.sh
 scp $TARFILE babybot@eris.liralab.it:/var/www/html/iCub/downloads/src/
 rm $TARFILE
