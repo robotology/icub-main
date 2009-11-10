2c9183c349a3888ea1096f93da34ba17fb66330d
Lorenzo Natale <lorenzo.natale@iit.it>
2009-11-10 00:17:49 +0000

    No commit message
    svn path=/trunk/iCub/; revision=1374

--- admin/scripts/icub-package-main.sh
+++ admin/scripts/icub-package-main.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-#export SOURCE_TMP_DIR=./package-tmp
-#export MODULE=iCub
+export SOURCE_TMP_DIR=./package-tmp
+export MODULE=iCub
 export REL=1.0.1
-#export TARFILE=$MODULE-src-$REL.tar.gz
-#export DEPFILE=$MODULE-dep-$REL.txt
-#export URL=https://robotcub.svn.sourceforge.net/svnroot/robotcub/trunk/iCub
-#./admin/scripts/icub-package-make-tar.sh
-#scp $TARFILE babybot@eris.liralab.it:/var/www/html/iCub/downloads/src/
-#rm $TARFILE
+export TARFILE=$MODULE-src-$REL.tar.gz
+export DEPFILE=$MODULE-dep-$REL.txt
+export URL=https://robotcub.svn.sourceforge.net/svnroot/robotcub/trunk/iCub
+./admin/scripts/icub-package-make-tar.sh
+scp $TARFILE babybot@eris.liralab.it:/var/www/html/iCub/downloads/src/
+rm $TARFILE
+
+cp ./admin/scripts/current_dependencies.txt $DEPFILE
+scp $DEPFILE babybot@eris.liralab.it:/var/www/html/iCub/downloads/src/
 
 export SOURCE_TMP_DIR=./package-tmp
 export MODULE=yarp2
