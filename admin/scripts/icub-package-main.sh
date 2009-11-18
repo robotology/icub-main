a4a245ad189fedb29a6af36bfd3cb3f06cbb0f32
Lorenzo Natale <lorenzo.natale@iit.it>
2009-11-18 22:56:05 +0000

    No commit message
    svn path=/trunk/iCub/; revision=1557

--- admin/scripts/icub-package-main.sh
+++ admin/scripts/icub-package-main.sh
@@ -2,7 +2,7 @@
 
 export SOURCE_TMP_DIR=./package-tmp
 export MODULE=iCub
-export REL=1.0.1
+export REL=1.0.2
 export TARFILE=$MODULE-src-$REL.tar.gz
 export DEPFILE=$MODULE-dep-$REL.txt
 export URL=https://robotcub.svn.sourceforge.net/svnroot/robotcub/trunk/iCub
@@ -13,12 +13,12 @@ rm $TARFILE
 cp ./admin/scripts/current_dependencies.txt $DEPFILE
 scp $DEPFILE babybot@eris.liralab.it:/var/www/html/iCub/downloads/src/
 
-export SOURCE_TMP_DIR=./package-tmp
-export MODULE=yarp2
-export TARFILE=yarp4iCub-src-$REL.tar.gz
-export URL=https://yarp0.svn.sourceforge.net/svnroot/yarp0/trunk/yarp2
-./admin/scripts/icub-package-make-tar.sh
-scp $TARFILE babybot@eris.liralab.it:/var/www/html/iCub/downloads/src/
-rm $TARFILE
+#export SOURCE_TMP_DIR=./package-tmp
+##export MODULE=yarp2
+#export TARFILE=yarp4iCub-src-$REL.tar.gz
+#export URL=https://yarp0.svn.sourceforge.net/svnroot/yarp0/trunk/yarp2
+#./admin/scripts/icub-package-make-tar.sh
+#scp $TARFILE babybot@eris.liralab.it:/var/www/html/iCub/downloads/src/
+#rm $TARFILE
 
 echo "Done"
--- admin/scripts/icub-package-make-tar.sh
+++ admin/scripts/icub-package-make-tar.sh
@@ -9,6 +9,13 @@ svn co $URL
 echo "Clean svn files"
 find ./ -iname .svn -exec rm -rf {} \;
 
+versionFile=$MODULE/VERSION
+echo "iCub snapshot version $REL" > versionFile
+echo "Built on `date`" >> versionFile
+echo "See $DEPFILE for list of library dependencies." >> versionFile
+
+cp ./admin/scripts/current_dependencies.txt $MODULE/$DEPFILE
+
 echo "Preparing tar file"
 tar cvfz ../$TARFILE $MODULE
 
