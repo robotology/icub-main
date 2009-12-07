eaecda9a55f842570521a56f2eabf569edca65b3
Christian Wressnegger <christian@wressnegger.info>
2009-12-07 10:26:23 +0000

    [eye2world] re-structured module
    svn path=/trunk/iCub/; revision=1913

---
+++ src/eye2world/module/src/Eye2world.cpp
@@ -0,0 +1,249 @@
+/*******************************************************************************
+ * Copyright (C) 2009 Christian Wressnegger
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *******************************************************************************/
+
+#include "iCub/vislab/Eye2world.h"
+
+using namespace std;
+using namespace iKin;
+using namespace ctrl;
+
+using namespace vislab::util;
+using namespace vislab::yarp::util;
+
+using namespace yarp::os;
+using namespace yarp::sig;
+using namespace yarp::math;
+
+namespace vislab {
+namespace math {
+
+Eye2world::Eye2world() :
+	AbstractRFModule("eye2world") {
+}
+
+bool Eye2world::configure(ResourceFinder &rf) {
+	if (!AbstractRFModule::configure(rf)) {
+		return false;
+	}
+	ConstString str;
+
+	str = "Adds a specific value to the z coordinate of the transformation to the projection plane.";
+	addModuleOption(new Option("heightOffset", str, Option::NUMERIC, Value(0.0)));
+	str
+			= "This value is added to the z coordinate of the transformation to the projection plane but is again subtracted from the result afterwards.";
+	addModuleOption(new Option("motor2eye", str, Option::NUMERIC, Value(0.06)));
+	str
+			= "A scale with is applied to the results. This might be used to compensate calibration issues.";
+	addModuleOption(new Option("scale", str, Option::NUMERIC, Value(1.0)));
+
+	Value* v = Value::makeList("1.0 2 3.3");
+	addModuleOption(new Option("vector", "Test vector", Option::VECTOR, *v));
+	delete v;
+
+	ConstString robotPort = prefix + robotName;
+
+	ConstString calibrationFilename = rf.check("eyeCalibration", Value("iCubEyes.ini"),
+			"Name of the configuration file of the iCub's eye calibration (string)").asString();
+
+	ConstString tableConfiguration = rf.check("tableConfiguration", Value("table.ini"),
+			"Name of the configuration file of the experiments table (string)").asString();
+
+	str
+			= "Input port providing 2D coordinates as Bottle([ConstString:cam,] double:x, double:y[, double:height])";
+	str = prefix + getName(rf.check("in", Value("/in"), str).asString());
+	dataPorts.add(id.Input_Coordinates2d, str, new BufferedPort<Bottle> );
+
+	str = prefix + getName(rf.check("headState", Value(robotPort + "/head/state:i"),
+			"Input port providing the head's position").asString());
+	dataPorts.add(id.Input_HeadPosition, str, new BufferedPort<Bottle> );
+
+	str = prefix + getName(rf.check("torsoState", Value(robotPort + "/torso/state:i"),
+			"Input port providing the torso's position").asString());
+	dataPorts.add(id.Input_TorsoPosition, str, new BufferedPort<Bottle> );
+
+	str = prefix + getName(rf.check("out", Value("/out"),
+			"Output port providing 3D coordinates as Bottle(double:x, double:y, double:z)").asString());
+	dataPorts.add(id.Output_Coordinates3d, str, new BufferedPort<Bottle> );
+
+	ResourceFinder config;
+	config.setDefaultConfigFile(calibrationFilename);
+	config.setDefaultContext("");
+	config.configure("ICUB_ROOT", 0, NULL, false);
+	rightEyeCalibration.fromString(config.findGroup("CAMERA_CALIBRATION_RIGHT").toString());
+	leftEyeCalibration.fromString(config.findGroup("CAMERA_CALIBRATION_LEFT").toString());
+
+	cameras["right"] = &rightEyeCalibration;
+	cameras["left"] = &leftEyeCalibration;
+
+	ResourceFinder tableConfig;
+	tableConfig.setDefaultConfigFile(tableConfiguration);
+	tableConfig.setDefaultContext("");
+	tableConfig.configure("ICUB_ROOT", 0, NULL, false);
+	tabletopPosition.fromString(tableConfig.findGroup("POSITION").toString());
+
+	vector<unsigned int> errorLog;
+	if (!dataPorts.open(&errorLog)) {
+		for (unsigned int i = 0; i < errorLog.size(); i++) {
+			cout << getName() << ": unable to open port " << dataPorts.getName(i) << endl;
+		}
+		return false; // unable to open; let RFModule know so that it won't run
+	}
+
+	return startThread();
+}
+
+Thread* Eye2world::createWorkerThread() {
+	workerThread = new WorkerThread(moduleOptions, dataPorts, id, cameras, tabletopPosition);
+	return workerThread;
+}
+
+Eye2world::WorkerThread::WorkerThread(const OptionManager& moduleOptions,
+		const Contactables& ports, struct PortIds ids, map<const string, Property*>& cams,
+		Property& table) :
+	AbstractWorkerThread(moduleOptions, ports) {
+
+	id = ids;
+
+	Vector v(3);
+	v[0] = table.find("x").asDouble();
+	v[1] = table.find("y").asDouble();
+	v[2] = table.find("z").asDouble();
+
+	map<const string, Property*>::const_iterator itr;
+	for (itr = cams.begin(); itr != cams.end(); ++itr) {
+
+		const string key = itr->first;
+		Property calib = *itr->second;
+
+		projections[key] = new EyeTableProjection(key.c_str(), calib, &v);
+	}
+}
+
+Eye2world::WorkerThread::~WorkerThread() {
+	map<const string, EyeTableProjection*>::const_iterator itr;
+	for (itr = projections.begin(); itr != projections.end(); ++itr) {
+		delete projections[itr->first];
+	}
+}
+
+bool Eye2world::WorkerThread::threadInit() {
+	return true;
+}
+
+//#define DEBUG_OFFLINE
+
+void Eye2world::WorkerThread::run() {
+	Vector head6d(6);
+	Vector torso3d(3);
+	double prevZOffset = 0.0;
+
+#ifndef DEBUG_OFFLINE
+	bool positionUpdated = false;
+	bool transformationAvailable = false;
+	BufferedPort<Bottle>* in = (BufferedPort<Bottle>*) dataPorts[id.Input_Coordinates2d];
+	BufferedPort<Bottle>* head = (BufferedPort<Bottle>*) dataPorts[id.Input_HeadPosition];
+	BufferedPort<Bottle>* torso = (BufferedPort<Bottle>*) dataPorts[id.Input_TorsoPosition];
+#endif
+	BufferedPort<Bottle>* out = (BufferedPort<Bottle>*) dataPorts[id.Output_Coordinates3d];
+
+	while (!isStopping()) {
+
+		string camera = "left";
+		Vector object2d(2);
+		double zOffset = 0;
+
+#ifdef DEBUG_OFFLINE
+		yarp::os::Time::delay(1);
+#else
+		do {
+			inputCoordinates = in->read(true);
+		} while (inputCoordinates == NULL && !isStopping());
+
+		if (isStopping()) {
+			break;
+		}
+
+		unsigned int coordinatesPos = 0;
+		if (inputCoordinates->get(0).isString()) {
+			camera = inputCoordinates->get(0).asString().c_str();
+			// optional! default: 0.0
+			coordinatesPos = 1;
+		} else {
+			// Assume (x,y) pair!
+			// This is for testing with YARP viewer click port.
+		}
+
+		double motor2eye = moduleOptions["motor2eye"].getValue().asDouble();
+
+		for (unsigned int i = 0; i < 2; i++) {
+			object2d[i] = inputCoordinates->get(coordinatesPos + i).asDouble();
+		}
+		zOffset = moduleOptions["heightOffset"].getValue().asDouble();
+		zOffset += motor2eye;
+		zOffset += inputCoordinates->get(coordinatesPos + 2).asDouble();
+
+		positionUpdated = zOffset != prevZOffset;
+
+		Bottle* headPosition = head->read(!transformationAvailable);
+		if (headPosition != NULL && headPosition->size() >= 6) {
+			//get data and convert from degrees to radiant
+			for (unsigned int i = 0; i < 6; i++) {
+				head6d[i] = headPosition->get(i).asDouble() * M_PI / 180.0;
+			}
+			positionUpdated = true;
+		}
+
+		Bottle* torsoPosition = torso->read(!transformationAvailable);
+		if (torsoPosition != NULL && torsoPosition->size() >= 3) {
+			//get data and convert from degrees to radiant
+			for (unsigned int i = 0; i < 3; i++) {
+				torso3d[i] = torsoPosition->get(i).asDouble() * M_PI / 180.0;
+			}
+			positionUpdated = true;
+		}
+
+		if (positionUpdated || !transformationAvailable) { // The 2nd one is just to be sure ;)
+			projections[camera]->setHeightOffset(zOffset, false);
+			projections[camera]->setBaseTransformation(torso3d, head6d);
+			transformationAvailable = true;
+		}
+#endif
+
+		Vector object3d(3);
+		projections[camera]->project(object2d, object3d);
+
+		// Add scalor to the result
+		double scale = moduleOptions["scale"].getValue().asDouble();
+		object3d[0] = object3d[0] * scale;
+		object3d[1] = object3d[1] * scale;
+
+		object3d[2] = object3d[2] - motor2eye;
+
+		Bottle &outputCoordinates = out->prepare();
+		outputCoordinates.clear();
+		for (unsigned int i = 0; i < 3; i++) {
+			outputCoordinates.addDouble(object3d[i]);
+		}
+		cout << "out = " << outputCoordinates.toString() << endl;
+		out->write();
+	}
+}
+
+void Eye2world::WorkerThread::threadRelease() {
+}
+
+}
+}
---
+++ src/eye2world/module/src/iCub/vislab/Eye2world.h
@@ -0,0 +1,288 @@
+/*******************************************************************************
+ * Copyright (C) 2009 Christian Wressnegger
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *******************************************************************************/
+
+/**
+ * @ingroup icub_module
+ *
+ * \defgroup icub_eye2world eye2world
+ *
+ * This module provides a projection from the image plane to the world represented as
+ * plane in front of the robot relatively specified to robot's base coordinates (e.g.
+ * think of a table the robot is operating on)
+ *
+ * 
+ * \section lib_sec Dependencies
+ *
+ * - YARP (YARP_{OS,dev,sig,math})
+ *   - ACE
+ * - iKin
+ *   - IPOPT
+ *   - ctrlLib
+ * - OpenVislab (libvislab, libvislab_YARP): http://OpenVislab.sf.net
+ *
+ * \section parameters_sec Parameters
+ * 
+ * \section cmdline_parameters_sec Command-line Parameters
+ * 
+ * The following key-value pairs can be specified as command-line parameters by prefixing -- to the key 
+ * (e.g. --from conf.ini). The value part can be changed to suit your needs; the default values are shown below.
+ *
+ * --from &lt;STRING&gt; <br />
+ *   specifies the configuration file. <br />
+ *   default: "conf.ini"
+ *
+ * --context &lt;STRING&gt; <br />
+ *   specifies the sub-path from $ICUB_ROOT/icub/app to the configuration file. <br />
+ *   default: "eye2world"
+ *
+ * --name &lt;STRING&gt; <br />
+ *   specifies the name of the module (used to form the stem of module port names). <br />
+ *   default: "eye2world"
+ *
+ * --robot &lt;STRING&gt; <br />
+ *   specifies the name of the robot (used to form the root of robot port names). <br />
+ *   default: "icub"
+ *
+ *
+ * \subsection conffile_parameters_sec Configuration File Parameters
+ *
+ * The following key-value pairs can be specified as parameters in the configuration file 
+ * (they can also be specified as command-line parameters).
+ * The value part can be changed to suit your needs; the default values are shown below. 
+ *   
+ * --eyeCalibration &lt;FILE&gt; <br />
+ *   specifies the file name to the calibration file for the iCub's eyes.
+ *
+ * --tableConfiguration &lt;FILE&gt; <br />
+ *   specifies the file name to configuration of the file.
+ *
+ * --in &lt;PORT&gt; <br />
+ *   specifies the input port name for the 2D coordinate to be transformed. <br />
+ *   default: /in
+ *
+ * --headState &lt;PORT&gt; <br />
+ *   specifies the input port name for head's state (position/ pose). <br />
+ *   default: /icub/head/state:i
+ *
+ * --torsoState &lt;PORT&gt; <br />
+ *   specifies the input port name for head's state (position/ pose). <br />
+ *   default: /icub/torso/state:i
+ *
+ * --out &lt;PORT&gt; <br />
+ *   specifies the output port name for the resulting 3D coordinates. <br />
+ *   default: /out
+ *
+ * 
+ * \section portsa_sec Ports Accessed
+ * 
+ * - /icub/head/state:o <br />
+ *   The coordinates are expected to be wrapped in a Bottle object as double values:
+ *   @code Bottle(double:neckPitch, double:neckRoll, double:neckYaw, double:eyesTilt, double:eyesVesion, double:eyesVergence) @endcode
+ *
+ * - /icub/torso/state:o <br />
+ *   The coordinates are expected to be wrapped in a Bottle object as double values:
+ *   @code Bottle(double:yaw, double:roll, double:pitch) @endcode
+ *                      
+ * \section portsc_sec Ports Created
+ *
+ * \subsection inputports_sec Input ports
+ *
+ *  - /eye2world <br />
+ *    This port is used to change the parameters of the module at run time or stop the module
+ *    The following commands are available:
+ *    - set &#91; &lt;id&gt; &lt;value&gt; &#93; <br />
+ *      available options:
+ *      - heightOffset (numeric) <br />
+ *        This value will be added to the z-offset of the projection plan to the robot's base
+ *        coordinates. This may help you time find the correct position of the projection plane
+ *        without modifying the configuration and restarting the module.
+ *      - motor2eye (numeric) <br />
+ *        This value is supposed to compensate the error introduced by the fact that the distance
+ *        between the lens and the motor of the eye is not taken into account for the kinematics.
+ *        In order to achieve this, the value is simply added to the z coordinate of the
+ *        transformation to the projection plane but is again subtracted from the result afterwards.
+ *        That way it is possible to adjust the accuracy of the project without messing about the
+ *        real relative distance to the projection plane.
+ *      - scale (numeric) <br />
+ *        This one will be multiplied to the resulting x and y coordinate. This is another
+ *        Possibility to tweak accuracy of the projection.
+ *    - echo <str>
+ *    - help
+ *    - quit
+ *
+ *    Note that the name of this port mirrors whatever is provided by the --name parameter value
+ *    The port is attached to the terminal so that you can type in commands and receive replies.
+ *    The port can be used by other modules but also interactively by a user through the yarp rpc
+ *    directive: yarp rpc /eye2world This opens a connection from a terminal to the port and allows
+ *    the user to then type in commands and receive replies.
+ *
+ *  - /eye2world/in <br />
+ *    cf. Input parameter "--in" <br />
+ *    The coordinates are wrapped in a Bottle object as double values: @code Bottle([ConstString:cam,] double:x, double:y[, double:height]) @endcode
+ *    Optionally it is possible to specify the camera/ eye to use ("left", "right") and an additional offset with respect to the
+ *    z-axes of the projection plane.
+ *
+ * \subsection outputports Output ports
+ *
+ *  - /eye2world <br />
+ *    see above
+ *
+ *  - /eye2world/out <br />
+ *    cf. Input parameter "--out" <br />
+ *    The coordinates are wrapped in a Bottle object as double values: @code Bottle(double:x, double:y, double:z) @endcode
+ *
+ *
+ * \section in_files_sec Input Data Files
+ *
+ * \subsection eyecalib_sec Eye calibration:
+ *
+ * \code
+ *   [CAMERA_CALIBRATION_${X}]
+ *   (...)
+ *   fx 216.171
+ *   fy 216.265
+ *   cx 173.283
+ *   cy 141.662
+ *   (...)
+ * \endcode
+ *
+ * where ${X} might be "LEFT" or "RIGHT"
+ *
+ * \subsection tableconf_sec Table configuration
+ *
+ * \code
+ *   [POSITION]
+ *   x  0.0
+ *   y  0.0
+ *   z -0.06
+ * \endcode
+ *
+ * where (x,y,z) is the 3D offset of the projection plane relative to the robot's reference frame in meters.
+ *
+ * \section out_data_sec Output Data Files
+ *
+ * None
+ *
+ * \section conf_file_sec Configuration Files
+ *
+ * conf.ini in $ICUB_ROOT/icub/app/Eye2world/
+ * 
+ * \section tested_os_sec Tested OS
+ *
+ * most extensively on
+ * Linux version 2.6.30-gentoo-r8 (gcc version 4.3.4 (Gentoo 4.3.4 p1.0, pie-10.1.5) ) but compiles
+ * and runs fine on Windows XP SP3 MSVC++8 as well.
+ *
+ * \section example_sec Example Instantiation of the Module
+ * 
+ * <dl>
+ * 	<dt>eye2world</dt>
+ * 		<dd>--in /in<dd>
+ *    <dd>--eyeCalibration "iCubLisboa01/conf/icubEyes.ini"</dd>
+ * 		<dd>--tableConfiguration "iCubLisboa01/conf/table.ini" </dd>
+ * 		<dd>--out /out</dd>
+ * </dl>
+ *
+ *
+ * \author Christian Wressnegger
+ * 
+ * Copyright (C) 2009 Christian Wressnegger<br />
+ * CopyPolicy: Released under the terms of the GNU GPL v2.0.
+ */
+
+#ifndef __ICUB_EYE2WORLD_MODULE_H__
+#define __ICUB_EYE2WORLD_MODULE_H__
+
+
+#include <vislab/yarp/all.h>
+#include <vislab/yarp/util/all.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <yarp/sig/all.h>
+#include <yarp/os/all.h>
+#include <yarp/math/Math.h>
+
+#include <iCub/iKinFwd.h>
+#include <iCub/ctrlMath.h>
+
+#include <iCub/vislab/EyeTableProjection.h>
+
+namespace vislab {
+namespace math {
+
+/**
+ * Implements a {@link RFModule} that determines the 3D position of a 2D coordinate pair
+ * by projecting from the image plane to the world represented as plane in front of the robot
+ * relatively specified to robot's base coordinates (e.g. think of a table the robot is
+ * operating on)
+ *
+ * @author Christian Wressnegger
+ * @date 2009
+ */
+class Eye2world: public vislab::yarp::util::AbstractRFModule {
+
+	struct PortIds {
+		unsigned int Input_Coordinates2d, Input_HeadPosition, Input_TorsoPosition, Output_Coordinates3d;
+	} id;
+
+	class WorkerThread: public vislab::yarp::util::AbstractRFModule::AbstractWorkerThread {
+	private:
+
+		::yarp::os::Bottle* inputCoordinates;
+		std::map<const std::string, EyeTableProjection*> projections;
+		struct PortIds id;
+
+	public:
+
+		WorkerThread(const vislab::yarp::util::OptionManager& moduleOptions,
+				const vislab::yarp::util::Contactables& ports, struct PortIds ids, std::map<
+						const std::string, ::yarp::os::Property*>& cameras, ::yarp::os::Property& table);
+		~WorkerThread();
+		bool threadInit();
+		void threadRelease();
+		void run();
+	};
+
+	::yarp::os::Property leftEyeCalibration;
+	::yarp::os::Property rightEyeCalibration;
+	::yarp::os::Property tabletopPosition;
+
+	std::map<const std::string, ::yarp::os::Property*> cameras;
+
+protected:
+	WorkerThread *workerThread;
+	virtual ::yarp::os::Thread* createWorkerThread();
+
+public:
+	/**
+	 * The default constructor.
+	 */
+	Eye2world();
+
+	/**
+	 * @see AbstractRFModule#configure(::yarp::os::ResourceFinder&)
+	 */
+	// configure all the module parameters and return true if successful
+	bool configure(::yarp::os::ResourceFinder& rf);
+};
+
+}
+}
+
+#endif // __ICUB_EYE2WORLD_MODULE_H__
