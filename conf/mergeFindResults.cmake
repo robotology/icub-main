70d9e6fca5cca08f0a5e83806424aece1ad2edc0
Christian Wressnegger <christian@wressnegger.info>
2009-12-15 09:04:24 +0000

    [libvislab] more CMake <2.6 stuff
    svn path=/trunk/iCub/; revision=2093

--- conf/mergeFindResults.cmake
+++ conf/mergeFindResults.cmake
@@ -14,6 +14,15 @@
 # Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ################################################################################
 
+macro(list_contains result needle)
+  set(${result} -1)
+  foreach(arg ${ARGN})
+    if(${needle} STREQUAL ${arg})
+      set(${result} 0)
+    endif(${needle} STREQUAL ${arg})
+  endforeach (arg)
+endmacro(list_contains)
+
 
 macro(mergeFindResults target) # [package1 [package2 [package3 ... ]]])
 
@@ -32,11 +41,17 @@ set(LIBRARIES "")
 foreach(arg ${ARGN})
   string(TOUPPER ${arg} ARG)
   if(${ARG}_FOUND)
-    if(${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}${CMAKE_PATCH_VERSION} LESS 2.48)
+  
+    if(${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION} LESS 2.4)
       set(b -1)
-    else(${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}${CMAKE_PATCH_VERSION} LESS 2.48)
-      list(FIND INCLUDES "${${ARG}_INCLUDES}" b)
-    endif(${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}${CMAKE_PATCH_VERSION} LESS 2.48)
+    else(${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION} LESS 2.4)
+      if(${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}${CMAKE_PATCH_VERSION} LESS 2.48)
+        list_contains(b "${${ARG}_INCLUDES}" ${INCLUDES})
+      else(${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}${CMAKE_PATCH_VERSION} LESS 2.48)
+        list(FIND INCLUDES "${${ARG}_INCLUDES}" b)
+      endif(${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}${CMAKE_PATCH_VERSION} LESS 2.48)
+    endif(${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION} LESS 2.4)
+
     if(b LESS 0)
       set(INCLUDES ${INCLUDES} ${${ARG}_INCLUDES})
    	endif(b LESS 0)
