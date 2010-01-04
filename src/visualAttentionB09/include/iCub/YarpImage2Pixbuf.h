33b7f66b45ff65630c14c0db94008a4f8689194f
Francesco Rea <francesco.rea@iit.it>
2010-01-04 10:14:05 +0000

    removed after the old name has been changed
    svn path=/trunk/iCub/; revision=2397

--- src/visualAttentionB09/include/iCub/YarpImage2Pixbuf.h
+++
@@ -1,8 +0,0 @@
-#ifndef __YARPIMAGE2PIXBUF__
-
-#include <yarp/sig/Image.h>
-#include <gtk/gtk.h>
-
-bool yarpImage2Pixbuf(yarp::sig::ImageOf<yarp::sig::PixelRgb> *sourceImg, 
-                      GdkPixbuf* destPixbuf);
-#endif
