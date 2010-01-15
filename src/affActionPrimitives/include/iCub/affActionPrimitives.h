47bfe95d43a3fe94d17aca8af2e1eaa1fb3d0594
Ugo Pattacini <ugo.pattacini@iit.it>
2010-01-15 10:55:17 +0000

    No commit message
    svn path=/trunk/iCub/; revision=3006

--- src/affActionPrimitives/include/iCub/affActionPrimitives.h
+++ src/affActionPrimitives/include/iCub/affActionPrimitives.h
@@ -466,7 +466,9 @@ public:
     * @param ctrl the cartesian interface.
     * @return true/false on success/fail. 
     *  
-    * \note Useful to access solver's options (\ref iKinSlv).
+    * \note Useful to access solver's options through the <a 
+    *       href="http://eris.liralab.it/yarpdoc/dd/de6/classyarp_1_1dev_1_1ICartesianControl.html">ICartesianControl</a>
+    *       interface.
     */
     virtual bool getCartesianIF(yarp::dev::ICartesianControl *&ctrl);
 
