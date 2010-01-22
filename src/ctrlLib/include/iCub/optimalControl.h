e8e11be3254dc151085a2ca929cf91544efcb85c
Ugo Pattacini <ugo.pattacini@iit.it>
2010-01-22 16:05:30 +0000

    No commit message
    svn path=/trunk/iCub/; revision=3283

--- src/ctrlLib/include/iCub/optimalControl.h
+++ src/ctrlLib/include/iCub/optimalControl.h
@@ -13,26 +13,17 @@
  *
  * Given the linear system:
  * \f[ x_{i+1} = A x_i + B u_i \ , \ i=0,1,\ldots,N-1 \f]
- * with the known initial state \f$ x_0 = \hat{x} \f$ , and the 
- * quadratic cost \f$ J \f$ : 
+ * with the known initial state \f$ x_0 = \hat{x} \f$, and the quadratic cost $J$:
  * \f[ J = \sum^{N-1}_{i=0} \left[ x^\top_i V x_i + u^\top_i P u_i \right] + x^\top_N V_N x_N \f]
- * with \f$ V=V^\top \geq 0 \f$ , \f$ V_N=V^\top_N \geq 0 \f$ , 
- * \f$ P=P^\top>0 \f$ , the problem is to find the sequence of 
- * optimal controls \f$ u^\circ_0, \ldots, u^\circ_{N-1} \f$ 
- * minimizing \f$ J \f$ . The optimal controls can be found via 
- * dynamic programming, and a closed form solution can be found. 
- * At time instant \f$ i \f$ the optimal cost-to-go and control 
- * are: 
- * \f[ \begin{align*}
- * J^\circ(x_{i}) &= x^\top_{i} \ T_{i} \ x_{i} \\
- * u^\circ_{i} &= - L_i \ x_{i}
- * \end{align*} \f]
+ * with \f$ V=V^\top \geq 0 \f$, \f$ V_N=V^\top_N \geq 0 \f$, \f$ P=P^\top>0 \f$, the problem is to find the sequence of 
+ * optimal controls \f$ u^\circ_0, \ldots, u^\circ_{N-1} \f$ minimizing \f$J\f$. 
+ * The optimal controls can be found via dynamic programming, and a closed form solution can be found.
+ * At time instant \f$i\f$ the optimal cost-to-go and control are:
+ * \f[ J^\circ(x_{i}) = x^\top_{i} \ T_{i} \ x_{i} \, u^\circ_{i} = - L_i \ x_{i} \f]
  * where \f$ L_i \f$ is:
  * \f[ L_i = (P+B^\top T_{i+1} B)^{-1} B^\top T_{i+1} A \f]
  * whilst \f$ T_i \f$ is computed after the ``discrete time algebraic Riccati equation'':
- * \f[ \begin{align*}T_N &= V_N \\
- * T_i &= V + A^\top [ T_{i+1} - T_{i+1} B (P+B^\top T_{i+1} B)^{-1} B^\top T_{i+1} ] A
- * \end{align*} \f]
+ * \f[ T_N = V_N \ , \ T_i = V + A^\top [ T_{i+1} - T_{i+1} B (P+B^\top T_{i+1} B)^{-1} B^\top T_{i+1} ] A \f]
  *
  *
  * \section code_example_sec Example
@@ -106,14 +97,14 @@ public:
              const yarp::sig::Matrix &_VN, bool verb=false);
 
 	 /**
-     * Get stored L_i matrix. Call this function only after doRiccati()
+     * Get stored L_i matrix; call this function only after solveRiccati()
      * 
      * @param step The time index of the i-th matrix
      */
 	 yarp::sig::Matrix L(int step);
 
 	 /**
-     * Get stored T_i matrix. Call this function only after doRiccati()
+     * Get stored T_i matrix; call this function only after solveRiccati()
      * 
      * @param step The time index of the i-th matrix
      */
@@ -134,7 +125,7 @@ public:
 
 	 /**
      * Solve recursively discrete algebraic Riccati equation (DARE) 
-     * and stores matrices Ti and Li, where i=0,...,N-1 is the time 
+     * and stores matrices Ti and Li, where i=0:N-1 is the time 
      * index 
      * 
      * @param steps The number N of steps of the finite horizon controller
@@ -159,7 +150,8 @@ public:
 	 void doLQcontrol(int step, const yarp::sig::Vector &x, yarp::sig::Vector &ret);
 
 	 /**
-     * Enable or disable verbose feedback (i.e. printing additional information) 
+     * Enable or disable verbose feedback(that is, printing 
+     * additional information) 
      * 
      * @param verb Flag for verbose mode
      */
