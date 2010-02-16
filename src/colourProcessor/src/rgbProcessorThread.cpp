c37b5b169e06682e64d1893871427f04fc4ed9f3
Francesco Rea <francesco.rea@iit.it>
2010-02-16 07:37:06 +0000

    switched back to the correct sequence of substractions
    svn path=/trunk/iCub/; revision=3717

--- src/colourProcessor/src/rgbProcessorThread.cpp
+++ src/colourProcessor/src/rgbProcessorThread.cpp
@@ -299,7 +299,7 @@ void rgbProcessorThread::colourOpponency(){
     ippiSub_8u_C1RSfs(bluePlane_ippi8u,psb,yellowPlane_ippi8u_f,psb,blueYellow_yarp->getRawImage(),blueYellow_yarp->getRowSize(),srcsize,0);*/
 
     
-
+    /*
 	//4.1 subtract the red from the green to obtain R+G-
 	ippiSub_32f_C1R(redPlane_ippi32,psb2,greenPlane_ippi32_f,psb2,greenRed_ippi32,psb2,srcsize);
 	//ippiSub_8u_C1RSfs(redGreen_ippi,psb,greenPlane_ippi_f,psb,redGreen_ippi,psb,srcsize,-1);
@@ -315,8 +315,23 @@ void rgbProcessorThread::colourOpponency(){
 	//4.5. subtract the yellow from the blue in order to get B+Y-
 	ippiSub_32f_C1R(bluePlane_ippi32,psb2,yellowPlane_ippi32_f,psb2,blueYellow_ippi32,psb2,srcsize);
 	//ippiSub_8u_C1RSfs(yellowPlane_ippi_f,psb,blueYellow_ippi,psb,blueYellow_ippi,psb,srcsize,-1);
+    */
 
-    
+    //4.1 subtract the red from the green to obtain R+G-
+	ippiSub_32f_C1R(redPlane_ippi32,psb2,greenPlane_ippi32_f,psb2,redGreen_ippi32,psb2,srcsize);
+	//ippiSub_8u_C1RSfs(redGreen_ippi,psb,greenPlane_ippi_f,psb,redGreen_ippi,psb,srcsize,-1);
+	//4.2 subtract the green from the red to obtain G+R-
+	ippiSub_32f_C1R(greenPlane_ippi32,psb2,redPlane_ippi32_f,psb2,greenRed_ippi32,psb2,srcsize);
+	//ippiSub_8u_C1RSfs(greenRed_ippi,psb,redPlane_ippi_f,psb,greenRed_ippi,psb,srcsize,-1);
+	//4.3 risht shift;
+	//ippiRShiftC_32s_C1IR(1,greenPlane_ippi32_f,psb2,srcsize);
+	//ippiRShiftC_32s_C1IR(1,redPlane_ippi32_f,psb2,srcsize);
+	//4.4 sum the filtered red and filtered green to get the filtered yellow
+	ippiAdd_32f_C1R(redPlane_ippi32_f,psb2,greenPlane_ippi32_f,psb2,yellowPlane_ippi32_f,psb2,srcsize);
+	//ippiAdd_8u_C1RSfs(redPlane_ippi_f,psb,greenPlane_ippi_f,psb,yellowPlane_ippi_f,psb,srcsize,-1);
+	//4.5. subtract the yellow from the blue in order to get B+Y-
+	ippiSub_32f_C1R(bluePlane_ippi32,psb2,yellowPlane_ippi32_f,psb2,blueYellow_ippi32,psb2,srcsize);
+	//ippiSub_8u_C1RSfs(yellowPlane_ippi_f,psb,blueYellow_ippi,psb,blueYellow_ippi,psb,srcsize,-1);
 
 
     /*for(int i=0;i< width*height; i++){
