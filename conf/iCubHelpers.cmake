d4a280960a5efca913c05890223cfb30c18f37f3
Lorenzo Natale <lorenzo.natale@iit.it>
2010-03-13 12:00:23 +0000

    better organization of directories, for public include in libraries
    svn path=/branches/iCub2.0/; revision=4141

--- conf/iCubHelpers.cmake
+++ conf/iCubHelpers.cmake
@@ -7,8 +7,8 @@
 
 # Export library to be used from external programs
 # @param name: name of the target to be export
-# @param headers: directory that contains public include files
-# @param header_files: public header files (to be installed)
+# @param public_inc_dirs: the directory that contains public header files, copied verbatim at 
+# install time to INSTALL_PREFIX/include/iCub
 # 
 # Variables created: 
 # ${name}_INCLUDE_DIRS: cache variable containing header files directory
@@ -16,19 +16,22 @@
 #
 # -add ${name} to group icub_targets
 # -append ${name} to our own local list of exported targets ICUB_TARGETS
-# -install rule to copy the list of ${header_files} to include/iCub postfix
+# -install rule to copy all header files in public_include_dir
 # -append export rules to a file in ${PROJECT_BINARY_DIR}
-macro(icub_export_library name headers header_files)
+macro(icub_export_library name public_inc_dirs)
 
 set(ICUB_EXPORTBUILD_FILE icub-export-build.cmake)
 
-set(${name}_INCLUDE_DIRS ${headers} CACHE STRING "include directories")
+set(${name}_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}/include/${public_inc_dirs}" CACHE STRING "include directories")
 set(${name}_LIBRARIES ${name} CACHE STRING "library")
 
 set(ICUB_TARGETS ${ICUB_TARGETS} "${name}" CACHE STRING "" FORCE)
 
 install(TARGETS ${name} DESTINATION ${CMAKE_INSTALL_PREFIX}/lib EXPORT icub-targets)
-install(FILES ${header_files} DESTINATION include/iCub)
+install(DIRECTORY "${PROJECT_SOURCE_DIR}/include/${public_inc_dirs}" DESTINATION include/iCub
+  FILES_MATCHING PATTERN "*.h"
+  PATTERN ".svn" EXCLUDE)
+
 export(TARGETS ${name} APPEND FILE ${CMAKE_BINARY_DIR}/${ICUB_EXPORTBUILD_FILE})
 
 endmacro(icub_export_library)
--- src/examples/lib1/src/Lib1.cpp
+++ src/examples/lib1/src/Lib1.cpp
@@ -1,13 +1,13 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
-#include <iCub/Lib0.h> //dependency
-#include <iCub/Lib1.h>
+#include <iCub/lib0/Lib0.h> //dependency
+#include <iCub/lib1/Lib1.h>
 
 #include <stdio.h>
 
 void Lib1::lib1() {
     Lib0 lib0;
-    printf("Warning using lib1\n");
-    printf("We need lib0\n");
+    printf("Using lib1\n");
+    printf("We also need lib0\n");
     lib0.lib0();
 }
