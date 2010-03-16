9ec41c08c1fa34da159341642225063afd16e30d
Lorenzo Natale <lorenzo.natale@iit.it>
2010-03-16 17:16:15 +0000

    No commit message
    svn path=/branches/iCub2.0/; revision=4169

--- conf/iCubFindDependencies.cmake
+++ conf/iCubFindDependencies.cmake
@@ -1,16 +1,44 @@
 # Copyright: (C) 2009 RobotCub Consortium
-# Authos: Lorenzo Natale
+# Authors: Lorenzo Natale
 # CopyPolicy: Released under the terms of the GNU GPL v2.0.
 # Based on code from yarp.
+
+macro(checkandset_dependency package)
+    if (${package}_FOUND)
+        set(ICUB_HAS_${package} true)
+        message(STATUS "found ${package}")
+    endif (${package}_FOUND)
+endmacro (checkandset_dependency)
 
 message(STATUS "Detecting required libraries")
 
 find_package(YARP)
+find_package(GSL)
+find_package(GtkMM)
+find_package(Qt3)
+find_package(OPENCV)
+
+find_package(OpenGL)
+find_package(ODE)
+find_package(SDL)
 
 message(STATUS "I have found the following libraries:")
 
-if (YARP_FOUND)
-  set(ICUB_HAS_YARP true)
-  message(STATUS "found YARP")
-endif (YARP_FOUND)
+checkandset_dependency(YARP)
+checkandset_dependency(GSL)
+checkandset_dependency(GtkMM)
+checkandset_dependency(Qt3)
+checkandset_dependency(OPENCV)
+checkandset_dependency(OpenGL)
+checkandset_dependency(ODE)
+checkandset_dependency(SDL)
+
+
+
+if (YARP_HAS_MATH)
+    set(ICUB_HAS_YARPMATH true)
+    message(STATUS "found yarp math library")
+endif (YARP_HAS_MATH)
+
+
 
