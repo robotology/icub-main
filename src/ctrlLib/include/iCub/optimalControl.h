9f01259255d2d1fa09af35cd0f44ffeb503c3546
Serena Ivaldi <arwen.h@gmail.com>
2010-03-25 14:22:15 +0000

    added comments
    svn path=/trunk/iCub/; revision=4283

--- src/ctrlLib/include/iCub/optimalControl.h
+++ src/ctrlLib/include/iCub/optimalControl.h
@@ -13,15 +13,48 @@
  *
  * Given the linear system:
  * \f[ x_{i+1} = A x_i + B u_i \ , \ i=0,1,\ldots,N-1 \f]
- * with the known initial state \f$ x_0 = \hat{x} \f$, and the 
- * quadratic cost \f$ J \f$: 
- * \f[ J = \sum^{N-1}_{i=0} \left[ x^{\top}_i V x_i + u^{\top}_i P u_i \right] + x^{\top}_N V_N x_N \f]
- * with \f$ V=V^\top \geq 0 \f$, \f$ V_N=V^{\top}_N \geq 0 \f$, \f$ P=P^\top>0 \f$, the problem is to find the sequence of 
- * optimal controls \f$ u^\circ_0, \ldots, u^\circ_{N-1} \f$ minimizing \f$ J \f$. 
+ * with the known initial state \f$ x_0 = \hat{x} \f$, and the quadratic cost $J$:
+ * \f[ J = \sum^{N-1}_{i=0} \left[ x^\top_i V x_i + u^\top_i P u_i \right] + x^\top_N V_N x_N \f]
+ * with \f$ V=V^\top \geq 0 \f$, \f$ V_N=V^\top_N \geq 0 \f$, \f$ P=P^\top>0 \f$, the problem is to find the sequence of 
+ * optimal controls \f$ u^\circ_0, \ldots, u^\circ_{N-1} \f$ minimizing \f$J\f$. 
  * The optimal controls can be found via dynamic programming, and a closed form solution can be found.
- * At time instant \f$ i \f$ the optimal cost-to-go and control are:
- * \f[ \begin{array}{l} J^\circ \left(x_{i}\right) = x^\top_{i} T_{i}x_{i} \\ u^\circ_{i} = - L_i x_{i} \end{array} \f]
- * where: \f[ \begin{array}{l} L_i = \left(P+B^\top T_{i+1} B\right)^{-1} B^\top T_{i+1} A \\ T_N = V_N \\ T_i = V + A^\top \left[ T_{i+1} - T_{i+1} B \left(P+B^\top T_{i+1} B\right)^{-1} B^\top T_{i+1} \right] A \end{array} \f]
+ * At time instant \f$i\f$ the optimal cost-to-go and control are:
+ * \f[\begin{align*}
+ * J^\circ(x_{i}) &= x^\top_{i} \ T_{i} \ x_{i} \\
+ * u^\circ_{i} &= - L_i \ x_{i}
+ * \end{align*}\f]
+ * where \f$ L_i \f$ is:
+ * \f[ L_i = (P+B^\top T_{i+1} B)^{-1} B^\top T_{i+1} A \f]
+ * whilst \f$ T_i \f$ is computed after the ``discrete time algebraic Riccati equation'':
+ * \f[ \begin{align*}T_N &= V_N \\
+ * T_i &= V + A^\top [ T_{i+1} - T_{i+1} B (P+B^\top T_{i+1} B)^{-1} B^\top T_{i+1} ] A
+ * \end{align*} \f]
+ *
+ *
+ * \section example_sec Example
+ *
+ * Given the linear system:
+ * \f[ x_{i+1} = A x_i + B u_i \ , \ i=0,1,\ldots,N-1 \f]
+ * with the known initial state \f$ x_0 = \hat{x} \f$ , and the 
+ * quadratic cost \f$ J \f$ : 
+ * \f[ J = \sum^{N-1}_{i=0} \left[ x^\top_i V x_i + u^\top_i P u_i \right] + x^\top_N V_N x_N \f]
+ * with \f$ V=V^\top \geq 0 \f$ , \f$ V_N=V^\top_N \geq 0 \f$ , 
+ * \f$ P=P^\top>0 \f$ , the problem is to find the sequence of 
+ * optimal controls \f$ u^\circ_0, \ldots, u^\circ_{N-1} \f$ 
+ * minimizing \f$ J \f$ . The optimal controls can be found via 
+ * dynamic programming, and a closed form solution can be found. 
+ * At time instant \f$ i \f$ the optimal cost-to-go and control 
+ * are: 
+ * \f[ \begin{align*}
+ * J^\circ(x_{i}) &= x^\top_{i} \ T_{i} \ x_{i} \\
+ * u^\circ_{i} &= - L_i \ x_{i}
+ * \end{align*} \f]
+ * where \f$ L_i \f$ is:
+ * \f[ L_i = (P+B^\top T_{i+1} B)^{-1} B^\top T_{i+1} A \f]
+ * whilst \f$ T_i \f$ is computed after the ``discrete time algebraic Riccati equation'':
+ * \f[ \begin{align*}T_N &= V_N \\
+ * T_i &= V + A^\top [ T_{i+1} - T_{i+1} B (P+B^\top T_{i+1} B)^{-1} B^\top T_{i+1} ] A
+ * \end{align*} \f]
  *
  *
  * \section code_example_sec Example
@@ -142,14 +175,13 @@ public:
      * Compute the LQ feedback control, in the form: u= - L(i) * x 
      * 
      * @param step The time index i
-	 * @param x The state vector
-	 * @param ret The control vector
+	  * @param x The state vector
+	  * @param ret The control vector
      */
 	 void doLQcontrol(int step, const yarp::sig::Vector &x, yarp::sig::Vector &ret);
 
 	 /**
-     * Enable or disable verbose feedback(that is, printing 
-     * additional information) 
+     * Enable or disable verbose feedback (that is, printing additional information) 
      * 
      * @param verb Flag for verbose mode
      */
