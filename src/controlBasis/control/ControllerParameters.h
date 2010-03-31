9efa76ec7bf8f02a894a442ed94713f9ff661cf9
Stephen Hart <stephen.hart@nasa.gov>
2010-03-31 10:23:12 +0000

    No commit message
    svn path=/trunk/iCub/; revision=4354

--- src/controlBasis/control/ControllerParameters.h
+++ src/controlBasis/control/ControllerParameters.h
@@ -1,79 +1,95 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-      
 /**
-  * This is a simple helper class that enumertes the resource names to be used
-  * in a control basis controller.  The resources include a sensor value,
-  * a reference value (that is compared to the sensor value), a potential function
-  * that evaluates a positive scalar "potential" value based on the sensor and
-  * reference signals, and an effector that can be moved to reduce the potential
-  * value.
-  **/
+ * This is a simple helper class that enumertes the resource names to be used
+ * in a control basis controller.  The resources include a sensor value,
+ * a reference value (that is compared to the sensor value), a potential function
+ * that evaluates a positive scalar "potential" value based on the sensor and
+ * reference signals, and an effector that can be moved to reduce the potential
+ * value.
+ **/
 #ifndef _CONTROLLER_PARAMETERS__H_
 #define _CONTROLLER_PARAMETERS__H_
 
 #include <iostream>
 
 namespace CB {
-
-  class ControllerParameters {
     
-  public:
-
-    /**
-     * the sensor string
-     **/
-    std::string Sensor;
+    class ControllerParameters {
+    
+    public:
         
-    /**
-     * the reference string (leave as "" if there is no reference)
-     **/
-    std::string Reference;
+        /**
+         * the sensor string
+         **/
+        std::string Sensor;
         
-    /**
-     * the effector string
-     **/
-    std::string Effector;
+        /**
+         * the reference string (leave as "" if there is no reference)
+         **/
+        std::string Reference;
+        
+        /**
+         * the effector string
+         **/
+        std::string Effector;
+        
+        /**
+         * the potential function string
+         **/
+        std::string PotentialFunction;
+        
+        /**
+         * a (proportional) gain for the controller output
+         **/
+        double Gain;
+        
+        /**
+         * Constructor
+         **/
+        ControllerParameters() {
+            Sensor = "";
+            Effector = "";
+            Reference = "";
+            PotentialFunction = "";
+            Gain = 1.0;
+        }
+        
+        ControllerParameters(std::string s, std::string r, std::string pf, std::string e, double g=1.0) {
+            Sensor = s;
+            Effector = e;
+            Reference = r;
+            PotentialFunction = pf;
+            Gain = g;
+        }
+
+        ControllerParameters(std::string s, std::string pf, std::string e, double g=1.0) {
+            Sensor = s;
+            Effector = e;
+            Reference = "";
+            PotentialFunction = pf;
+            Gain = g;
+        }
+        
+        /** 
+         * Destructor
+         **/
+        ~ControllerParameters() { }
+        
+        /**
+         * display function
+         **/
+        void display() {
+            std::cout << std::endl << std::endl;
+            std::cout << "Controller Info:" << std::endl << "------------------" << std::endl;
+            std::cout << "Sensor: " << Sensor.c_str() << std::endl;
+            if(Reference != "")  std::cout << "Reference: " << Reference.c_str() << std::endl;
+            std::cout << "Potential Function: " << PotentialFunction.c_str() << std::endl;
+            std::cout << "Effector: " << Effector.c_str() << std::endl;
+            std::cout << "Gain: " << Gain << std::endl << std::endl;
+        }
+        
+    };
     
-    /**
-     * the potential function string
-     **/
-    std::string PotentialFunction;
-    
-    /**
-     * a (proportional) gain for the controller output
-     **/
-    double Gain;
-    
-    /**
-     * Constructor
-     **/
-    ControllerParameters() {
-      Sensor = "";
-      Effector = "";
-      Reference = "";
-      PotentialFunction = "";
-      Gain = 1.0;
-    }
-    
-    /** 
-     * Destructor
-     **/
-    ~ControllerParameters() { }
-    
-    /**
-     * display function
-     **/
-    void display() {
-      std::cout << std::endl << std::endl;
-      std::cout << "Controller Info:" << std::endl << "------------------" << std::endl;
-      std::cout << "Sensor: " << Sensor.c_str() << std::endl;
-      if(Reference != "")  std::cout << "Reference: " << Reference.c_str() << std::endl;
-      std::cout << "Potential Function: " << PotentialFunction.c_str() << std::endl;
-      std::cout << "Effector: " << Effector.c_str() << std::endl;
-      std::cout << "Gain: " << Gain << std::endl << std::endl;
-    }
-    
-  };
-  
 }
 
 #endif
