7b833ae23bcd858fa5d8acbdba6beb338dfdb384
Ugo Pattacini <ugo.pattacini@iit.it>
2010-05-11 09:13:02 +0000

    No commit message
    svn path=/trunk/iCub/; revision=4775

--- src/iKinGazeCtrl/src/main.cpp
+++ src/iKinGazeCtrl/src/main.cpp
@@ -301,14 +301,13 @@ public:
 
     void waitPart(const Property &partOpt, const double ping_robot_tmo)
     {
-        string robotName=const_cast<Property&>(partOpt).find("robot").asString().c_str();
-        string partName=const_cast<Property&>(partOpt).find("part").asString().c_str();
-        string portName="/"+robotName+"/"+partName+"/state:o";
+        string portName=const_cast<Property&>(partOpt).find("remote").asString().c_str();
+        portName+="/state:o";
         double t0=Time::now();
     
         while (Time::now()-t0<ping_robot_tmo)
         {   
-            cout << "Checking if " << portName << " port is active ... " << endl;
+            cout << "Checking if " << portName << " port is active ... ";
         
             NameClient &nic=NameClient::getNameClient();
             Address address=nic.queryName(portName.c_str());
@@ -416,13 +415,11 @@ public:
 
         string remoteHeadName="/"+robotName+"/"+partName;
         string localHeadName="/"+ctrlName+"/"+partName;
-        optHead.put("robot",robotName.c_str());
         optHead.put("remote",remoteHeadName.c_str());
         optHead.put("local",localHeadName.c_str());
 
         string remoteTorsoName="/"+robotName+"/"+torsoName;
         string localTorsoName=localHeadName+"/"+torsoName;
-        optTorso.put("robot",robotName.c_str());
         optTorso.put("remote",remoteTorsoName.c_str());
         optTorso.put("local",localTorsoName.c_str());
 
