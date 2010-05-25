453c7daae8b6918510363d66a70cf4b1959589cb
Francesco Rea <francesco.rea@iit.it>
2010-05-25 11:59:14 +0000

    No commit message
    svn path=/trunk/iCub/; revision=4975

---
+++ src/colourProcessor/include/iCub/imageReaderThread.h
@@ -0,0 +1,214 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#ifndef _IMAGEREADERTHREAD_H_
+#define _IMAGEREADERTHREAD_H_
+
+//YARP include
+//os section
+#include <yarp/os/Network.h>
+#include <yarp/os/Port.h>
+#include <yarp/os/Thread.h>
+#include <yarp/os/RateThread.h>
+#include <yarp/os/Bottle.h>
+#include <yarp/os/Time.h>
+#include <yarp/os/Vocab.h>
+#include <yarp/os/BufferedPort.h>
+//sig section
+#include <yarp/sig/Image.h>
+
+#include <string>
+
+//IPP include
+#include <ipp.h>
+
+//const double TIMEOUT=0.1;
+//const double STOP_TIME=3;
+const int THREAD_RATE_IMAGE=30;
+
+class imageReaderThread: public yarp::os::RateThread
+{
+private:
+    /**
+    * width of images
+    */
+    int width;
+    /**
+    * height of images
+    */
+    int height;
+    /**
+    * IppiSize reference to the dimension of the input image
+    */
+    IppiSize srcsize;
+    /**
+    * port where the input image is read from
+    */
+    yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelRgb> > inputPort;
+    /**
+    * port where the red plane of the image is streamed
+    */
+    yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelMono> > redPort;
+    /**
+    * port where the green plane of the image is streamed
+    */
+    yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelMono> > greenPort;
+    /**
+    * port where the blue plane of the image is streamed
+    */
+    yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelMono> > bluePort;
+    /**
+    * port where the difference of gaussian R+G- is streamed
+    */
+    yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelMono> > rgPort;
+    /**
+    * port where the difference of gaussian G+R- is streamed
+    */
+    yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelMono> > grPort;
+    /**
+    * port where the difference of gaussian B+Y- of the image is streamed
+    */
+    yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelMono> > byPort;
+    /**
+    * port where the yellow plane of the image is streamed
+    */
+    yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelMono> > yellowPort;
+    /**
+    * port where the ychannel of the image is streamed
+    */
+    yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelMono> > yPort;
+    /**
+    * port where the uchannel of the image is streamed
+    */
+    yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelMono> > uPort;
+    /**
+    * port where the vchannel plane of the image is streamed
+    */
+    yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelMono> > vPort;
+     /**
+    * port where the uvchannel of the input image is streamed
+    */
+    yarp::os::BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelMono> > uvPort;
+    /***
+    * temporary image mono for processing
+    */
+    yarp::sig::ImageOf<yarp::sig::PixelMono>* tmp;
+    /**
+    * temporary psb for processing
+    */
+    int psb;
+    /**
+    * flag that indicates when the reinitiazation has already be done
+    */
+    bool reinit_flag;
+    /**
+    * input image
+    */
+    yarp::sig::ImageOf<yarp::sig::PixelRgb> *img; 
+    
+    
+    //_______________ private method  __________________________
+    /**
+    * function that streams the images out on the ports
+    */
+    void outPorts();
+    /**
+    * name of the module and rootname of the connection
+    */
+    std::string name;
+    
+public:
+    /**
+    * constructor of the thread
+    */
+    imageReaderThread();//:RateThread(THREAD_RATE){};
+    /**
+    * constructor of the thread
+    */
+    //processorThread(Property &op);
+    /**
+    * destructor of the thread
+    */
+    ~imageReaderThread();
+    /**
+    *	initialization of the thread 
+    */
+    bool threadInit();
+    /**
+    * active loop of the thread
+    */
+    void run();
+    /**
+    *	releases the thread
+    */
+    void threadRelease();
+
+    /**
+    * function called when the module is poked with an interrupt command
+    */
+    void interrupt();
+    /**
+    * function that reinitiases some attributes of the class
+    */
+    void reinitialise(int width, int height);
+    /**
+    * function that gives reference to the name of the module
+    * @param name of the module
+    */
+    void setName(std::string name);
+    /**
+    * function that returns the name of the module
+    * @param str string to be added
+    * @return name of the module
+    */
+    std::string getName(const char* str);
+
+    //______ public attributes________
+    /**
+    * input image
+    */
+    yarp::sig::ImageOf<yarp::sig::PixelRgb> *inputImg;
+    /**
+    *yarp mono image of the red channel
+    */
+    yarp::sig::ImageOf<yarp::sig::PixelMono>* redPlane; 
+    /**
+    *yarp mono image of the green channel
+    */
+    yarp::sig::ImageOf<yarp::sig::PixelMono>* greenPlane; 
+    /**
+    *yarp mono image of the blue channel
+    */
+    yarp::sig::ImageOf<yarp::sig::PixelMono>* bluePlane; 
+    /**
+    * yarp image for Opponency Map R+G-
+    */
+    yarp::sig::ImageOf<yarp::sig::PixelMono>* redGreen_yarp;
+    /**
+    * yarp image for Opponency Map G+R-
+    */
+    yarp::sig::ImageOf<yarp::sig::PixelMono>* greenRed_yarp;
+    /**
+    * yarp image for Opponency Map B+Y-
+    */
+    yarp::sig::ImageOf<yarp::sig::PixelMono>* blueYellow_yarp;
+    /**
+    *yarp mono image of the y channel (intensity information)
+    */
+    yarp::sig::ImageOf<yarp::sig::PixelMono>* yPlane; 
+    /**
+    *yarp mono image of the u channel (chrominance information)
+    */
+    yarp::sig::ImageOf<yarp::sig::PixelMono>* uPlane; 
+    /**
+    *yarp mono image of the v channel (chrominance information)
+    */
+    yarp::sig::ImageOf<yarp::sig::PixelMono>* vPlane;
+    /**
+    *yarp mono image of the uv channel (chrominance information)
+    */
+    yarp::sig::ImageOf<yarp::sig::PixelMono>* uvPlane;
+};
+
+#endif //_IMAGEREADERTHREAD_H_
+
+//----- end-of-file --- ( next line intentionally left blank ) ------------------
---
+++ src/colourProcessor/src/imageReaderThread.cpp
@@ -0,0 +1,228 @@
+#include <iCub/imageReaderThread.h>
+#include <iCub/convert_bitdepth.h>
+#include <ipps.h>
+#include <iostream>
+
+
+using namespace yarp::sig;
+using namespace yarp::os;
+using namespace std;
+
+imageReaderThread::imageReaderThread():RateThread(THREAD_RATE_IMAGE)
+{
+    reinit_flag=false;
+        
+    inputImg=0;
+    img=0;
+
+    redPlane=0;
+    greenPlane=0;
+    bluePlane=0;
+
+    redGreen_yarp=0;
+    greenRed_yarp=0;
+    blueYellow_yarp=0;
+
+    yPlane=0;
+    uPlane=0;
+    vPlane=0;
+    uvPlane=0;
+}
+
+imageReaderThread::~imageReaderThread()
+{
+    delete img;
+    delete inputImg;
+
+    delete redPlane;
+    delete greenPlane;
+    delete bluePlane;
+
+    delete redGreen_yarp;
+    delete greenRed_yarp;
+    delete blueYellow_yarp;
+
+    delete yPlane;
+    delete uPlane;
+    delete vPlane;
+    delete uvPlane;
+}
+
+/*processorThread::processorThread(Property &op):processorThread(){
+        
+        
+}*/
+
+void imageReaderThread::reinitialise(int width, int height){
+    srcsize.width=width;
+    srcsize.height=height;
+
+    inputImg=new ImageOf<PixelRgb>;
+    inputImg->resize(width,height);
+}
+
+void imageReaderThread::setName(std::string str){
+    this->name=str; 
+}
+
+
+std::string imageReaderThread::getName(const char* p){
+    string str(name);
+    str.append(p);
+    printf("name: %s", name.c_str());
+    return str;
+}
+
+
+bool imageReaderThread::threadInit(){
+    printf("Thread initialisation.. \n");    
+    inputPort.open(getName("image:i").c_str());
+        
+    redPort.open(getName("red:o").c_str());
+    greenPort.open(getName("green:o").c_str());
+    bluePort.open(getName("blue:o").c_str());
+
+    rgPort.open(getName("rg:o").c_str());
+    grPort.open(getName("gr:o").c_str());
+    byPort.open(getName("by:o").c_str());
+
+    yPort.open(getName("ychannel:o").c_str());
+    uPort.open(getName("uchannel:o").c_str());
+    vPort.open(getName("vchannel:o").c_str());
+    uvPort.open(getName("uvchannel:o").c_str());
+
+   
+
+    return true;
+}
+
+/**
+* function called when the module is poked with an interrupt command
+*/
+void imageReaderThread::interrupt(){
+    inputPort.interrupt();
+    
+    redPort.interrupt();
+    greenPort.interrupt();
+    bluePort.interrupt();
+
+    rgPort.interrupt();
+    grPort.interrupt();
+    byPort.interrupt();
+    
+    yPort.interrupt();
+    uPort.interrupt();
+    vPort.interrupt();
+    uvPort.interrupt();
+}
+
+
+void imageReaderThread::run(){
+    img = this->inputPort.read(false);
+    if(0==img)
+        return;
+
+    if(!reinit_flag){
+        
+	    srcsize.height=img->height();
+	    srcsize.width=img->width();
+        reinitialise(img->width(), img->height());
+        reinit_flag=true;
+        
+
+    }
+
+    //copy the inputImg into a buffer
+    ippiCopy_8u_C3R(img->getRawImage(), img->getRowSize(),inputImg->getRawImage(), inputImg->getRowSize(),srcsize);
+   
+    outPorts();   
+}
+   
+void imageReaderThread::outPorts(){
+    //port2.prepare() = *img;	
+    //rgb section
+    if((redPlane!=0)&&(redPort.getOutputCount())){
+        redPort.prepare() = *(redPlane);		
+        redPort.write();
+    }
+    if((bluePlane!=0)&&(bluePort.getOutputCount())){
+        bluePort.prepare() = *(bluePlane);		
+        bluePort.write();
+    }
+    if((greenPlane!=0)&&(greenPort.getOutputCount())){
+        greenPort.prepare() = *(greenPlane);		
+        greenPort.write();
+    }
+    
+    if((yPlane!=0)&&(yPort.getOutputCount())){
+        yPort.prepare() = *(yPlane);		
+        yPort.write();
+    }
+    if((uPlane!=0)&&(uPort.getOutputCount())){
+        uPort.prepare() = *(uPlane);		
+        uPort.write();
+    }
+    if((vPlane!=0)&&(vPort.getOutputCount())){
+        vPort.prepare() = *(vPlane);		
+        vPort.write();
+    }
+    if((uvPlane!=0)&&(uvPort.getOutputCount())){
+        uvPort.prepare() = *(uvPlane);		
+        uvPort.write();
+    }
+    
+    //colour opponency section
+    if((redGreen_yarp!=0)&&(rgPort.getOutputCount())){
+        rgPort.prepare()=*(redGreen_yarp);
+        rgPort.write();
+    }
+    if((greenRed_yarp!=0)&&(grPort.getOutputCount())){
+        grPort.prepare()=*(greenRed_yarp);
+        grPort.write();
+    }
+    if((blueYellow_yarp!=0)&&(byPort.getOutputCount())){
+        byPort.prepare()=*(blueYellow_yarp);
+        byPort.write();
+    }
+    
+}
+
+void imageReaderThread::threadRelease(){
+   
+    printf("Thread releasing.. \n");
+    printf("input port closing .... \n");
+    inputPort.close();
+    
+    printf("red channel port closing .... \n");
+    redPort.close();
+    printf("green channel port closing .... \n");
+    greenPort.close();
+    printf("blue channel port closing .... \n");
+    bluePort.close();
+
+    printf("R+G- colourOpponency port closing .... \n");
+    rgPort.close();
+    printf("G+R- colourOpponency port closing .... \n");
+    grPort.close();
+    printf("B+Y- colourOpponency port closing .... \n");
+    byPort.close();
+
+    printf("intensity channel port closing .... \n");
+    yPort.close();
+    printf("chrominance channel port closing .... \n");
+    uPort.close();
+    vPort.close();
+    uvPort.close();
+}
+
+
+
+/*void imageReaderThread::setInputImage(ImageOf<PixelRgb>* inputImage){
+    this->img=inputImage;
+    this->width=inputImage->width();
+    this->height=inputImage->height();
+    reinitialise();
+}*/
+
+
+//----- end-of-file --- ( next line intentionally left blank ) ------------------
