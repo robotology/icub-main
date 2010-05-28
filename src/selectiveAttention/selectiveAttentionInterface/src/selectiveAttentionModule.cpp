efaf96e79de2dea423156377dab5c5d279f6eda8
Francesco Rea <francesco.rea@iit.it>
2010-05-28 07:48:03 +0000

    added cstdlib and cstring in the selectiveAttentionModule
    svn path=/trunk/iCub/; revision=5046

--- src/selectiveAttention/selectiveAttentionInterface/src/selectiveAttentionModule.cpp
+++ src/selectiveAttention/selectiveAttentionInterface/src/selectiveAttentionModule.cpp
@@ -1,6 +1,8 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 #include <iCub/selectiveAttentionModule.h>
 #include <iCub/graphicThread.h>
+#include <cstring>
+#include <cstdlib>
 
 using namespace yarp::os;
 using namespace yarp::sig;
