149d22d7d2513ed13c80d7a397585b9f60f7223f
Francesco Nori <francesco.nori@iit.it>
2010-06-01 16:11:54 +0000

    fixing ace, removing the use of yarp string, including stdlib.h
    svn path=/trunk/iCub/; revision=5165

--- src/drummingEPFL/drumGenerator/DrumGeneratorModule.cpp
+++ src/drummingEPFL/drumGenerator/DrumGeneratorModule.cpp
@@ -1,7 +1,7 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
 #include "DrumGeneratorModule.h"
-
+#include <stdlib.h>
 #define DEBUG 0
 
 //#include <ppEventDebugger.h>
@@ -468,7 +468,7 @@ bool generatorThread::init(Searchable &s)
 			printf("GeneratorThread::init>> ERROR getting the environment variable ICUB_DIR, exiting\n");
 			return false;
 		}
-		yarp::String cubPathStr(cubPath);
+		string cubPathStr(cubPath);
 		options.fromConfigFile((cubPathStr + "/app/drummingEpfl/conf/" + partName + "Config.ini").c_str());
 	}
   
