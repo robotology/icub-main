f9ed1a83021a7eb89779ca65568e5cb0e1e3ce4c
Marco Randazzo <marco.randazzo@iit.it>
2010-06-01 13:56:58 +0000

    Francesco fixed the small fast movement during the calibration
    svn path=/trunk/iCub/; revision=5150

--- src/modules/armcalibrator/iCubArmCalibratorJ4.cpp
+++ src/modules/armcalibrator/iCubArmCalibratorJ4.cpp
@@ -163,6 +163,10 @@ bool iCubArmCalibratorJ4::calibrate(DeviceDriver *dd)
 	fprintf(stderr, "ARMCALIB::start! new calibrator for torque controlled shoulder by randaz \n");
 
     int k;
+	for (k =0; k < nj; k++)
+		calibrateJoint(k);
+	Time::delay(1.0);
+
     for (k = 0; k < nj; k++) 
     {
         fprintf(stderr, "ARMCALIB::Calling enable amp for joint %d\n", k);
@@ -170,44 +174,12 @@ bool iCubArmCalibratorJ4::calibrate(DeviceDriver *dd)
         fprintf(stderr, "ARMCALIB::Calling enable pid for joint %d\n", k);
         iPids->enablePid(k);
     }
-
-    ret = true;
-    bool x;
-
 	////////////////////////////////////////////
-    iPids->disablePid(2);
-    iAmps->disableAmp(2);
-
-	int firstSetOfJoints[] = {0, 1 , 3};
-    for (k =0; k < 3; k++)
-		calibrateJoint(firstSetOfJoints[k]);
-	for (k =0; k < 3; k++)
-	{
-		x = checkCalibrateJointEnded(firstSetOfJoints[k]);
-		ret = ret && x;
-	}
-
-	for (k = 0; k < 3; k++)
-		goToZero(firstSetOfJoints[k]);
-	for (k = 0; k < 3; k++)
-		checkGoneToZero(firstSetOfJoints[k]);
+	for (k = 0; k < nj; k++)
+		goToZero(k);
+	for (k = 0; k < nj; k++)
+		checkGoneToZero(k);
 	//////////////////////////////////////////
-    iAmps->enableAmp(2);
-    iPids->enablePid(2);
-	int secondSetOfJoints[] = {2};
-	for (k =0; k < 1; k++)
-		calibrateJoint(secondSetOfJoints[k]);
-	for (k =0; k < 1; k++)
-	{
-		x = checkCalibrateJointEnded(secondSetOfJoints[k]);
-		ret = ret && x;
-	}
-
-	for (k = 0; k < 1; k++)
-		goToZero(secondSetOfJoints[k]);
-	for (k = 0; k < 1; k++)
-		checkGoneToZero(secondSetOfJoints[k]);
-
     fprintf(stderr, "ARMCALIB::calibration done!\n");
     return ret;
 }
