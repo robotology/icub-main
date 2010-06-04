d0abaa1bba3aee7c05390d2db2903d0fcfbeeb81
Marco Randazzo <marco.randazzo@iit.it>
2010-06-04 08:14:46 +0000

    strain_board handler new version
    svn path=/trunk/iCub/; revision=5248

--- src/firmware/controller4Dc/Code/strain_board.c
+++ src/firmware/controller4Dc/Code/strain_board.c
@@ -1,25 +1,80 @@
 #include "strain_board.h"
 #include "ad.h"
+#include "can1.h" 
 
-Int16 _strain[6] = INIT_ARRAY (0);
-Int16 _strain_init[6] = INIT_ARRAY (0);
-Int16 _strain_old[6] = INIT_ARRAY (0);
-Int32 _Feq = 0;
+Int16 _strain[STRAIN_MAX][6];
+Int16 _strain_init[STRAIN_MAX][6];
+Int16 _strain_old[STRAIN_MAX][6];
 
-byte  _strain_wtd = STRAIN_SAFE;
+byte  _strain_wtd[STRAIN_MAX];
+
+/*************************************************************************** 
+ * Initialization
+ ***************************************************************************/
+void start_strain (word can_address)
+{
+	//start the can transmission. CAN must be already intialized.
+	canmsg_t canmsg;
+	canmsg.CAN_messID = can_address;
+	canmsg.CAN_data[0] = 0x07;
+	canmsg.CAN_data[1] = 0x00;
+	canmsg.CAN_length = 2;
+	canmsg.CAN_frameType = DATA_FRAME;
+	CAN1_send (canmsg.CAN_messID, canmsg.CAN_frameType, canmsg.CAN_length, canmsg.CAN_data);	
+}
+
+/*************************************************************************** 
+ * Initialization
+ ***************************************************************************/
+void stop_strain (word can_address)
+{
+	//start the can transmission. CAN must be already intialized.
+	canmsg_t canmsg;
+	canmsg.CAN_messID = can_address;
+	canmsg.CAN_data[0] = 0x07;
+	canmsg.CAN_data[1] = 0x01;
+	canmsg.CAN_length = 2;
+	canmsg.CAN_frameType = DATA_FRAME;
+	CAN1_send (canmsg.CAN_messID, canmsg.CAN_frameType, canmsg.CAN_length, canmsg.CAN_data);	
+}
 
 /*************************************************************************** 
  * Initialization
  ***************************************************************************/
 void init_strain (void)
 {
-	#if VERSION == 0x0170
-		//do nothing
-	#elif VERSION == 0x0171
+	byte i=0;
+	byte k=0;
+	canmsg_t canmsg;
+		
+	for (i=0; i<6; i++)
+		for (k=0; k<STRAIN_MAX; k++)
+		{
+			_strain[k][i] = 0;
+			_strain_init[k][i] = 0;
+			_strain_old[k][i] = 0;			
+		}
+	
+	for (k=0; k<STRAIN_MAX; k++)
+		{
+			_strain_wtd[k]= STRAIN_SAFE;	
+		}
+	
+	/*
+	// No more used. Now icubinterface starts the boards.			
+	#if VERSION == 0x0170 || VERSION == 0x0172
+		//start the can transmission. CAN must be already intialized.
+		start_strain(0x205);
+	#elif VERSION ==0x0157 || VERSION ==0x0150
+		//start the can transmission. CAN must be already intialized.
+		start_strain(0x20D);
+		start_strain(0x20C);	
+	#elif VERSION == 0x0171 
 		AD_init ();
 		AD_enableIntTriggerA ();
 		AD_enableIntTriggerB ();		
 	#endif
+	*/
 }
 
 /*************************************************************************** 
@@ -30,9 +85,9 @@ word read_strain(byte jnt, bool sign)
 	word temp;
 	Int32 temporary;	
 
-	#if VERSION == 0x0170
-		return _strain[jnt];
-	#else if VERSION == 0x0171
+	#if VERSION == 0x0170 || VERSION == 0x0172
+		return _strain[0][jnt];
+	#else if VERSION == 0x0171 
 		switch (jnt)
 		{
 			case 0: AD_getChannel16A (2, &temp); break;
--- src/firmware/controller4Dc/Code/strain_board.h
+++ src/firmware/controller4Dc/Code/strain_board.h
@@ -3,14 +3,29 @@
 
 #include "controller.h"
 
-extern Int32 _Feq;
-extern Int16 _strain[6];
-extern Int16 _strain_init[6];
-extern Int16 _strain_old[6];
-extern byte  _strain_wtd;  //watchdog timer: disables pwm if strain doesn't communicate
-#define STRAIN_SAFE 15     //number of ms for the watchdog timer
+#define STRAIN_MAX 4
+
+#define CAN_ID_JNT_STRAIN_11		11
+#define CAN_ID_JNT_STRAIN_12		12
+#define CAN_ID_6AX_STRAIN_13		13
+#define CAN_ID_6AX_STRAIN_14 		14
+
+#define WDT_JNT_STRAIN_11		0
+#define WDT_JNT_STRAIN_12		1
+#define WDT_6AX_STRAIN_13		2
+#define WDT_6AX_STRAIN_14 		3
+
+
+extern Int16 _strain[STRAIN_MAX][6];
+extern Int16 _strain_init[STRAIN_MAX][6];
+extern Int16 _strain_old[STRAIN_MAX][6];
+extern byte  _strain_wtd[STRAIN_MAX];  		//watchdog timer: disables pwm if strain doesn't communicate
+#define STRAIN_SAFE 15     					//number of ms for the watchdog timer
 
 void init_strain ();
 word read_strain(byte jnt, bool sign);
+void start_strain(word can_address);
+void stop_strain(word can_address);
+
 
 #endif
