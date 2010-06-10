5536a77b23eb3969b9aafd564660f0fd1c61cc82
Marco Randazzo <marco.randazzo@iit.it>
2010-06-10 08:18:20 +0000

    added get_relative_turn() function
    svn path=/trunk/iCub/; revision=5412

--- src/firmware/libDsp56f807/include/abs_ssi_interface.h
+++ src/firmware/libDsp56f807/include/abs_ssi_interface.h
@@ -2,16 +2,18 @@
 #ifndef __abs_position_sens_ssi_h__
 #define __abs_position_sens_ssi_h__
 
-void 	init_position_abs_ssi(void);
-UInt16  get_position_abs_ssi(byte jnt);
-Int32   get_relative_position_abs_ssi(byte jnt);
-Int32   init_relative_position_abs_ssi();
-Int32   set_relative_position_abs_ssi(byte jnt,Int32 offset);
-void    set_relative_position_abs_ssi_turns(byte jnt, Int16 turn);
-UInt16  get_absolute_real_position_abs_ssi(byte jnt);
-void    set_max_position(byte jnt, UInt16 max_pos);
-UInt16  get_max_position(byte jnt);
-void    set_current_as_middle_position(byte jnt);
-void get_status_abs_ssi(bool* s_ocf,bool* s_cof,bool* s_lin, bool* s_inc, bool* s_dec, byte jnt);
-byte get_error_abs_ssi(byte jnt);
+void 	init_position_abs_ssi           	(void);
+Int32   init_relative_position_abs_ssi  	();
+
+UInt16  get_position_abs_ssi            	(byte jnt);
+Int32   get_relative_position_abs_ssi   	(byte jnt);
+Int32   set_relative_position_abs_ssi   	(byte jnt, Int32 offset);
+Int16   get_relative_turn                   (byte jnt);
+void    set_relative_position_abs_ssi_turns (byte jnt, Int16 turn);
+UInt16  get_absolute_real_position_abs_ssi  (byte jnt);
+void    set_max_position                    (byte jnt, UInt16 max_pos);
+UInt16  get_max_position                	(byte jnt);
+void    set_current_as_middle_position  	(byte jnt);
+void    get_status_abs_ssi	(bool* s_ocf,bool* s_cof,bool* s_lin, bool* s_inc, bool* s_dec, byte jnt);
+byte    get_error_abs_ssi               	(byte jnt);
 #endif 
