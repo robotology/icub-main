de442238ce237d54ff0719a1522c5605ec9e9527
Francesco Rea <francesco.rea@iit.it>
2010-06-11 15:34:48 +0000

    verbosity
    svn path=/trunk/iCub/; revision=5497

--- src/selectiveAttention/selectiveAttentionEngine/src/selectiveAttentionProcessor.cpp
+++ src/selectiveAttention/selectiveAttentionEngine/src/selectiveAttentionProcessor.cpp
@@ -215,7 +215,9 @@ void selectiveAttentionProcessor::run(){
         }
 
         
-        extractContour(outputImage,outputColourImage,inImage,centroid_y,centroid_y);
+        extractContour(outputImage,outputColourImage,inImage,centroid_x,centroid_y);
+        printf("centroid_x %d, centroid_y %d", centroid_x, centroid_y);
+
         //ippiCopy_8u_C1R((const Ipp8u *)dst->imageData,dst->widthStep, outputImage->getRawImage(), outputImage->getRowSize(),srcsize);
         //get the colour of the inputImage starting in the centroid_x and centroid_y position
         //unsigned char* pColour=inImage->getPixelAddress(centroid_x,centroid_y);
