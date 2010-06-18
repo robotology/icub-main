2b9eb954ff29342f6cd49429810c53f3114eb417
Lorenzo Natale <lorenzo.natale@iit.it>
2010-06-18 23:18:42 +0000

    No commit message
    svn path=/trunk/iCub/; revision=5644

--- admin/scripts/test2-compile-code.sh
+++ admin/scripts/test2-compile-code.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-type=lenny
+type=$1
 
-SOURCE=$PWD
-echo Working in directory $SOURCE | tee should_report.txt
-./admin/scripts/test2-compilable.sh $type main || echo done compiling
-./admin/scripts/test2-upload-report.sh $type main
+EXPECTED_ARGS=1
+ 
+if [ $# -ne $EXPECTED_ARGS ]
+    SOURCE=$PWD
+    echo Working in directory $SOURCE | tee should_report.txt
+    ./admin/scripts/test2-compilable.sh $type main || echo done compiling
+    ./admin/scripts/test2-upload-report.sh $type main
+then
+   echo "Usage: `basename $0` {distribution}"
+fi
+
 
 
 
