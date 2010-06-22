50b5de9ae7286a9b088ede34dbe5a1da0c8b8dcd
Lorenzo Natale <lorenzo.natale@iit.it>
2010-06-22 10:30:18 +0000

    fixing once again conditionals
    svn path=/trunk/iCub/; revision=5694

--- admin/scripts/test2-compile-code.sh
+++ admin/scripts/test2-compile-code.sh
@@ -4,14 +4,14 @@ type=$1
 
 EXPECTED_ARGS=1
  
-if [ $# -e $EXPECTED_ARGS ];
+if [ $# -ne $EXPECTED_ARGS ];
 then
+    echo "Usage `basename $0` {distribution}"
+else
     SOURCE=$PWD
     echo Working in directory $SOURCE | tee should_report.txt
     ./admin/scripts/test2-compilable.sh $type main || echo done compiling
     ./admin/scripts/test2-upload-report.sh $type main
-else
-   echo "Usage: `basename $0` {distribution}"
 fi
 
 
--- admin/scripts/test2-upload-doc.sh
+++ admin/scripts/test2-upload-doc.sh
@@ -9,12 +9,12 @@ EXPECTED_ARGS=1
  
 if [ $# -ne $EXPECTED_ARGS ];
 then
+    echo "Usage: `basename $0` {main/contrib}"
+else
     mod=$1
     doc=$ICUB_ROOT/$mod/doc
 
     cd $doc && rsync --rsh="ssh -x -l $WEB_USER" --modify-window=2 -lavzP . $WEB_SERVER:$WEB_DOC_DIR/$WEB_DOC_SUFFIX/dox
-else
-    echo "Usage: `basename $0` {main/contrib}"
 fi
 
 
