dda9bd3bfc29d5ec0997f590681593ffee893e09
Lorenzo Natale <lorenzo.natale@iit.it>
2010-07-08 10:22:19 +0000

    No commit message
    svn path=/trunk/iCub/; revision=5996

--- admin/scripts/test2-compile-doc.sh
+++ admin/scripts/test2-compile-doc.sh
@@ -6,13 +6,14 @@ EXPECTED_ARGS=1
  
 if [ $# -ne $EXPECTED_ARGS ];
 then
+   echo "Usage: `basename $0` {main/contrib}"
+else
     base=$PWD
     cd $ICUB_ROOT/$1
     doxygen ./conf/Doxyfile.txt
     cd $base
-else
-   echo "Usage: `basename $0` {main/contrib}"
 fi
+
 
 
 
