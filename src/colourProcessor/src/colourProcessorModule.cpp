2f8808f8042c20068b64a105f818e2844f1222d9
Francesco Rea <francesco.rea@iit.it>
2010-07-12 13:58:17 +0000

    removed attached terminal
    svn path=/trunk/iCub/; revision=6070

--- src/colourProcessor/src/colourProcessorModule.cpp
+++ src/colourProcessor/src/colourProcessorModule.cpp
@@ -30,7 +30,7 @@ bool colourProcessorModule::configure(ResourceFinder &rf)
     Time::turboBoost();
     cmdPort.open(getName("/cmd:i"));
     attach(cmdPort);
-    attachTerminal();
+    //attachTerminal();
     //printf("resource finder configuration after time turbo boosting \n");
     
     rgbProcessor=0;
