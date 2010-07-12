00d62bc25547c40d91474fadee82cb58d0413d3a
Arjan Gijsberts <arjan.gijsberts@idiap.ch>
2010-07-12 21:46:19 +0000

    Initial commit of migrated learningMachine.
    svn path=/trunk/iCub/; revision=6083

---
+++ src/libraries/learningMachine/src/Standardizer.cpp
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2007-2010 RobotCub Consortium, European Commission FP6 Project IST-004370
+ * author:  Arjan Gijsberts
+ * email:   arjan.gijsberts@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+ */
+
+#include <sstream>
+#include <cmath>
+
+#include <yarp/os/Value.h>
+
+#include "iCub/learningMachine/Standardizer.h"
+
+namespace iCub {
+namespace learningmachine {
+
+Standardizer::Standardizer(double m, double s)
+  : noSamples(0), runningMean(0.), runningStd(0.), squaredErrors(0.) {
+    this->setName("Standardizer");
+    this->setDesiredMean(m);
+    this->setDesiredStd(s);
+}
+
+void Standardizer::update(double val) {
+    /*
+     * Running variance calculation, taken from:
+     *
+     * Donald E. Knuth (1998). The Art of Computer Programming, volume 2:
+     *   Seminumerical Algorithms, 3rd edn., p. 232. Boston: Addison-Wesley
+     */
+    this->noSamples++;
+    double delta = val - this->runningMean;
+    this->runningMean += (delta / this->noSamples);
+    this->squaredErrors += (delta * (val - this->runningMean)); // M2 in TAOCP
+    this->runningStd = sqrt(this->squaredErrors / (this->noSamples + 1));
+
+    this->scale = this->runningStd / this->std;
+    this->offset = (this->runningMean - this->mean) * this->scale;
+}
+
+std::string Standardizer::getInfo() {
+    std::ostringstream buffer;
+    buffer << this->IScaler::getInfo() << ", ";
+    buffer << "Input Stats: " << this->runningMean << " +/- " << this->runningStd << ", ";
+    buffer << "Desired: " << this->getDesiredMean() << " +/- " << this->getDesiredStd();
+    return buffer.str();
+}
+
+void Standardizer::writeBottle(Bottle& bot) {
+    bot.addInt(this->noSamples);
+    bot.addDouble(this->squaredErrors);
+    bot.addDouble(this->mean);
+    bot.addDouble(this->std);
+    bot.addDouble(this->runningMean);
+    bot.addDouble(this->runningStd);
+
+    // make sure to call the superclass's method
+    this->IScaler::writeBottle(bot);
+}
+
+void Standardizer::readBottle(Bottle& bot) {
+    // make sure to call the superclass's method (will reset transformer)
+    this->IScaler::readBottle(bot);
+
+    this->runningStd = bot.pop().asDouble();
+    this->runningMean = bot.pop().asDouble();
+    this->std = bot.pop().asDouble();
+    this->mean = bot.pop().asDouble();
+    this->squaredErrors = bot.pop().asDouble();
+    this->noSamples = bot.pop().asInt();
+}
+
+bool Standardizer::configure(Searchable& config) {
+    bool success = this->IScaler::configure(config);
+
+    // set the desired output mean (double)
+    if(config.find("mean").isDouble() || config.find("mean").isInt()) {
+        this->setDesiredMean(config.find("mean").asDouble());
+        success = true;
+    }
+    // set the desired output standard deviation (double)
+    if(config.find("std").isDouble() || config.find("std").isInt()) {
+        this->setDesiredStd(config.find("std").asDouble());
+        success = true;
+    }
+
+    return success;
+}
+
+
+} // learningmachine
+} // iCub
+
---
+++ src/modules/learningMachine/include/iCub/learningMachine/IPortEventListener.h
@@ -0,0 +1,120 @@
+/*
+ * Copyright (C) 2007-2010 RobotCub Consortium, European Commission FP6 Project IST-004370
+ * author:  Arjan Gijsberts
+ * email:   arjan.gijsberts@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+ */
+
+#ifndef LM_IPORTEVENTLISTENER__
+#define LM_IPORTEVENTLISTENER__
+
+#include <string>
+
+#include <yarp/os/Port.h>
+
+#include "iCub/learningMachine/IEventListener.h"
+
+
+using namespace yarp::os;
+
+namespace iCub {
+namespace learningmachine {
+
+/**
+ * The abstract base class for EventListeners that output to a port. This class
+ * is introduced so that base functionality to register ports can be inherited
+ * by multiple subclasses.
+ *
+ * \see iCub::learningmachine::PredictEventListener
+ * \see iCub::learningmachine::TrainEventListener
+ *
+ * \author Arjan Gijsberts
+ */
+
+class IPortEventListener : public IEventListener {
+protected:
+    /**
+     * The outgoing port for the events.
+     */
+    Port port;
+
+    /**
+     * A prefix path for the ports that will be registered.
+     */
+    std::string portPrefix;
+
+    /**
+     * Resets the port and opens it at the specified name. If passed an empty
+     * string the port is opened at the first free port with the predefined
+     * prefix.
+     *
+     * @param portName the name of the port
+     */
+    void resetPort(std::string portName);
+
+public:
+    /**
+     * Constructor.
+     *
+     * @param pp the standard prefix for opening the ports
+     */
+    IPortEventListener(std::string pp) : portPrefix(pp) { }
+
+    /**
+     * Copy Constructor.
+     */
+    IPortEventListener(const IPortEventListener& other)
+      : IEventListener(other), portPrefix(other.portPrefix) { }
+
+    /**
+     * Destructor.
+     */
+    virtual ~IPortEventListener() {
+        this->port.interrupt();
+        this->port.close();
+    }
+
+    /**
+     * Assignment operator.
+     */
+    virtual IPortEventListener& operator=(const IPortEventListener& other);
+
+    /**
+     * Starts the IEventListener.
+     */
+    virtual void start() {
+        this->resetPort("");
+    }
+
+    /*
+     * Inherited from IConfig.
+     */
+    virtual bool configure(Searchable& config);
+
+    /**
+     * Asks the event listener to return a string containing information on
+     * its configuration so far.
+     *
+     * @return the information on the IEventListener
+     */
+    virtual std::string getInfo() {
+        return this->IEventListener::getInfo() + " [port: " + this->port.where().toString().c_str() + "]";
+    }
+
+};
+
+} // learningmachine
+} // iCub
+
+#endif
