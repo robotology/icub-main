79c951aeadb47e2e7b001c6581c176b46ff4ee3f
Ugo Pattacini <ugo.pattacini@iit.it>
2010-07-23 16:23:14 +0000

    No commit message
    svn path=/trunk/iCub/; revision=6404

---
+++ src/modules/camCalib/src/CalibToolFactory.cpp
@@ -0,0 +1,11 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Copyright (C) 2007 Jonas Ruesch inspired by Paul Fitzpatrick
+ * CopyPolicy: Released under the terms of the GNU GPL v2.0.
+ *
+ */
+
+#include <iCub/CalibToolFactory.h>
+
+CalibToolFactories CalibToolFactories::pool;
