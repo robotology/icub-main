75b40146cc265e060647ac113405bc515de984bb
Vadim Tikhanoff <vadim.tikhanoff@iit.it>
2010-07-27 10:34:07 +0000

    a bit of documentation
    svn path=/trunk/iCub/; revision=6548

--- src/modules/zdfModule/src/zdfMod.cpp
+++ src/modules/zdfModule/src/zdfMod.cpp
@@ -1,6 +1,138 @@
+/** 
+ * @ingroup icub_module
+ *
+ * \defgroup icub_zdfModule zdfMod
+ *
+ * Receives the left and right images from the robot and segments objects that are located in the fovea. Performs marker-less pixel-wise segmentation of an object located in the fovea.The output is an image of the segmented object in grayscale and a difference of gausian segmentation.
+ *
+ * 
+ * \section lib_sec Libraries
+ *
+ * YARP
+ * IPP
+ * OPENCV
+ *
+ * \section parameters_sec Parameters
+ * 
+ * <b>Command-line Parameters <\b> 
+ * 
+ * The following key-value pairs can be specified as command-line parameters by prefixing \c -- to the key 
+ * (e.g. \c --from file.ini. The value part can be changed to suit your needs; the default values are shown below. 
+ *
+ * - \c from \c zdfMod.ini \n 
+ *   specifies the configuration file
+ *
+ * - \c context \c zdfMod/conf \n
+ *   specifies the sub-path from \c $ICUB_ROOT/icub/app to the configuration file
+ *
+ * - \c name \c zdfMod \n   
+ *   specifies the name of the module (used to form the stem of module port names)  
+ *
+ * <b>Configuration File Parameters </b>
+ *
+ * The following key-value pairs can be specified as parameters in the configuration file 
+ * (they can also be specified as command-line parameters if you so wish). 
+ * The value part can be changed to suit your needs; the default values are shown below. 
+ *   
+ * - \c inPortLeft \c /imageLeft:i \n    
+ *   specifies the input port name (this string will be prefixed by \c /zdfMod 
+ *   or whatever else is specifed by the name parameter
+ *
+ * - \c inPortRight \c /imageRight:i \n    
+ *   specifies the input port name (this string will be prefixed by \c /zdfMod 
+ *   or whatever else is specifed by the name parameter
+ *
+ * - \c outPortProb \c /imageProb:o \n    
+ *   specifies the input port name (this string will be prefixed by \c /zdfMod 
+ *   or whatever else is specifed by the name parameter
+ *
+ * - \c outPortSeg \c /imageSeg:o \n  
+ *   specifies the output port name (this string will be prefixed by \c /zdfMod 
+ *   or whatever else is specifed by the name parameter
+ *
+ * - \c outPortDog \c /imageDog:o \n  
+ *   specifies the output port name (this string will be prefixed by \c /zdfMod 
+ *   or whatever else is specifed by the name parameter
+
+ * \section portsa_sec Ports Accessed
+ * 
+ * - None
+ *                      
+ * \section portsc_sec Ports Created
+ *
+ *  <b>Input ports</b>
+ *
+ *  - \c /zdfMod \n
+ *    This port is used to change the parameters of the module at run time or stop the module. \n
+ *    The following commands are available
+ * 
+ *    \c help \n
+ *    \c quit \n
+ *
+ *    Note that the name of this port mirrors whatever is provided by the \c --name parameter value
+ *    The port is attached to the terminal so that you can type in commands and receive replies.
+ *    The port can be used by other modules but also interactively by a user through the yarp rpc directive, viz.: \c yarp \c rpc \c /zdfMod
+ *    This opens a connection from a terminal to the port and allows the user to then type in commands and receive replies.
+ *       
+ *  - \c /zdfMod/imageLeft:i \n
+ *  - \c /zdfMod/imageRight:i \n
+ *
+ * <b>Output ports</b>
+ *
+ *  - \c /zdfMod \n
+ *    see above
+ *
+ *  - \c /zdfMod/imageProb:o \n
+ *  - \c /zdfMod/imageSeg:o \n
+ *  - \c /zdfMod/imageDog:o \n
+ *
+ * <b>Port types </b>
+ *
+ * The functional specification only names the ports to be used to communicate with the module 
+ * but doesn't say anything about the data transmitted on the ports. This is defined by the following code. 
+ *
+ * \c BufferedPort<ImageOf<PixelBgr> >   \c inPortLeft; \n 
+ * \c BufferedPort<ImageOf<PixelBgr> >   \c inPortRight; \n 
+ * \c BufferedPort<ImageOf<PixelMono> >   \c outPortProb; \n 
+ * \c BufferedPort<ImageOf<PixelMono> >   \c outPortSeg;   
+ * \c BufferedPort<ImageOf<PixelMono> >   \c outPortDog;       
+ *
+ * \section in_files_sec Input Data Files
+ *
+ * None
+ *
+ * \section out_data_sec Output Data Files
+ *
+ * None
+ *
+ * \section conf_file_sec Configuration Files
+ *
+ * \c zdfMod.ini  in \c $ICUB_ROOT/app/zdfMod/conf \n
+ * 
+ * \section tested_os_sec Tested OS
+ *
+ * Linux: Ubuntu 9.10 and Debian Stable 
+ *
+ * \section example_sec Example Instantiation of the Module
+ * 
+ * <tt>zdfMod --name zdfMod --context zdfMod/conf --from zdfMod.ini </tt>
+ *
+ * \author 
+ * 
+ * Vadim Tikhanoff, Andrew Dankers
+ * 
+ * Copyright (C) 2009 RobotCub Consortium
+ * 
+ * CopyPolicy: Released under the terms of the GNU GPL v2.0.
+ * 
+ * This file can be edited at \c $ICUB_ROOT/src/yuvProc/include/iCub/yuvProc.h
+ * 
+ */
+
+
 /* 
  * Copyright (C) 2009 RobotCub Consortium, European Commission FP6 Project IST-004370
- * Authors: Vadim Tikhanoff
+ * Authors: Vadim Tikhanoff Andrew Dankers
  * email:   vadim.tikhanoff@iit.it
  * website: www.robotcub.org 
  * Permission is granted to copy, distribute, and/or modify this program
@@ -38,15 +170,6 @@ bool zdfMod::configure(yarp::os::ResourceFinder &rf)
     */
    
     setName(moduleName.c_str());
-
-   /* params.iter_max                  = 1; // INI!!!
-    params.randomize_every_iteration = 0;
-    params.smoothness_penalty_base   = 50;
-    params.smoothness_penalty        = 600;
-    params.data_penalty              = 106;
-    params.smoothness_3sigmaon2      = 13;
-*/
-
 
     parameters.iter_max = rf.findGroup("PARAMS").check("max_iteration",Value(1),"what did the user select?").asInt();
     parameters.randomize_every_iteration = rf.findGroup("PARAMS").check("randomize_iteration",Value(1),"what did the user select?").asInt();
