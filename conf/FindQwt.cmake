40a1ec0de8214755e60d65be010390ef06433c6f
Francesco Nori <francesco.nori@iit.it>
2010-07-27 16:42:06 +0000

    updated to compile the portScope
    svn path=/trunk/iCub/; revision=6607

--- conf/FindQwt.cmake
+++ conf/FindQwt.cmake
@@ -1,36 +1,50 @@
-# Try to find qwt
-# Once done this will define
+# Find Qwt
+# ~~~~~~~~
+# Copyright (c) 2010, Tim Sutton <tim at linfiniti.com>
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
-#  QWT_FOUND - system has qwt
-#  QWT_INCLUDE_DIR - ~ the qwt include directory (needs <qwt/qwt_header_xy.h>)
-#  QWT_LIBRARY - Link these to use qwt
+# Once run this will define: 
+# 
+# QWT_FOUND       = system has QWT lib
 #
-#  2007, Jonas Ruesch
+# QWT_LIBRARY     = full path to the QWT library
+#
+# QWT_INCLUDE_DIR      = where to find headers 
 #
 
-FIND_PATH(QWT_INCLUDE_DIR 
-NAMES   qwt/qwt_plot.h
-PATHS   /usr/local/include
-        /usr/include
-)
 
+#MESSAGE("Searching for QWT")
+FIND_PATH(QWT_INCLUDE_DIR qwt.h 
+  PATHS /usr/include
+  /usr/local/include
+  "$ENV{QWT_DIR}/src" 
+  PATH_SUFFIXES qwt-qt4 qwt
+  )
+FIND_LIBRARY(QWT_LIBRARY qwt PATHS 
+  /usr/lib
+  /usr/local/lib
+  "$ENV{QWT_DIR}/lib" 
+  )
+IF (NOT QWT_LIBRARY)
+  # try using ubuntu lib naming
+  FIND_LIBRARY(QWT_LIBRARY qwt-qt4 PATHS 
+    /usr/lib
+    /usr/local/lib
+    "$ENV{QWT_DIR}/lib" 
+    )
+ENDIF (NOT QWT_LIBRARY)
 
-FIND_LIBRARY(QWT_LIBRARY
-NAMES qwt
-PATHS /usr/lib
-      /usr/local/lib
-)
+IF (QWT_INCLUDE_DIR AND QWT_LIBRARY)
+  SET(QWT_FOUND TRUE)
+ENDIF (QWT_INCLUDE_DIR AND QWT_LIBRARY)
 
-
-if (QWT_LIBRARY)
-    set(QWT_FOUND TRUE)
-else (QWT_LIBRARY)
-    set(QWT_FOUND FALSE)    
-endif (QWT_LIBRARY)
-
-set(QWT_INCLUDE_DIR
-    ${QWT_INCLUDE_DIR}
-)
-
-#MESSAGE("----------qwt: ${QWT_INCLUDE_DIR} qwtlib: ${QWT_LIBRARY}")
-
+IF (QWT_FOUND)
+  IF (NOT QWT_FIND_QUIETLY)
+    MESSAGE(STATUS "Found QWT: ${QWT_LIBRARY}")
+  ENDIF (NOT QWT_FIND_QUIETLY)
+ELSE (QWT_FOUND)
+  IF (QWT_FIND_REQUIRED)
+    MESSAGE(FATAL_ERROR "Could not find QWT")
+  ENDIF (QWT_FIND_REQUIRED)
+ENDIF (QWT_FOUND)
