de7fd3e066c82329ee51c4c135855387b99f25a8
Lorenzo Natale <lorenzo.natale@iit.it>
2010-07-28 16:28:28 +0000

    support for installing applications
    svn path=/trunk/iCub/; revision=6673

--- conf/iCubHelpers.cmake
+++ conf/iCubHelpers.cmake
@@ -189,6 +189,63 @@ MACRO(PARSE_ARGUMENTS prefix arg_names option_names)
 ENDMACRO(PARSE_ARGUMENTS)
 
 
+macro(icub_app target)
+	message(STATUS "declare target ${target}")
+	set(dummy f-${target})
+    set(dapp ${ICUB_DESTINATION_APP}/app/${target})
+	add_custom_command(OUTPUT ${dummy}
+					COMMAND ${CMAKE_COMMAND} -E make_directory ${target}
+					COMMENT "Creating directories ${target}")
+endmacro(icub_app)
+
+macro(icub_app_install target)
+  PARSE_ARGUMENTS(${target}
+    "FILES;DESTINATION"
+    "VERBOSE"
+    ${ARGN}
+    )
+	
+	set(dummy f-${target})
+ 
+  set(VERBOSE ${${target}_VERBOSE})
+  if(VERBOSE)
+    MESSAGE(STATUS "*** Arguments for ${target}")
+    MESSAGE(STATUS "Files: ${${target}_FILES}")
+    MESSAGE(STATUS "Destination: ${${target}_DESTINATION}")
+    MESSAGE(STATUS "Option verbosity: ${${target}_VERBOSE}")
+  endif(VERBOSE)
+  
+  set(files ${${target}_FILES})
+  set(destination ${${target}_DESTINATION})
+  
+  set(dapp "${ICUB_DESTINATION_APP}/app/${target}/${destination}")
+
+  #message(STATUS "${CMAKE_COMMAND} -E make_directory ${dapp}/${destination}")
+
+  add_custom_command(OUTPUT ${dummy}
+					COMMAND ${CMAKE_COMMAND} -E make_directory ${dapp}
+					COMMENT "Creating directories ${dapp}"
+					APPEND)
+
+	foreach(f ${files})
+		set(command2 ${CMAKE_COMMAND} -E copy ${f} ${dapp})
+		add_custom_command(OUTPUT ${dummy}
+					COMMAND ${command2}
+					COMMENT "Copy to ${f} to ${dapp}"
+					APPEND)		
+	endforeach(f ${files})					
+
+
+endmacro(icub_app_install)
+
+macro(icub_add_target target)
+	message(STATUS "adding target ${target}")
+	set(dummy f-${target})
+	add_custom_target(app-${target} DEPENDS ${dummy})
+	
+	icub_set_property(GLOBAL APPEND PROPERTY ICUB_APPLICATIONS app-${target})
+endmacro(icub_add_target)
+
 ### From yarp.
 # Helper macro to work around a bug in set_property in cmake 2.6.0
 # We use icub_ prefix to avoid name clashes with yarp.
@@ -212,36 +269,7 @@ MACRO(icub_set_property _global _property _append varname)
   endif (_icub_append_chk)
 ENDMACRO(icub_set_property)
 
-# Export library to be used from external programs
-# @param name: name of the target to be export
-# @param private_inc_dirs: directory that contains private header files
-# @param public_inc_dirs: a list of header files that will be copied at 
-# install time to INSTALL_PREFIX/include/iCub
-#
-# Creates a cache variable and property to store the target include directory.
-#
-# -add ${name} to group icub_targets
-# -append ${name} to global property ICUB_TARGETS
-# -create property INCLUDE_DIRS for target ${name} 
-# -install rule to copy all header files to include/iCub
-# -append export rules to a file in ${PROJECT_BINARY_DIR}
-
-macro(icub_export_library_old name private_inc_dirs dest_dir)
-
-set(ICUB_EXPORTBUILD_FILE icub-export-build.cmake)
 
 
-set_property(TARGET ${name} PROPERTY INCLUDE_DIRS  "${private_inc_dirs}")
-set(${name}_INCLUDE_DIRS "${private_inc_dirs}" CACHE STRING "include directories")
-#set(${name}_LIBRARIES ${name} CACHE STRING "library")
-icub_set_property(GLOBAL APPEND PROPERTY ICUB_TARGETS ${name})
 
-#set(ICUB_TARGETS ${ICUB_TARGETS} "${name}" CACHE STRING "" FORCE)
-
-install(TARGETS ${name} DESTINATION ${CMAKE_INSTALL_PREFIX}/lib EXPORT icub-targets)
-install(FILES ${ARGN} DESTINATION ${dest_dir})
-
-export(TARGETS ${name} APPEND FILE ${CMAKE_BINARY_DIR}/${ICUB_EXPORTBUILD_FILE})
-
-endmacro(icub_export_library_old)
 
