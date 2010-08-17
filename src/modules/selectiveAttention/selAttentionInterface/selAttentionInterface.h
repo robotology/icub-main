1cb92df9c0d1331c031d07a6fc2a584561400767
Francesco Rea <francesco.rea@iit.it>
2010-08-17 15:44:37 +0000

    added new GUI that overcomes some issue with the computation demand
    svn path=/trunk/iCub/; revision=7128

---
+++ src/modules/selectiveAttention/selAttentionInterface/selAttentionInterface.h
@@ -0,0 +1,134 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/* 
+ * Copyright (C) 2009 RobotCub Consortium, European Commission FP6 Project IST-004370
+ * Authors: Rea Francesco
+ * email:   francesco.rea@iit.it
+ * website: www.robotcub.org 
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+ */
+
+#if !defined(GCAMVIEW_H)
+#define GCAMVIEW_H
+
+//=============================================================================
+// YARP Includes - Class Specific
+//=============================================================================
+#include <yarp/sig/Image.h>
+#include <yarp/os/Semaphore.h>
+#include <yarp/os/Bottle.h>
+#include <yarp/os/BufferedPort.h>
+//=============================================================================
+// GTK Includes 
+//=============================================================================
+#include <gtk/gtk.h>
+#include "ViewerResources.h"
+#include <string>
+
+//-------------------------------------------------
+// Callbacks
+//-------------------------------------------------
+gboolean forceDraw(gpointer data);
+// Timeout CB
+gint timeout_CB (gpointer data);
+// Main Window close CB
+gboolean delete_event( GtkWidget *widget, GdkEvent *event, gpointer data );
+//  Window redraw CB
+gint expose_CB (GtkWidget *widget, GdkEventExpose *event, gpointer data);
+// Click on Drawinf Area CB
+gint clickDA_CB (GtkWidget *widget, GdkEventButton *event, gpointer data);
+// Menubar CBs
+gint menuFileQuit_CB(GtkWidget *widget, gpointer data);
+gint menuHelpAbout_CB(GtkWidget *widget, gpointer data);
+gint menuImageSize_CB(GtkWidget *widget, gpointer data);
+gint menuImageRatio_CB(GtkWidget *widget, gpointer data);
+gint menuImageInterval_CB(GtkWidget *widget, gpointer data);
+gint menuImageFramerate_CB(GtkWidget *widget, gpointer data);
+gint menuFileSingle_CB(GtkWidget *widget, GdkEventExpose *event, gpointer data);
+gint menuFileSet_CB(GtkWidget *widget, GdkEventExpose *event, gpointer data);
+gint menuImageFreeze_CB(GtkWidget *widget, GdkEventExpose *event, gpointer data);
+// Dialogs CBs
+gint saveSingleDelete_CB(GtkWidget *widget, gpointer data);
+gint saveSetDelete_CB(GtkWidget *widget, gpointer data);
+gint saveSingleClicked_CB(GtkWidget *widget, gpointer data);
+gint saveSetStartClicked_CB(GtkWidget *widget, gpointer data);
+gint saveSetStopClicked_CB(GtkWidget *widget, gpointer data);
+
+void setTimedMode(guint dT);
+
+void setSynchroMode();
+
+void saveImageNow();
+
+//-------------------------------------------------
+// Non Modal Dialogs
+//-------------------------------------------------
+GtkWidget* createSaveSingleDialog();
+GtkWidget* createSaveSetDialog();
+//-------------------------------------------------
+// Main Window Menubar
+//-------------------------------------------------
+GtkWidget* createMenubar();
+//-------------------------------------------------
+// Main Window Statusbar
+//-------------------------------------------------
+void updateStatusbar(GtkWidget *statusbar, gchar *msg);
+//-------------------------------------------------
+// Main Window
+//-------------------------------------------------
+GtkWidget* createMainWindow(void);
+//-------------------------------------------------
+// Service Functions
+//-------------------------------------------------
+bool getImage();
+void parseOptFile(char *fileName);
+bool parseParameters(int argc, char *argv[]);
+void saveOptFile(char *fileName);
+void setOptionsToDefault();
+bool openPorts();
+void closePorts();
+bool setUp();
+void cleanExit();
+
+void printHelp();
+
+//-------------------------------------------------
+// Global Variables
+//-------------------------------------------------
+// main window 
+
+
+//-------------------------------------------------
+// Program Options 
+//-------------------------------------------------
+struct mOptions
+{
+    unsigned int    refreshTime;
+    std::string     portName;
+    char            networkName[256];
+    int             windWidth;
+    int             windHeight;
+    int             posX;
+    int             posY;
+    char            fileName[256];
+    int             saveOnExit;
+    std::string           outPortName;
+    char            outNetworkName[256];
+    int             outputEnabled;
+    bool            synch;
+};
+typedef struct mOptions pgmOptions;
+
+
+
+#endif // #if !defined(GCAMVIEW_H)
---
+++ src/modules/selectiveAttention/selAttentionInterface/viewerResources.h
@@ -0,0 +1,249 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+/* 
+ * Copyright (C) 2009 RobotCub Consortium, European Commission FP6 Project IST-004370
+ * Author Lorenzo Natale
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or later, see LGPL.TXT
+ */
+
+#ifndef __VIEWERRESOURCES__
+#define __VIEWERRESOURCES__
+
+#include <gtk/gtk.h>
+//#include "YarpImage2Pixbuf.h"
+
+#include <yarp/os/Semaphore.h>
+#include <yarp/os/Time.h>
+
+//#include "FpsStats.h"
+
+class ViewerResources
+{
+private:
+    GdkPixbuf *frame;
+    GdkPixbuf *scaledFrame;
+ 
+    //FpsStats  *fpsData;
+    yarp::sig::ImageOf<yarp::sig::PixelRgb> yimage;
+
+    bool freezed;
+
+    bool origSize;
+
+    unsigned int windowW;
+    unsigned int windowH;
+
+    yarp::os::Semaphore mutex;
+
+ public:
+
+    GtkWidget *drawArea;
+    GtkWidget *mainWindow;
+
+public:
+    ViewerResources()
+    {
+        frame=0;
+        drawArea=0;
+        //fpsData=0;
+        scaledFrame=0;
+        freezed=false;
+        windowH=0;
+        windowW=0;
+    }
+
+    ~ViewerResources()
+    {
+        if (frame)
+            g_object_unref(frame);
+
+        if (scaledFrame)
+            g_object_unref(scaledFrame);
+    }
+
+    /*
+        bool attach(FpsStats *d)
+    {
+        if (fpsData!=0)
+            return false;
+        //fpsData=d;
+        return true;
+    }
+    */
+
+    void freeze()
+    {
+        freezed=true;
+    }
+
+    void unfreeze()
+    {
+        freezed=false;
+    }
+
+    bool isFreezed()
+    { return freezed; }
+
+    void lock() { mutex.wait(); }
+    void unlock() { mutex.post(); }
+
+    void invalidateDrawArea()
+    {
+        if (drawArea)
+            gtk_widget_queue_draw (drawArea);
+    }
+
+    void resizeWindow()
+        {
+            int targetWidth, targetHeight;
+            targetWidth = imageWidth();
+            targetHeight = imageHeight();
+            if (targetWidth!=0&&targetHeight!=0) {
+                unsigned int windowH=mainWindow->allocation.height;
+                unsigned int windowW=mainWindow->allocation.width;
+                unsigned int daH=drawArea->allocation.height;
+                unsigned int daW=drawArea->allocation.width;
+
+                //trick: we compute the new size of the window by difference
+                
+                unsigned int newHeight=(windowH-daH)+targetHeight;
+                unsigned int newWidth=(windowW-daW)+targetWidth;
+                gtk_window_resize(GTK_WINDOW(mainWindow), newWidth, newHeight);
+            }
+        }
+
+    void draw(GtkWidget *widget, 
+        unsigned int areaX,
+        unsigned int areaY,
+        unsigned int areaW,
+        unsigned int areaH,
+        bool force=false)
+    {
+/*
+        lock();
+        if (origSize)
+            resizeWindow();
+        unlock();
+
+        guchar *pixels;
+        unsigned int rowstride;
+
+        lock();
+        if ( (!force) && freezed)
+        {
+            unlock();
+            return;
+        }
+
+        unsigned int imageH=yimage.height();
+        unsigned int imageW=yimage.width();
+
+        if ((imageH==0) || (imageW==0))
+        {
+            //nothing to draw
+            unlock();
+            return;
+        }
+
+        if (fpsData!=0)
+            fpsData->update(yarp::os::Time::now());
+
+        if (!frame)
+            frame=gdk_pixbuf_new (GDK_COLORSPACE_RGB, FALSE, 8, imageW, imageH);
+
+        //first rescale frame depending on image size
+        unsigned int frameH=gdk_pixbuf_get_height(frame);
+        unsigned int frameW=gdk_pixbuf_get_width(frame);
+
+        if ( (frameH!=imageH) || (frameW!=imageW) || !frame)
+        {
+            if (frame)
+                g_object_unref(frame);
+
+            frame=gdk_pixbuf_new (GDK_COLORSPACE_RGB, FALSE, 8, imageW, imageH);
+        }
+
+        //copy to frame
+        yarpImage2Pixbuf(&yimage, frame);
+
+        if (scaledFrame)
+            g_object_unref(scaledFrame);
+
+        scaledFrame = gdk_pixbuf_scale_simple(frame,
+            windowW,
+            windowH,
+            GDK_INTERP_BILINEAR); // Best quality
+
+        pixels = gdk_pixbuf_get_pixels (scaledFrame);
+        rowstride = gdk_pixbuf_get_rowstride(scaledFrame);
+        gdk_draw_rgb_image (widget->window,
+            widget->style->black_gc,
+            areaX, areaY,
+            windowW, windowH,
+            GDK_RGB_DITHER_NORMAL,
+            pixels,
+            rowstride);
+        unlock();
+        */
+    }
+
+    void pushImage(yarp::sig::FlexImage &n)
+    {
+        if (freezed)
+            return;
+
+        lock();
+        yimage.copy(n);
+        //fprintf(stderr, "%d %d\n", yimage.height(), yimage.width());
+        unlock();
+    }
+
+    void configure(GtkWidget *widget,
+        unsigned int width,
+        unsigned int height)
+    {
+        lock();
+        windowW=width;
+        windowH=height;
+        unlock();
+    }
+
+    unsigned int imageHeight()
+    {
+        lock();
+        unsigned int ret=yimage.height();
+        unlock();
+
+        return ret;
+    }
+
+    unsigned int imageWidth()
+    {
+        lock();
+        unsigned int ret=yimage.width();
+        unlock();
+        return ret;
+    }
+
+    bool getLastImage(yarp::sig::ImageOf<yarp::sig::PixelRgb> &i)
+    {
+        lock();
+        i=yimage;
+        unlock();
+        return true;
+    }
+
+    void lockSize()
+        {
+            origSize=true;
+        }
+
+    void unlockSize()
+        {
+            origSize=false;
+        }
+
+
+};
+
+
+#endif
