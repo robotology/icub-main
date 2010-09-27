4df2d8dd9a68191012986ecbdd3744217acbd4b1
Marco Maggiali <marco.maggiali@iit.it>
2010-09-27 14:15:11 +0000

    No commit message
    svn path=/trunk/iCub/; revision=7813

---
+++ app/kaspar/KASPAR_run.bat
@@ -0,0 +1,26 @@
+
+start iCubSkinGui --from KASPAR_rightarm.ini --useCan
+
+start iCubSkinGui --from KASPAR_righthand_top.ini --useCan
+
+start iCubSkinGui --from KASPAR_righthand_bottom.ini --useCan
+
+start iCubSkinGui --from KASPAR_leftarm.ini --useCan
+
+start iCubSkinGui --from KASPAR_lefthand_top.ini --useCan
+
+start iCubSkinGui --from KASPAR_lefthand_bottom.ini --useCan
+
+start iCubSkinGui --from KASPAR_cheeks.ini --useCan
+
+start iCubSkinGui --from KASPAR_torso.ini --useCan
+
+start iCubSkinGui --from KASPAR_rightfoot.ini --useCan
+
+start iCubSkinGui --from KASPAR_leftfoot.ini --useCan
+
+
+
+
+
+
