515b8ad1e3ec8c2c5f343923eb0cbf8be90ebebc
Ugo Pattacini <ugo.pattacini@iit.it>
2010-10-07 21:01:14 +0000

    comments changed
    svn path=/trunk/iCub/; revision=8041

--- src/modules/objectsPropertiesCollector/main.cpp
+++ src/modules/objectsPropertiesCollector/main.cpp
@@ -103,11 +103,11 @@ and the corresponding relational operator (e.g. >, <=, ==,
 ...).\n 
 The special command "[ask] (all)" returns the whole set of ids 
 present within the database. \n 
-Currently nested conditions such as (cond1) && ((cond2) || 
-(cond3)) are not handled; by the way, this is not a real 
-limitation since nested conditions can be properly expanded: 
-indeed, the previous condition can be cast back to 
-(cond1)&&(cond2) || (cond1)&&(cond3). 
+In order to simplify the implementation, nested conditions such 
+as (cond1) && ((cond2) || (cond3)) are not handled; by the way, 
+this is not a real limitation since nested conditions can be 
+properly expanded: indeed, the previous example can be cast back 
+to (cond1)&&(cond2) || (cond1)&&(cond3). 
  
 \section lib_sec Libraries 
 - YARP libraries. 
