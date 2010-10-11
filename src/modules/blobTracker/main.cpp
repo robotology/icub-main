69c90c2357bec7c114f8acbb20d15b96ff33eab7
Stephen Hart <stephen.hart@nasa.gov>
2010-10-11 13:27:11 +0000

    added dox tag and docs
    svn path=/trunk/iCub/; revision=8087

--- src/modules/blobTracker/main.cpp
+++ src/modules/blobTracker/main.cpp
@@ -1,8 +1,8 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-      
 /**
-@ingroup icub_tools
+@ingroup icub_module
+\defgroup icub_blobTracker blobTracker
 
-\defgroup blobTracker blobTracker
  
 Detects independent moving points of a grid used to sample the 
 input images. 
