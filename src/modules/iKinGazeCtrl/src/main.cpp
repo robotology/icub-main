78c5cc4d910d59ab8d383c72b120881a7359ceb8
Ugo Pattacini <ugo.pattacini@iit.it>
2010-10-23 12:03:03 +0000

    No commit message
    svn path=/trunk/iCub/; revision=8312

--- src/modules/iKinGazeCtrl/src/main.cpp
+++ src/modules/iKinGazeCtrl/src/main.cpp
@@ -401,27 +401,27 @@ public:
         Time::turboBoost();
 
         if (rf.check("ctrlName"))
-            ctrlName=rf.find("ctrlName").asString();
+            ctrlName=rf.find("ctrlName").asString().c_str();
         else
             ctrlName="iKinGazeCtrl";
 
         if (rf.check("robot"))
-            robotName=rf.find("robot").asString();
+            robotName=rf.find("robot").asString().c_str();
         else
             robotName="icub";
 
         if (rf.check("part"))
-            partName=rf.find("part").asString();
+            partName=rf.find("part").asString().c_str();
         else
             partName="head";
 
         if (rf.check("torso"))
-            torsoName=rf.find("torso").asString();
+            torsoName=rf.find("torso").asString().c_str();
         else
             torsoName="torso";
 
         if (rf.check("inertial"))
-            inertialName=rf.find("inertial").asString();
+            inertialName=rf.find("inertial").asString().c_str();
         else
             inertialName="inertial";
 
