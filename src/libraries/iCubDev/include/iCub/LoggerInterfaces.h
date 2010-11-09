df8796361529facbaebbe81aafaffab4fc6d4419
Lorenzo Natale <lorenzo.natale@iit.it>
2010-11-09 10:23:04 +0000

    new interface for logging iCubInterface status
    svn path=/trunk/iCub/; revision=8677

---
+++ src/libraries/iCubDev/include/iCub/LoggerInterfaces.h
@@ -0,0 +1,78 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/* 
+ * Copyright (C) 2010 RobotCub Consortium, European Commission FP6 Project IST-004370
+ * Authors: Lorenzo Natale
+ * website: www.robotcub.org 
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+ */
+
+/**
+ * @file LoggerInterface.h
+ * @brief Group interfaces that handles logging status messages for devices.
+ */
+
+#ifndef __LOGGERINTERFACES__
+#define __LOGGERINTERFACES__
+
+#include <yarp/dev/DeviceDriver.h>
+
+namespace yarp{
+    namespace dev {
+        class IClientLogger;
+        class IServerLogger;
+    }
+}
+
+/**
+ * @ingroup icub_icubDev
+ * Interface for a server that logs status messages.
+ *
+ */
+class IServerLogger
+{
+public:
+    /*
+    * Log data to server.
+    * @param key value identifier/address 
+    * @param data
+    * @return true/false on success failure (failure is notified if the 
+    * format of the message is unknown)
+    */
+    bool log(const std::string &key, const yarp::os::Value &data)=0;
+
+    /*
+    * Log data to server.
+    * @param data a bottle contains the data to log
+    * @return true/false on success failure (failure is notified if the 
+    * format of the message is unknown)
+    */
+    bool log(const yarp::os::Bottle &data)=0;
+};
+
+/**
+ * @ingroup icub_icubDev
+ * Interface for a client that logs status messages to a server.
+ *
+ */
+class IClientLogger
+{
+public:
+    /*
+    * Set data logger server.
+    * @param server is a pointer to a server implementing IServerLogger interface.
+    */
+    void setServerLogger(const IServerLogger *server)=0;
+};
+
+#endif
