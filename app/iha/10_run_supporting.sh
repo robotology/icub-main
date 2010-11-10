156ea415897a9e8bd257b8c2d40e3b562bf9eb67
Ugo Pattacini <ugo.pattacini@iit.it>
2010-11-10 14:38:01 +0000

    No commit message
    svn path=/trunk/iCub/; revision=8757

--- app/iha/10_run_supporting.sh
+++
@@ -1,9 +0,0 @@
-# Start supporting processes
-#./cameras.sh start
-./facedetector.sh start
-#./viewers.sh start
-./viewers.sh start_fdonly
-./portaudio.sh start
-./soundserver.sh start
-./dynamics.sh start
-
--- app/iha/20_connect_supporting.sh
+++
@@ -1,6 +0,0 @@
-# connect all supporting processes
-./facedetector.sh connect
-#./viewers.sh connect
-./viewers.sh connect_fdonly
-./soundserver.sh connect
-./dynamics.sh connect
