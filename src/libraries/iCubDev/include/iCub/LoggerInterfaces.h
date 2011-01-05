b2d32d86e90b66e39fba5940eb5023047449180d
Alessandro Scalzo <alessandro.scalzo@iit.it>
2011-01-05 09:41:15 +0000

    No commit message
    svn path=/trunk/iCub/; revision=9657

--- src/libraries/iCubDev/include/iCub/LoggerInterfaces.h
+++ src/libraries/iCubDev/include/iCub/LoggerInterfaces.h
@@ -25,14 +25,79 @@
 #ifndef __LOGGERINTERFACES__
 #define __LOGGERINTERFACES__
 
+#include <yarp/os/Value.h>
+#include <yarp/os/Semaphore.h>
 #include <yarp/dev/DeviceDriver.h>
 
 namespace yarp{
     namespace dev {
         class IClientLogger;
         class IServerLogger;
+        //class LoggerDataRef;
     }
 }
+
+#ifdef notdef
+/**
+ * @ingroup icub_icubDev
+ * Server logger data access reference.
+ * It can be created only by yarp::dev::IServerLogger
+ * and provides an alternative access to server logger data. 
+ * Its use is not mandatory, but it is more efficient than 
+ * using yarp::dev::IServerLogger::log(const std::string &key,const yarp::os::Value &data).
+ */
+class yarp::dev::LoggerDataRef
+{
+public:
+    /**
+    * Constructor. 
+    * Only friend class yarp::dev::IServerLogger can create objects of this class.
+    * @param data address of referenced data.
+    * @param flag address of the referenced data change flag.
+    */
+    LoggerDataRef(yarp::os::Value* data,bool *flag) 
+        : pData(data),pFlag(flag),pMutex(NULL)
+    {
+    }
+    
+    /**
+    * Default destructor.
+    */
+    virtual ~LoggerDataRef(){}
+    
+    /**
+    * Set a mutex reference for data access synchronization.
+    * @param external mutex address from IServerLogger. 
+    */
+    void setMutex(yarp::os::Semaphore* mutex)
+    {
+        pMutex=mutex;
+    }
+
+    /**
+    * Log data to server.
+    * The data are addressed by the key passed to 
+    * yarp::dev::IServerLogger::getDataReference(const std::string &key)
+    * when the instance was created.
+    * @param data
+    */
+    void write(const yarp::os::Value &data)
+    {
+        if (pMutex) pMutex->wait();
+        if (*pData!=data)
+        {
+            *pData=yarp::os::Value(data);
+            *pFlag=true;
+        }
+        if (pMutex) pMutex->post();
+    }
+
+protected:
+    yarp::os::Semaphore* pMutex;
+    yarp::os::Value* pData;
+    bool* pFlag;
+};
+#endif
 
 /**
  * @ingroup icub_icubDev
@@ -42,7 +107,14 @@ namespace yarp{
 class yarp::dev::IServerLogger
 {
 public:
-    /*
+    /**
+    * Get a reference to log server data.
+    * @param key value identifier/address.
+    * @return pointer to a data access interface or NULL if key is not found.
+    */
+    //virtual LoggerDataRef* getDataReference(const std::string &key)=0;
+
+    /**
     * Log data to server.
     * @param key value identifier/address 
     * @param data
@@ -51,7 +123,7 @@ public:
     */
     virtual bool log(const std::string &key,const yarp::os::Value &data)=0;
 
-    /*
+    /**
     * Log data to server.
     * @param data a bottle contains the data to log
     * @return true/false on success failure (failure is notified if the 
