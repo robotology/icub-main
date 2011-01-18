0789ecbd3617fef4010a22d88a65bf39871d5e5f
Alessandro Scalzo <alessandro.scalzo@iit.it>
2011-01-18 16:18:21 +0000

    Documentation added.
    svn path=/trunk/iCub/; revision=9851

--- src/tools/iCubInterfaceGui/iCubInterfaceGuiClient/mainClient.cpp
+++ src/tools/iCubInterfaceGui/iCubInterfaceGuiClient/mainClient.cpp
@@ -1,3 +1,51 @@
+/**
+*
+* \defgroup icubinterfacegui_gui iCubInterfaceGuiClient
+* @ingroup icub_guis
+*
+*
+* A gui that shows the full state of the robot control board networks.
+*
+*
+* \section intro_sec Description
+* 
+* Displays the status of the robot motor and sensor control board networks
+* in a hyerarchical tree view. It connects to its server part running inside
+* the \ref icub_iCubInterface iCubInterface. 
+* 
+* \image html icubinterfacegui.jpg "Screenshots: iCubInterfaceGuiClient running on Windows"
+*
+*
+* \section lib_sec Libraries
+*  - YARP
+*  - GtkMM.
+*
+* \section parameters_sec Parameters
+* It doesn't need any parameter or configuration file because
+* it receives the network configuration from the iCubInterface
+* server side in the connection phase.
+*
+* \section portsa_sec Ports Accessed
+*
+* \code
+* icubinterfacegui/server
+* \endcode
+*
+* \section tested_os_sec Tested OS
+*
+* Linux and Windows.
+*
+* \author Alessandro Scalzo
+*
+* Copyright (C) 2009 RobotCub Consortium
+*
+* CopyPolicy: Released under the terms of the GNU GPL v2.0.
+*
+* This file can be edited at main/src/tools/iCubInterfaceGui/iCubInterfaceGuiClient/mainClient.cpp.
+*/
+
+
+
 #include <gtkmm.h>
 #include <yarp/os/Network.h>
 
