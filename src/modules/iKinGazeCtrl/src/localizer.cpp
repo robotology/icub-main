9d0857c4c39eb2c859a17fb9709019416be426b1
Ugo Pattacini <ugo.pattacini@iit.it>
2011-01-23 23:47:18 +0000

    No commit message
    svn path=/trunk/iCub/; revision=9969

--- src/modules/iKinGazeCtrl/src/localizer.cpp
+++ src/modules/iKinGazeCtrl/src/localizer.cpp
@@ -79,8 +79,8 @@ Localizer::Localizer(exchangeData *_commData, const string &_localName,
     Vector N(1),  Tt(1);
     Matrix satLim(1,2);
 
-    Kp=0.003;
-    Ki=0.005;
+    Kp=0.001;
+    Ki=0.001;
     Kd=0.0;
 
     Wp=Wi=Wd=1.0;
