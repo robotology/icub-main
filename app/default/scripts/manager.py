c114e4793f00fa5fead2f7b49d66a8abd33fa2c4
Lorenzo Natale <lorenzo.natale@iit.it>
2011-02-08 18:24:34 +0000

    scrollbar patch from Philipp Robbel
    svn path=/trunk/iCub/; revision=10290

--- app/default/scripts/manager.py
+++ app/default/scripts/manager.py
@@ -22,6 +22,8 @@
 ## 10/06/2010. Added portableKill() function to ensure we have a portable
 ## way to terminate a process with windows+python < 2.6
 
+## Feb 08 2011 -- Applied scrollbarpatch from Philipp Robbel. 
+
 import sys
 import time
 import xml.dom.minidom
@@ -640,6 +642,22 @@ class App:
     def dispParameters(self, moduleData):
         w=Window(self.master, moduleData)
 
+# From http://effbot.org/zone/tkinter-autoscrollbar.htm
+class AutoScrollbar(Scrollbar):
+    # a scrollbar that hides itself if it's not needed.  only
+    # works if you use the grid geometry manager.
+    def set(self, lo, hi):
+        if float(lo) <= 0.0 and float(hi) >= 1.0:
+            # grid_remove is currently missing from Tkinter!
+            self.tk.call("grid", "remove", self)
+        else:
+            self.grid()
+        Scrollbar.set(self, lo, hi)
+    def pack(self, **kw):
+        raise TclError, "cannot use pack with this widget"
+    def place(self, **kw):
+        raise TclError, "cannot use place with this widget"
+
 def printUsage(scriptName):
     print "manager.py: python gui for parsing applications xml files"
     print "Usage:"
@@ -790,10 +808,44 @@ if __name__ == '__main__':
 #        app.display()
 
     root = Tk()
-    frame = Frame(root)
 
+    # create scrolled canvas
+    vscrollbar = AutoScrollbar(root)
+    vscrollbar.grid(row=0, column=1, sticky=N+S)
+    hscrollbar = AutoScrollbar(root, orient=HORIZONTAL)
+    hscrollbar.grid(row=1, column=0, sticky=E+W)
+    
+    canvas = Canvas(root,
+                    yscrollcommand=vscrollbar.set,
+                    xscrollcommand=hscrollbar.set)
+    canvas.grid(row=0, column=0, sticky=N+S+E+W)
+    
+    vscrollbar.config(command=canvas.yview)
+    hscrollbar.config(command=canvas.xview)
+    
+    # make the canvas expandable
+    root.grid_rowconfigure(0, weight=1)
+    root.grid_columnconfigure(0, weight=1)
+    
+    # create canvas contents
+    frame = Frame(canvas)
+    frame.rowconfigure(1, weight=1)
+    frame.columnconfigure(1, weight=1)
+    
+    # The application
     app = App(frame, applicationList[0])
+    
+    canvas.create_window(0, 0, anchor=NW, window=frame)
+    frame.update_idletasks()
+    canvas.config(scrollregion=canvas.bbox("all"))
+    # resize canvas
+    frame.update()
+    canvas['width'] = frame.winfo_width() + 10
+    height = frame.winfo_height()
+    if height > 700:
+        height = 700
+    canvas['height'] = height
 
-    frame.pack(fill=BOTH, expand=YES)
     root.title("Application Manager")
     root.mainloop()
+    
