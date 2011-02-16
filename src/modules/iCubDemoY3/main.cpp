f90700ad347ff7d9f391077947774528acb53346
Ugo Pattacini <ugo.pattacini@iit.it>
2011-02-16 22:16:17 +0000

    license fixed
    svn path=/trunk/iCub/; revision=10459

--- src/modules/iCubDemoY3/main.cpp
+++ src/modules/iCubDemoY3/main.cpp
@@ -1,8 +1,11 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
-# Copyright (C) 2008 RobotCub Consortium
-# Author: Lorenzo Natale
-# CopyPolicy: Released under the terms of the GNU GPL v2.0.
+/*
+ * Copyright (C) 2010 RobotCub Consortium
+ * Author: Lorenzo Natale
+ * CopyPolicy: Released under the terms of the GNU GPL v2.0.
+ *
+ */
 
 /**
 *
