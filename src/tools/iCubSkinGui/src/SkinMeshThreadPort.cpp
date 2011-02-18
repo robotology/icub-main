263951f7e73523b0249ceb90e3a0c8ff92a8a375
Marco Randazzo <marco.randazzo@iit.it>
2011-02-18 09:55:27 +0000

    trying to fix inconsistent line endings
    svn path=/trunk/iCub/; revision=10499

--- src/tools/iCubSkinGui/src/SkinMeshThreadPort.cpp
+++ src/tools/iCubSkinGui/src/SkinMeshThreadPort.cpp
@@ -2,10 +2,10 @@
 
 /*
  * Copyright (C) 2009 RobotCub Consortium
- * Author: Marco Maggiali marco.maggiali@iit.it, Alessandro Scalzo alessandro.scalzo@iit.it
+ * Author: Marco Randazzo marco.randazzo@iit.it Marco Maggiali marco.maggiali@iit.it, Alessandro Scalzo alessandro.scalzo@iit.it
  * CopyPolicy: Released under the terms of the GNU GPL v2.0.
  *
- */
+ */ 
 
 #include "include/SkinMeshThreadPort.h"
 
