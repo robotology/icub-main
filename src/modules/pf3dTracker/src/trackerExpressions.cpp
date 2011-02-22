bc426eea93594c1f1f70b59484c4f8587d2cea16
Matteo Taiana <mtaiana@isr.ist.utl.pt>
2011-02-22 14:59:10 +0000

    Added information on author and copyright.
    svn path=/trunk/iCub/; revision=10565

--- src/modules/pf3dTracker/src/trackerExpressions.cpp
+++ src/modules/pf3dTracker/src/trackerExpressions.cpp
@@ -1,3 +1,16 @@
+/**
+*
+* Library that controls the iCub facial expressions based on whether a tracker is tracking the object of interest or not.
+* See \ref icub_trackerexpressions \endref
+*
+* Copyright (C) 2009 RobotCub Consortium
+*
+* Author: Matteo Taiana
+*
+* CopyPolicy: Released under the terms of the GNU GPL v2.0.
+*
+*/
+
 #include <highgui.h>
 #include <iostream>
 #include <iomanip>
@@ -78,7 +91,6 @@ bool TrackerExpressions::interruptModule()
 
 //member that is repeatedly called by YARP, to give this object the chance to do something.
 //should this function return "false", the object would be terminated.
-//I already have one image, when I get here (I either acquire it in the initialization method or in the end of this same method).
 bool TrackerExpressions::updateModule()
 {
      Bottle *tracker=_inputPort.read(false);
@@ -120,193 +132,4 @@ bool TrackerExpressions::updateModule()
     return true; //continue. //in this case it means everything is fine.
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 	string stringa;
-// 	int position;
-// 	string a, b;
-// 	a=";";
-// 	b="\n";
-//        Bottle * head=_inputHeadPort.read(false);
-//        if(head!=NULL)
-//        {
-// 	  //get data and convert from degrees to radians
-// 	  head0=(head->get(0)).asDouble()*M_PI/180;
-// 	  head1=(head->get(1)).asDouble()*M_PI/180;
-// 	  head2=(head->get(2)).asDouble()*M_PI/180;
-// 	  head3=(head->get(3)).asDouble()*M_PI/180;
-// 	  head4=(head->get(4)).asDouble()*M_PI/180;
-// 	  head5=(head->get(5)).asDouble()*M_PI/180;
-// 	  //cout<<"Received head values: "<<head0<<" "<<head1<<" "<<head2<<" "<<head3<<" "<<head4<<" "<<head5<<endl;
-// 	  receivedHead = true;
-//        }
-// 
-//        Bottle * torso=_inputTorsoPort.read(false);
-//        if(torso!=NULL)
-//        {
-// 	  //get data and convert from degrees to radians
-// 	  torso0=(torso->get(0)).asDouble()*M_PI/180;
-// 	  torso1=(torso->get(1)).asDouble()*M_PI/180;
-// 	  torso2=(torso->get(2)).asDouble()*M_PI/180;
-// 	  //cout<<"received torso values: "<<torso0<<" "<<torso1<<" "<<torso2<<endl;
-// 	  receivedTorso = true;
-//        }
-// 
-//        Bottle * ballPositionIn=_inputBallPositionPort.read(false);
-//        if(ballPositionIn!=NULL)
-//        {
-// 	  //get the data and convert millimeters to meters
-// 	  ballPositionInX=(ballPositionIn->get(0)).asDouble()/1000;
-// 	  ballPositionInY=(ballPositionIn->get(1)).asDouble()/1000;
-// 	  ballPositionInZ=(ballPositionIn->get(2)).asDouble()/1000;
-// 	  ballPositionInGood=(ballPositionIn->get(6)).asDouble();
-// 	  //cout<<"Received ballPositionIn values: "<<ballPositionInX<<" "<<ballPositionInY<<" "<<ballPositionInZ<<" "<<ballPositionInGood<<endl;
-// 	  receivedBallPosition = true;
-//        }
-// 
-//       if(receivedHead && receivedTorso && receivedBallPosition)
-//       {
-// 	//DO THE PROCESSING AND THE OUTPUT
-// 
-// 
-// 	//cout<<"Received head values: "<<head0<<" "<<head1<<" "<<head2<<" "<<head3<<" "<<head4<<" "<<head5<<endl;
-//         //cout<<"received torso values: "<<torso0<<" "<<torso1<<" "<<torso2<<endl;
-// 	//cout<<"Received ballPositionIn values: "<<ballPositionInX<<" "<<ballPositionInY<<" "<<ballPositionInZ<<" "<<ballPositionInGood<<endl;
-// 	//need to check the order of these parameters ??? !!! WARNING
-// 
-// 
-// 
-//     //Vector v(8);
-// 
-//     
-//     v[0]=torso2;
-//     v[1]=torso1;
-//     v[2]=torso0;
-//     v[3]=head0;
-//     v[4]=head1;
-//     v[5]=head2;
-//     v[6]=head3;
-//     //v[7]=head4+head5; //left eye. VERSION BY UGO.
-//     //v[7]=(head4+head5)/2; //left eye. VERSION BY MANUEL, WORKED BEFORE THE CHANGE IN THE CONFIGURATION OF THE ENCODER TICKS.
-//     v[7]=head4+head5/2; //left eye, VERSION ACCORDING TO THE WIKI: http://eris.liralab.it/wiki/Vergence%2C_Version_and_Disparity
-// 
-//     //Matrix transformation;
-//     transformation = chainEyeL.getH(v);
-//     stringa= transformation.toString();
-//     position =stringa.find(a,0);
-//     stringa.replace( position, 1, b, 0, 2);
-//     position =stringa.find(a,0);
-//     stringa.replace( position, 1, b, 0, 2);
-//     position =stringa.find(a,0);
-//     stringa.replace( position, 1, b, 0, 2);
-// 	counter++;
-// 	if(counter==10)
-// 	{	
-//                 cout<<"Torso values: "<<torso0<<" "<<torso1<<" "<<torso2<<endl;
-//                 cout<<"Head values: "<<head0<<" "<<head1<<" "<<head2<<" "<<head3<<" "<<head4<<" "<<head5<<endl;
-// 		cout<<"Matrix:\n";
-//                 cout<<stringa<<endl;
-// 		counter=0;
-// 	}
-// 	//Vector eyeBallPosition(4);
-// 	//need to check this reference frame of the eye ??? !!! WARNING
-// 	eyeBallPosition[0]=ballPositionInX;
-// 	eyeBallPosition[1]=ballPositionInY;
-// 	eyeBallPosition[2]=ballPositionInZ;
-// 	eyeBallPosition[3]=1;
-// 
-// 	//Vector rootBallPosition(4);
-// 	rootBallPosition=transformation*eyeBallPosition;
-// 	//cout<<"Output:\n";
-//         //cout<<rootBallPosition.toString()<<endl;
-// 	//yarp::os::Time::delay(5);
-// 	if(rootBallPosition[0]<-0.25)
-//         {
-// 		positionConsideredSafe = true;
-// 	}
-// 	else
-//         {
-// 		positionConsideredSafe = false;
-// 	}
-// 
-// 
-// 	//send the data on the output port, ONLY if likelihood is over threshold (flag==1)
-// 	if(ballPositionInGood && positionConsideredSafe)
-// 	{
-// 	  Bottle& output=_outputBallPositionPort.prepare();
-// 	  output.clear();
-// 	  output.addDouble(rootBallPosition[0]);
-// 	  output.addDouble(rootBallPosition[1]);
-// 	  output.addDouble(rootBallPosition[2]);
-// 	  _outputBallPositionPort.write();
-// 	}
-// 
-// 
-//       }
-//       else
-//       {
-// 	//YOU DO NOT HAVE ENOUGH DATA: DO NOTHING.
-//       }
-//      
-//       yarp::os::Time::delay(0.05);
-// 
-// //     double K=0.02;
-// //     double horizontalError, horizontalSpeed, verticalError, verticalSpeed, U, V;
-// //     Bottle * estimate=_inputEstimatePort.read(false);
-// //     if(estimate!=NULL)
-// //     {
-// //         U=(estimate->get(4)).asDouble(); //PF coordinate U
-// //         V=(estimate->get(5)).asDouble(); //PF coordinate V
-// // 
-// //       horizontalError=-320+U; //positive means the ball is on the right, in the image.
-// //       verticalError=240-V; //positive means the ball is high, in the image.
-// //       //printf("horizontalError: %f\n",horizontalError);
-// //       horizontalSpeed=K*horizontalError;
-// //       verticalSpeed=K*verticalError;
-// //       int leftEye=0;
-// //       int rightEye=1;
-// //       int tilt=2;
-// //       int pan=3;
-// //       balta->move(leftEye, horizontalSpeed);
-// //       balta->move(tilt, verticalSpeed);
-// //     }
 
--- src/modules/pf3dTracker/src/trackerExpressionsMain.cpp
+++ src/modules/pf3dTracker/src/trackerExpressionsMain.cpp
@@ -1,9 +1,43 @@
-//#############################
-//# TrackerExpressionsMain.cpp #
-//#############################
-//
-//
-//
+/** 
+\defgroup icub_trackerexpressions trackerExpressions
+@ingroup icub_module   
+
+A module that controls the iCub facial expressions based on whether a tracker is tracking the object of interest or not.
+
+Copyright (C) 2009 RobotCub Consortium
+ 
+Author: Matteo Taiana
+
+CopyPolicy: Released under the terms of the GNU GPL v2.0.
+
+\section intro_sec Description
+... 
+ 
+\section lib_sec Libraries 
+... 
+ 
+\section parameters_sec Parameters
+... 
+ 
+\section portsa_sec Ports Accessed
+... 
+ 
+\section portsc_sec Ports Created 
+... 
+ 
+\section in_files_sec Input Data Files
+...
+
+\section out_data_sec Output Data Files 
+...
+
+\section tested_os_sec Tested OS
+...
+
+\author Matteo Taiana
+*/ 
+
+
 
 #include <yarp/os/Network.h>
 #include <yarp/os/Module.h>
