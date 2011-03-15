28ca9454c08c8e0f491d3b184e4074fcddd75931
Marco Randazzo <marco.randazzo@iit.it>
2011-03-15 18:35:44 +0000

    minor message fix
    svn path=/trunk/iCub/; revision=10881

--- src/modules/wholeBodyTorqueObserver/main.cpp
+++ src/modules/wholeBodyTorqueObserver/main.cpp
@@ -1381,11 +1381,11 @@ public:
     bool updateModule() 
 	{
 	    static unsigned long int alive_counter = 0;
-        //static double curr_time = Time::now();
-        //if (Time::now() - curr_time > 60)
+        static double curr_time = Time::now();
+        if (Time::now() - curr_time > 60)
         {
             printf ("wholeBodyTorqueObserver is alive! running for %ld mins.\n",++alive_counter);
-            //curr_time = Time::now();
+            curr_time = Time::now();
         }
 
 		if (inv_dyn==0) 
