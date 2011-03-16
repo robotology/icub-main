39334cca6de730049ac97150f1aefc473a5a1a35
Lorenzo Natale <lorenzo.natale@iit.it>
2011-03-16 17:00:36 +0000

    fixes
    svn path=/trunk/iCub/; revision=10900

--- app/default/scripts/icub-cluster-server.sh
+++ app/default/scripts/icub-cluster-server.sh
@@ -3,12 +3,9 @@
 ### Simple startup script for "yarp server".
 # nat
 
-if [ X$USER == "Xicub" ];   
-    then 
-    cmd="bash -l -c"; 
-else    
-    cmd="su - icub -c"; 
-fi;
+cmd="bash -l -c"; 
+
+echo "Parameters: $1 $2"
 
 case "$1" in
 	start)
@@ -16,17 +13,17 @@ case "$1" in
 			yarpserver) 
 				echo "Starting up yarp server"
 				$cmd "cp -f $YARP_DIR/bin/yarpserver /tmp/yarpserver"
-				$cmd "/tmp/yarpserver 2>&1 2>/dev/null &"
+				$cmd "/tmp/yarpserver 2>&1 1>/dev/null &"
 				echo "done!"
 				;;
 			*) #yarpserver3 is the default
 				echo "Starting up yarp server"
 				$cmd "cp -f $YARP_DIR/bin/yarpserver3 /tmp/yarpserver3"
-				$cmd "/tmp/yarpserver3 --portdb /tmp/ports.db --subdb /tmp/subs.db 2>&1 2>/dev/null &"
+				$cmd "/tmp/yarpserver3 --portdb /tmp/ports.db --subdb /tmp/subs.db 2>&1 1>/dev/null &"
 				echo "done!"
 				;;
 		esac
-	
+		;;
 	stop)
 		echo "Stopping yarp server"
 		$cmd "killall yarpserver3"
