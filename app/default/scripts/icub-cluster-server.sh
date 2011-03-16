c4370df5a6ac2b4ed70c0dd6804e39a116a7822f
Lorenzo Natale <lorenzo.natale@iit.it>
2011-03-16 15:42:35 +0000

    handle yarpserver
    svn path=/trunk/iCub/; revision=10898

--- app/default/scripts/icub-cluster-server.sh
+++ app/default/scripts/icub-cluster-server.sh
@@ -12,20 +12,31 @@ fi;
 
 case "$1" in
 	start)
-		echo "Starting up yarp server"
-		$cmd "cp -f $YARP_ROOT/bin/yarpserver3 /tmp/yarpserver3"
-		$cmd "/tmp/yarpserver3 --portdb /tmp/ports.db --subdb /tmp/subs.db 2>&1 2>/dev/null &"
-		echo "done!"
-		;;
-
+		case "$2" in 
+			yarpserver) 
+				echo "Starting up yarp server"
+				$cmd "cp -f $YARP_DIR/bin/yarpserver /tmp/yarpserver"
+				$cmd "/tmp/yarpserver 2>&1 2>/dev/null &"
+				echo "done!"
+				;;
+			*) #yarpserver3 is the default
+				echo "Starting up yarp server"
+				$cmd "cp -f $YARP_DIR/bin/yarpserver3 /tmp/yarpserver3"
+				$cmd "/tmp/yarpserver3 --portdb /tmp/ports.db --subdb /tmp/subs.db 2>&1 2>/dev/null &"
+				echo "done!"
+				;;
+		esac
+	
 	stop)
 		echo "Stopping yarp server"
 		$cmd "killall yarpserver3"
+		$cmd "killall yarpserver"
 		;;
 
 	kill)
 		echo "Killing yarp server"
 		$cmd "killall -9 yarpserver3"
+		$cmd "killall -9 yarpserver"
 		;;
 	*)
 		echo "start stop kill"
