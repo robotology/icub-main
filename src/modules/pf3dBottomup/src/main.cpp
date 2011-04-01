b6a0c7a1972b69cd4fb4301e4b26f4ebc1d522ed
Matteo Taiana <mtaiana@isr.ist.utl.pt>
2011-04-01 18:02:57 +0000

    Documented the Bottomup module with doxygen.
    svn path=/trunk/iCub/; revision=11132

--- src/modules/pf3dBottomup/src/main.cpp
+++ src/modules/pf3dBottomup/src/main.cpp
@@ -1,16 +1,23 @@
-/*
- * A bottom-up approach for generating particles for the "pf3dTracker" module
- *
- * Copyright (C) 2010 RobotCub Consortium
- *
- * Author: Martim Brandao
- * Note: Should you use or reference my work on your own research, please let me know (mbrandao _AT_ isr.ist.utl.pt)
- *
- * Image sequence as input, N particles (3D position of balls) as output.
- *
- * CopyPolicy: Released under the terms of the GNU GPL v2.0.
- *
- */
+/** 
+\defgroup icub_pf3dBottomup pf3dBottomup
+@ingroup icub_module   
+
+A bottom-up approach for generating particles for the \ref icub_pf3dTracker "pf3dTracker" module
+
+Copyright (C) 2010 RobotCub Consortium
+ 
+Author: Martim Brandao
+
+Note: Should you use or reference my work on your own research, please let me know (mbrandao _AT_ isr.ist.utl.pt)
+
+Image sequence as input, N particles (3D position of balls) as output.
+
+CopyPolicy: Released under the terms of the GNU GPL v2.0.
+
+\section intro_sec Description
+<b>For an explanation on how to configure the tracker and bottom up modules, how to connect them, how to run them through the application manager, etc., please have a look at <A HREF="http://mediawiki.isr.ist.utl.pt/wiki/3D_ball_tracker">this page</A>.</b>
+
+*/
 
 // yarp
 #include <yarp/os/Network.h>
