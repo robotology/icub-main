cb45ef991cc3e632fa0017e6f5aa2e6809094ed5
Ugo Pattacini <ugo.pattacini@iit.it>
2011-06-26 11:31:10 +0000

    doc updated
    svn path=/trunk/iCub/; revision=12288

--- src/libraries/perceptiveModels/include/iCub/perception/models.h
+++ src/libraries/perceptiveModels/include/iCub/perception/models.h
@@ -20,7 +20,7 @@
  * @defgroup Models Models
  * @ingroup PerceptiveModels 
  *  
- * Abstract layers for dealing with perceptive models framework.
+ * Classes for distributing perceptive frameworks.
  *
  * @author Ugo Pattacini 
  *  
@@ -30,8 +30,10 @@
  *
  * @section intro_sec Description
  *
- * ... 
- *  
+ * A Model is meant to be a container that encapsulates a 
+ * collection of nodes representing the perceptive architecture 
+ * and provides the higher layers suitable routines to perform 
+ * data acquisition. 
  */ 
 
 #ifndef __PERCEPTIVEMODELS_MODELS_H__
@@ -55,29 +57,99 @@ namespace perception
 
 /**
 * @ingroup Models
-*
+*  
+* An abstract class that provides basic methods for interfacing 
+* with the data acquisition. 
 */
 class Model
 {
 protected:
     std::string name;
-
     std::map<std::string,Node*> nodes;
 
 public:
+    /**
+    * Constructor. 
+    */
     Model();
+
+    /**
+    * Retrieve the model name. 
+    * @return a string containing the model name. 
+    */
     std::string getName() const { return name; }
 
+    /**
+    * Attach a node object to the model. 
+    * @param node the node object to be attached, so that the mode 
+    *             can employ it.
+    */
     void attachNode(Node &node);
+
+    /**
+    * Retrieve an attached node by its name.
+    * @param name the name of the attached node. 
+    * @return the pointer to the node. 
+    */
     Node* getNode(const std::string &name) const;
 
+    /**
+    * Configure the model taking its parameters from a Property 
+    * object. 
+    * @param options a Property containing the configuration 
+    *                parameters.
+    * @return true/false on success/failure.
+    */
     virtual bool fromProperty(const yarp::os::Property &options) = 0;
+
+    /**
+    * Return a Property representation of all the model parameters.
+    * @param options a Property filled with the configuration 
+    *                parameters.
+    */
     virtual void toProperty(yarp::os::Property &options) const = 0;
+
+    /**
+    * Similar to the @see toProperty method but it operates on 
+    * output streams (e.g. string, ofstream, ...). It allows to 
+    * better manage the storing of the configuration over files. 
+    * @param str the reference to the output stream. 
+    * @return true/false on success/failure. 
+    */
     virtual bool toStream(std::ostream &str) const = 0;
+
+    /**
+    * Some kinds of models need to be calibrated to properly 
+    * operate. This method executes the calibration phase. 
+    * @param options a Property containing the calibration 
+    *                parameters.
+    * @return true/false on success/failure. 
+    * @note Usually this method should call the corresponding 
+    *       calibrate method of all the nodes attached to the model.
+    * @note For models that do not envisage any calibration, this 
+    *       method should always return true.
+    */
     virtual bool calibrate(const yarp::os::Property &options) = 0;
+
+    /**
+    * Return the internal status of the calibration.
+    * @return true/false on calibrated/uncalibrated-failure.
+    * @note For models that do not envisage any calibration, this 
+    *       method should always return true.
+    */
     virtual bool isCalibrated() const = 0;
+
+    /**
+    * Provide the higher layers with the model output computed over 
+    * the attached sensors. 
+    * @param out a Value containing the model output.
+    * @return true/false on success/failure. 
+    */
     virtual bool getOutput(yarp::os::Value &out) const = 0;
 
+    /**
+    * Destructor. 
+    */
     virtual ~Model() { }
 };
 
