1ca6608e0ff767e3643ad62ed04a099bdd9f0371
Ugo Pattacini <ugo.pattacini@iit.it>
2011-06-26 16:49:21 +0000

    No commit message
    svn path=/trunk/iCub/; revision=12292

---
+++ src/tools/iSpeak/main.cpp
@@ -0,0 +1,213 @@
+/* 
+ * Copyright (C) 2011 RobotCub Consortium, European Commission FP6 Project IST-004370
+ * Author: Ugo Pattacini
+ * email:  ugo.pattacini@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+#include <yarp/os/Network.h>
+#include <yarp/os/RFModule.h>
+#include <yarp/os/BufferedPort.h>
+#include <yarp/os/Port.h>
+#include <yarp/os/Semaphore.h>
+#include <yarp/os/RateThread.h>
+#include <yarp/os/Time.h>
+
+#include <stdio.h>
+#include <string>
+#include <deque>
+
+using namespace std;
+using namespace yarp::os;
+
+
+class iSpeak : public BufferedPort<Property>,
+               public RateThread
+{
+    string name;
+    string robot;    
+    Port emotions;
+
+    deque<Property> buffer;
+    Semaphore mutex;
+
+    double t0;
+    double dt;
+    string state;
+    bool speaking;
+
+public:
+    iSpeak() : RateThread(200)
+    {
+        speaking=false;
+        state="sur";
+    }
+
+    void configure(ResourceFinder &rf)
+    {
+        name=rf.find("name").asString().c_str();
+        robot=rf.find("robot").asString().c_str();
+    }
+
+    bool threadInit()
+    {
+        open(("/"+name).c_str());
+        useCallback();
+
+        emotions.open(("/"+name+"/emotions").c_str());
+        Network::connect(emotions.getName().c_str(),("/"+robot+"/face/emotions").c_str());
+
+        return true;
+    }
+
+    void threadRelease()
+    {
+        interrupt();
+        close();
+
+        emotions.interrupt();
+        emotions.close();
+    }
+
+    void onRead(Property &request)
+    {
+        mutex.wait();
+        buffer.push_back(request);
+        mutex.post();
+    }
+
+    void speak(Property &request)
+    {
+        string phrase=request.check("phrase",Value("I have received nothing to say")).asString().c_str();
+        double time=request.check("time",Value(1.0)).asDouble();
+
+    #ifdef LINUX
+        system(("echo \""+phrase+"\" | festival --batch --tts &").c_str());
+    #endif
+
+        t0=Time::now();
+        dt=time;
+        speaking=true;
+    }    
+
+    void run()
+    {
+        mutex.wait();
+        if (buffer.size()>0)
+        {            
+            speak(buffer.front());
+            buffer.pop_front();
+        }
+        mutex.post();
+
+        if (speaking)
+        {
+            if (state=="sur")
+                state="hap";
+            else
+                state="sur";
+
+            if (Time::now()-t0>dt)
+            {
+                state="hap";
+                speaking=false;
+            }
+
+            Bottle cmd, reply;
+            cmd.addVocab(Vocab::encode("set"));
+            cmd.addVocab(Vocab::encode("mou"));
+            cmd.addVocab(Vocab::encode(state.c_str()));
+            emotions.write(cmd,reply);
+        }        
+    }
+    
+    bool isSpeaking() const
+    {
+        return speaking;
+    }    
+};
+
+
+class Launcher: public RFModule
+{
+protected:
+    iSpeak speaker;
+    Port   rpc;
+
+public:
+    bool configure(ResourceFinder &rf)
+    {
+        Time::turboBoost();
+
+        speaker.configure(rf);
+        if (!speaker.start())
+            return false;
+
+        string name=rf.find("name").asString().c_str();
+        rpc.open(("/"+name+"/rpc").c_str());
+        attach(rpc);
+
+        return true;
+    }
+
+    bool close()
+    {
+        rpc.interrupt();
+        rpc.close();
+
+        speaker.stop();
+
+        return true;
+    }
+
+    bool respond(const Bottle &command, Bottle &reply)
+    {
+        if (command.get(0).asVocab()==VOCAB4('s','t','a','t'))
+        {
+            reply.addString(speaker.isSpeaking()?"speaking":"quiet");
+            return true;
+        }
+        else
+            return RFModule::respond(command,reply);
+    }
+
+    double getPeriod()
+    {
+        return 1.0;
+    }
+
+    bool updateModule()
+    {
+        return true;
+    }
+};
+
+
+int main(int argc, char *argv[])
+{
+    Network yarp;
+    if (!yarp.checkNetwork())
+        return -1;
+
+    ResourceFinder rf;
+    rf.setVerbose(true);
+    rf.setDefault("name","iSpeak");
+    rf.setDefault("robot","icub");
+    rf.configure("ICUB_ROOT",argc,argv);
+
+    Launcher launcher;
+    return launcher.runModule(rf);
+}
+
+
