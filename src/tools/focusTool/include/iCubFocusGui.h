f1d838f1096ad4f4c3bfd219b4ba4cd005564be2
Alessandro Scalzo <alessandro.scalzo@iit.it>
2011-06-28 13:19:17 +0000

    Added camera focus calibration tool.
    svn path=/trunk/iCub/; revision=12331

---
+++ src/tools/focusTool/include/iCubFocusGui.h
@@ -0,0 +1,139 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Copyright (C) 2010 RobotCub Consortium
+ * Author: Alessandro Scalzo alessandro.scalzo@iit.it
+ * CopyPolicy: Released under the terms of the GNU GPL v2.0.
+ */
+
+#ifndef __GTKMM_ICUB_INTERFACE_GUI_CLIENT_H__
+#define __GTKMM_ICUB_INTERFACE_GUI_CLIENT_H__
+
+#include <gtkmm.h>
+#include <yarp/os/all.h>
+#include <yarp/dev/RemoteFrameGrabber.h>
+
+class iCubFocusGuiThread : public yarp::os::Thread
+{
+public:
+    iCubFocusGuiThread(yarp::os::ConstString& loc,yarp::os::ConstString& rem)
+    {
+        mR=mD=0.0;
+
+        // grabber
+        yarp::os::Property config;
+		config.put("local",loc);
+        config.put("remote",rem);
+        pFG=new yarp::dev::RemoteFrameGrabber();
+        pFG->open(config);
+    }
+
+    ~iCubFocusGuiThread()
+    {
+        if (pFG)
+        {
+            pFG->close();
+            delete pFG;
+            pFG=NULL;
+        }
+    }
+
+    double getValue(){ return mR; }
+    
+    void run()
+    { 
+        if (pFG->getImage(img))
+        {
+            int mW=img.width();
+            int mH=img.height();
+            unsigned char *p=img.getRawImage();
+            
+            double Rx,Gx,Bx;
+            double Ry,Gy,By;
+            double D=0.0;
+
+            for (int y=1; y<mH-1; ++y)
+            {
+                for (int x=1; x<mW-1; ++x)
+                {
+                    Rx=p[R(x+1,y-1)]-p[R(x-1,y-1)]+2*(p[R(x+1,y)]-p[R(x-1,y)])+p[R(x+1,y+1)]-p[R(x-1,y+1)];
+                    Ry=p[R(x-1,y+1)]-p[R(x-1,y-1)]+2*(p[R(x,y+1)]-p[R(x,y-1)])+p[R(x+1,y+1)]-p[R(x+1,y-1)];
+
+                    Gx=p[G(x+1,y-1)]-p[G(x-1,y-1)]+2*(p[G(x+1,y)]-p[G(x-1,y)])+p[G(x+1,y+1)]-p[G(x-1,y+1)];
+                    Gy=p[G(x-1,y+1)]-p[G(x-1,y-1)]+2*(p[G(x,y+1)]-p[G(x,y-1)])+p[G(x+1,y+1)]-p[G(x+1,y-1)];
+
+                    Bx=p[B(x+1,y-1)]-p[B(x-1,y-1)]+2*(p[B(x+1,y)]-p[B(x-1,y)])+p[B(x+1,y+1)]-p[B(x-1,y+1)];
+                    By=p[B(x-1,y+1)]-p[B(x-1,y-1)]+2*(p[B(x,y+1)]-p[B(x,y-1)])+p[B(x+1,y+1)]-p[B(x+1,y-1)];
+
+                    D+=Rx*Rx+Gx*Gx+Bx*Bx+Ry*Ry+Gy*Gy+By*By;
+                }
+            }
+
+            if (mD<D) mD=D;
+
+            mR=D/mD;
+
+            mSigWindow();  
+        }
+    }
+    
+    Glib::Dispatcher mSigWindow;
+    
+protected:
+    int mW,mH;
+    
+    double mR;
+    double mD;
+
+    yarp::dev::RemoteFrameGrabber *pFG;
+    yarp::sig::ImageOf<yarp::sig::PixelRgb> img;
+
+    inline int R(int x,int y){ return (y*mW+x)*3;   }
+    inline int G(int x,int y){ return (y*mW+x)*3+1; }
+    inline int B(int x,int y){ return (y*mW+x)*3+2; }
+};
+
+class iCubFocusGuiWindow : public Gtk::Window
+{
+public:
+    iCubFocusGuiWindow(yarp::os::ConstString& loc,yarp::os::ConstString& rem)
+    {
+        // gui
+        set_title("Camera focus GUI");
+        set_border_width(5);
+        set_default_size(400,300);
+
+        add(mVBox);
+        mVBox.pack_start(mPB);
+        show_all();
+
+        yarp::os::Time::delay(1.0);
+        
+        mThread=new iCubFocusGuiThread(loc,rem);
+        mThread->mSigWindow.connect(sigc::mem_fun(*this,&iCubFocusGuiWindow::run));
+        mThread->start();
+    }
+
+    virtual ~iCubFocusGuiWindow()
+    {
+        if (mThread)
+        {
+            mThread->stop();
+            delete mThread;
+            mThread=NULL;
+        }
+    }
+
+    void run()
+    {
+        mPB.set_fraction(mThread->getValue());
+    }
+
+protected:
+    double mD;
+    Gtk::VBox mVBox;
+    Gtk::ProgressBar mPB;
+    iCubFocusGuiThread* mThread;
+};
+
+#endif
---
+++ src/tools/focusTool/src/main.cpp
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2010 RobotCub Consortium
+ * Author: Alessandro Scalzo alessandro.scalzo@iit.it
+ * CopyPolicy: Released under the terms of the GNU GPL v2.0.
+ *
+ */
+
+/**
+*
+* \defgroup icubinterfacegui_gui iCubInterfaceGuiClient
+* @ingroup icub_guis
+*
+*
+* A gui that shows the full state of the robot control board networks.
+*
+*
+* \section intro_sec Description
+* 
+* Displays the status of the robot motor and sensor control board networks
+* in a hyerarchical tree view. It connects to its server part running inside
+* the \ref icub_iCubInterface iCubInterface. 
+* 
+* \image html icubinterfacegui.jpg "Screenshots: iCubInterfaceGuiClient running on Windows"
+*
+*
+* \section lib_sec Libraries
+*  - YARP
+*  - GtkMM.
+*
+* \section parameters_sec Parameters
+* It doesn't need any parameter or configuration file because
+* it receives the network configuration from the iCubInterface
+* server side in the connection phase.
+*
+* \section portsa_sec Ports Accessed
+*
+* \code
+* icubinterfacegui/server
+* \endcode
+*
+* \section tested_os_sec Tested OS
+*
+* Linux and Windows.
+*
+* \author Alessandro Scalzo
+*
+* Copyright (C) 2010 RobotCub Consortium
+*
+* CopyPolicy: Released under the terms of the GNU GPL v2.0.
+*
+* This file can be edited at main/src/tools/iCubInterfaceGui/iCubInterfaceGuiClient/mainClient.cpp.
+*/
+
+
+
+#include <gtkmm.h>
+#include <yarp/os/ConstString.h>
+#include <yarp/os/Network.h>
+
+#include "iCubFocusGui.h"
+
+int main(int argc, char *argv[])
+{
+    Glib::thread_init();
+    Gtk::Main kit(argc,argv);
+
+	yarp::os::Network yarp;
+
+    yarp::os::ConstString loc(argv[1]);
+    yarp::os::ConstString rem(argv[2]);
+    iCubFocusGuiWindow client(loc,rem);
+
+    kit.run(client);
+
+    return 0;
+}
