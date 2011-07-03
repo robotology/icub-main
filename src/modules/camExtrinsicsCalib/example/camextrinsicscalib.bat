926c946d7a5d93a64157cd856c46f893c30411b3
Ugo Pattacini <ugo.pattacini@iit.it>
2011-07-03 12:11:24 +0000

    camExtrinsicsCalib moved under contrib/src/misc
    svn path=/trunk/iCub/; revision=12438

--- src/modules/camExtrinsicsCalib/example/camextrinsicscalib.bat
+++
@@ -1,3 +0,0 @@
-start "CAMEXTRINSICSCALIB" cmd /c c:/iCub/src/camextrinsicscalib/debug/camextrinsicscalib.exe --file camextrinsicscalib.ini --groupPattern CALIBRATION_PATTERN --groupIntrinsics INTRINSICS
-
- 
