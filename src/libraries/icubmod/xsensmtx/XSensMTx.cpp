d53004fefcff43e2221c3abf636d3962537bcceb
Lorenzo Natale <lorenzo.natale@iit.it>
2011-08-29 13:36:35 +0000

    Rasu --> Rusu
    svn path=/trunk/iCub/; revision=13286

--- src/libraries/icubmod/xsensmtx/XSensMTx.cpp
+++ src/libraries/icubmod/xsensmtx/XSensMTx.cpp
@@ -1,7 +1,7 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
 /*
- * Copyright (C) 2006 Radu Bogdan Rasu, Alexis Maldonado
+ * Copyright (C) 2006 Radu Bogdan Rusu, Alexis Maldonado
  * CopyPolicy: Released under the terms of the GNU GPL v2.0.
  *
  */
