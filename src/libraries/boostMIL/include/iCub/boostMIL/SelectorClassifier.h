0f46aecf8f2da3517459d1f734a8df2cec1b4b0b
Carlo Ciliberto <carlo.ciliberto@iit.it>
2011-10-24 17:24:34 +0000

    new Library boostMIL
    svn path=/trunk/iCub/; revision=14161

---
+++ src/libraries/boostMIL/include/iCub/boostMIL/SelectorClassifier.h
@@ -0,0 +1,139 @@
+
+/* 
+ * Copyright (C) 2010 RobotCub Consortium, European Commission FP6 Project IST-004370
+ * Author: Carlo Ciliberto
+ * email:  carlo.ciliberto@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+/**
+ * \defgroup Selector Classifier
+ *  
+ * @ingroup boostMIL
+ *
+ * A weak classifier used for online learning which allows to keep perform feature selection. Originally 
+ * proposed in [<a href="http://www.google.it/url?sa=t&rct=j&q=online%20boosting%20an%20vision&source=web&cd=1&ved=0CCMQFjAA&url=http%3A%2F%2Fwww.icg.tu-graz.ac.at%2FMembers%2Fhgrabner%2Fpub_hgrabner%2Fgrabner2006OnlineBoosting.pdf%2Fat_download%2Ffile&ei=F4OlToC2A6H64QTVs4HuBA&usg=AFQjCNH6lmEE4tff6UhkMR-IUPL9-DzhIQ&sig2=XB_8z7O0ES1P5ecedlpvHw">PDF</a>] 
+ *
+ *
+ * \author Carlo Ciliberto
+ * 
+ * Copyright (C) 2010 RobotCub Consortium
+ *
+ * CopyPolicy: Released under the terms of the GNU GPL v2.0. 
+ *  
+ */ 
+
+
+
+#ifndef __SELECTOR_CLASSIFIER__
+#define __SELECTOR_CLASSIFIER__
+
+#include <vector>
+
+#include "WeakClassifier.h"
+
+
+
+namespace iCub
+{
+
+namespace boostMIL
+{
+
+
+
+class SelectorClassifier: public WeakClassifier
+{
+private:
+    /**
+    * Size allowed for the pool of WeakClassifiers
+    */
+    int                                 pool_size;
+    
+    /**
+    * Actual list (or pool) of WeakClassifiers allowed to the Selector.
+    */
+    std::vector<WeakClassifier*>        pool;
+
+    /**
+    * index to the selected element in the pool.
+    */
+    int                                 selected;
+
+    std::vector<double>                 correct;
+    std::vector<double>                 wrong;
+
+    //Give the new WeakClassifiers some time to adapt to the data before consider them.
+    std::vector<int>                    asleep;
+    int                                 sleep_time;
+
+    //absolute and relative thresholds to detect bad performing WeakClassifiers
+    double                              abs_thresh;
+    double                              rel_thresh;
+
+    //pointer to the function space passed during construction
+    std::deque<WeakClassifier*>         *function_space;
+
+    //private methods
+    virtual void                        initResource();
+    virtual bool                        substitute(const double &e) const;
+    virtual WeakClassifier              *randomSample();
+
+    //private and unimplemented on purpose (for now)
+    virtual std::list<WeakClassifier*>  *pack       (const Inputs *input)           const   {return NULL;}
+    virtual WeakClassifier              *create     ()                              const   {return NULL;}
+
+
+
+public:
+    SelectorClassifier      (const std::string &_type, yarp::os::ResourceFinder &_resource, std::deque<WeakClassifier*> *_function_space);
+
+    ~SelectorClassifier     ()      {clear();}
+
+
+    //public methods
+    virtual bool        isReady     ();
+    virtual void        clear       ();
+
+    /**
+    * Encodes the classifier in a single string
+    *
+    * @return a string encoding the current classifier form
+    */
+    virtual std::string                  toString    () const;
+
+    /**
+    * Loads a classifier from a string
+    *
+    * @param the string encoding the classifier to be loaded
+    */
+    virtual void                         fromString  (const std::string &str);
+
+    /**
+    * Classifies the given input.
+    *
+    * @param input the input to classify.
+    * @return 1,-1 or 0 if the classifier classifies the input (respectively): 
+    *         positive, negative or undetermined (e.g. the classifier is not ready to classify yet).
+    */
+    virtual int         classify    (const Inputs *input, Output *output)   const;
+    virtual void        update      (const Inputs *input, double &weight);
+};
+
+
+}
+
+}
+
+#endif
---
+++ src/libraries/boostMIL/src/SelectorClassifier.cpp
@@ -0,0 +1,283 @@
+
+/* 
+ * Copyright (C) 2010 RobotCub Consortium, European Commission FP6 Project IST-004370
+ * Author: Carlo Ciliberto
+ * email:  carlo.ciliberto@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+#include <iostream>
+#include <string>
+#include <deque>
+#include <math.h>
+
+
+#include <fstream>
+#include <cassert>
+#include <stdexcept>
+#include <sstream>
+
+#include <yarp/os/Bottle.h>
+#include <yarp/math/Rand.h>
+
+#include <iCub/boostMIL/SelectorClassifier.h>
+#include <iCub/boostMIL/ClassifierFactory.h>
+
+
+
+using namespace iCub::boostMIL;
+
+
+SelectorClassifier::SelectorClassifier(const std::string &_type, yarp::os::ResourceFinder &_resource, std::deque<WeakClassifier*> *_function_space)
+    :WeakClassifier(_type,_resource),function_space(_function_space)
+{
+    //if a function space is not provided. the sytem fails.
+    if(function_space == NULL)
+    {
+        std::ostringstream buffer;
+        buffer << "Error! Function Space not available!";
+        throw std::runtime_error(buffer.str());
+    }
+
+    initResource();
+    //initialize the random generator.
+    yarp::math::Rand::init();
+}
+
+
+
+void SelectorClassifier::initResource()
+{
+    clear();
+
+    if(resource.check(type.c_str()))
+    {
+        yarp::os::Bottle data = resource.findGroup(type.c_str());
+
+        pool_size = data.check("pool_size",yarp::os::Value(1)).asInt();
+        abs_thresh = data.check("abs_thresh",yarp::os::Value(1.0)).asDouble();
+        rel_thresh = data.check("rel_thresh",yarp::os::Value(1e10)).asDouble();
+        sleep_time = data.check("sleep_time",yarp::os::Value(0)).asInt();
+    }
+    else
+    {
+        pool_size = 1;
+        abs_thresh = 1.0;
+        rel_thresh = 1e10;
+        sleep_time = 0;
+    }
+
+    //initialize the vectors
+    for(int i = 0; i < pool_size; i++)
+    {
+        correct.push_back(1.0);
+        wrong.push_back(1.0);
+        asleep.push_back(sleep_time);
+    }
+}
+
+
+void SelectorClassifier::clear()
+{
+    selected = -1;
+
+    pool_size = 0;
+    while(pool.size())
+    {
+        delete pool.back();
+        pool.pop_back();
+    }
+
+    correct.clear();
+    wrong.clear();
+    asleep.clear();
+    sleep_time = 0;
+    abs_thresh = 0.0;
+    rel_thresh = 0.0;
+}
+
+
+int SelectorClassifier::classify(const Inputs *input, Output *output) const
+{
+    //The selector abstains from classifing if: it has not been initialized
+    if(!ready || selected == -1 || asleep[selected] >= 0)
+        return 0;
+    else
+        return pool[selected]->classify(input,output);
+}
+
+bool SelectorClassifier::isReady()
+{
+    ready = true;
+    //if the selector is not empty
+    if(pool_size)
+    {
+        //fill the pool with new WeakClassifiers with the elements in the function_space list
+        while(pool.size() < (unsigned) pool_size && function_space->size())
+        {
+            pool.push_back(function_space->front());
+            function_space->pop_front();
+        }
+
+        //if the selector is still not completly filled it is not ready
+        if(pool.size() != pool_size)
+            ready = false;
+
+        for(unsigned int i = 0; i < pool.size(); i++)
+            if(!pool[i]->isReady()) ready = false;
+    }
+    else
+        ready = false;
+
+    return ready;
+}
+
+
+
+
+void SelectorClassifier::update(const Inputs *input, double &weight)
+{
+    if(isReady())
+    {
+        error = 1.0;
+        std::vector<double> errors(pool.size());
+
+        for(unsigned int i = 0; i < pool.size(); i++)
+        {
+            pool[i]->update(input,weight);
+
+            //estimate the (online) error
+            pool[i]->classify(input)==input->getLabel()?correct[i]++:wrong[i]++;
+
+            errors[i] = wrong[i]/(correct[i]+wrong[i]);
+            asleep[i]--;
+
+            //keep track of the best WeakClassifier
+            if(asleep[i] < 0 && errors[i] < error)
+            {
+                error = errors[i];
+                selected = i;
+            }
+        }
+
+        //handle unconsistencies of the trained WeakClassifiers
+        if(selected>0 && !asleep[selected] && (error < 1e-6 || error > 0.5))
+        {
+            //debug
+            std::cout << "this selector has best error " << this->error << std::endl;
+        }
+
+        //if needed, substitute bad performing Weak Classifiers
+        for(unsigned int i = 0; i < pool.size(); i++)
+        {
+            if(asleep[i] < 0 && function_space->size() && substitute(errors[i]) && i != selected)
+            {
+                delete pool[i];
+                pool[i] = randomSample();
+                correct[i] = 1.0;
+                wrong[i] = 1.0;
+                asleep[i] = sleep_time;
+            }
+        }
+    }
+}
+
+
+
+bool SelectorClassifier::substitute(const double &e) const
+{
+    return (e > rel_thresh*error || e > abs_thresh);
+}
+
+
+
+WeakClassifier *SelectorClassifier::randomSample()
+{
+    int idx = (int) floor(yarp::math::Rand::scalar(0,function_space->size()));
+
+    //avoid index unconsistency
+    if(idx==function_space->size()) idx--;
+
+    WeakClassifier *rnd_h = function_space->at(idx);
+
+    //Erase the sampled element from the function_space
+    function_space->erase(function_space->begin() + idx);
+
+    return rnd_h;
+}
+
+
+
+
+std::string  SelectorClassifier::toString() const
+{
+    if(ready)
+    {
+        std::stringstream strstr;
+
+        strstr << "(type " << type << ") ";
+        strstr << "(selected " << selected << ") ";
+
+        strstr << "(abs_thresh " << abs_thresh <<") ";
+        strstr << "(rel_thresh " << rel_thresh <<") ";
+        strstr << "(sleep_time " << sleep_time <<") ";
+
+        strstr << "(pool (";
+        for(unsigned int i=0; i<pool.size(); i++)
+        {
+            strstr << "(";
+            strstr << "(correct " << correct[i] << ") ";
+            strstr << "(wrong " << wrong[i] << ") ";
+            strstr << "(asleep " << asleep[i] << ") ";
+            strstr << "(weak_classifier (" << pool[i]->toString() << "))";
+            strstr << ") ";
+        }
+        strstr << "))";
+
+        return strstr.str().c_str();
+    }
+    else
+        return "";
+}
+
+
+
+void   SelectorClassifier::fromString(const std::string &str)
+{
+    clear();
+
+    yarp::os::Bottle bLoader(str.c_str());
+
+    type=bLoader.find("type").asString().c_str();
+    selected=bLoader.find("selected").asInt();
+    abs_thresh=bLoader.find("abs_thresh").asDouble();
+    rel_thresh=bLoader.find("rel_thresh").asDouble();
+    sleep_time=bLoader.find("sleep_time").asInt();
+
+    yarp::os::Bottle *bPool=bLoader.find("pool").asList();
+
+    pool_size=bPool->size();
+    for(int i=0; i<pool_size; i++)
+    {
+        yarp::os::Bottle *bElement=bPool->get(i).asList();
+        correct.push_back(bElement->find("correct").asDouble());
+        wrong.push_back(bElement->find("wrong").asDouble());
+        asleep.push_back(bElement->find("asleep").asInt());
+
+        yarp::os::Bottle *bWL=bElement->find("weak_classifier").asList();
+        pool.push_back(ClassifierFactory::instance().create(bWL->find("type").asString().c_str()));
+        pool.back()->fromString(bWL->toString().c_str());
+    }
+}
+
