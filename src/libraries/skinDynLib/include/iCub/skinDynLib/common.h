68b99e5506ff81cc4aee5a49639bf8b76dc12235
Andrea Del Prete <andrea.delprete@iit.it>
2011-11-10 10:16:05 +0000

    Reduced the number of skin parts from 7 to 3.
    svn path=/trunk/iCub/; revision=14345

--- src/libraries/skinDynLib/include/iCub/skinDynLib/common.h
+++ src/libraries/skinDynLib/include/iCub/skinDynLib/common.h
@@ -40,10 +40,13 @@ enum VerbosityLevel { NO_VERBOSE, VERBOSE, MORE_VERBOSE};
 enum BodyPart {UNKNOWN_BODY_PART, HEAD, TORSO, LEFT_ARM, RIGHT_ARM, LEFT_LEG, RIGHT_LEG, ALL_BODY_PARTS, BODY_PART_SIZE};
 const std::string BodyPart_s[] = {"unknown_body_part", "head", "torso", "left_arm", "right_arm", "left_leg", "right_leg", "all_body_parts", "body_part_size"};
 
-enum SkinPart { UNKNOWN_SKIN_PART, HAND, FOREARM_LOWER, FOREARM_UPPER, ARM_INTERNAL, ARM_EXTERNAL, 
-    ARM_LOWER, ARM_UPPER, ALL_SKIN_PARTS, SKIN_PART_SIZE};
-const std::string SkinPart_s[] = {"unknown_skin_part", "hand", "forearm_lower", "forearm_upper", "arm_internal", 
-    "arm_external", "arm_lower", "arm_upper", "all_skin_parts", "skin_part_size"};
+//enum SkinPart { UNKNOWN_SKIN_PART, HAND, FOREARM_LOWER, FOREARM_UPPER, ARM_INTERNAL, ARM_EXTERNAL, 
+//    ARM_LOWER, ARM_UPPER, ALL_SKIN_PARTS, SKIN_PART_SIZE};
+//const std::string SkinPart_s[] = {"unknown_skin_part", "hand", "forearm_lower", "forearm_upper", "arm_internal", 
+//    "arm_external", "arm_lower", "arm_upper", "all_skin_parts", "skin_part_size"};
+
+enum SkinPart { UNKNOWN_SKIN_PART, HAND, FOREARM, UPPER_ARM, ALL_SKIN_PARTS, SKIN_PART_SIZE};
+const std::string SkinPart_s[] = {"unknown_skin_part", "hand", "forearm", "upper_arm", "all_skin_parts", "skin_part_size"};
 
 enum HandPart { INDEX, MIDDLE, RING, LITTLE, THUMB, PALM, ALL_HAND_PARTS, HAND_PART_SIZE};
 const std::string HandPart_s[] = {"index", "middle", "ring", "little", "thumb", "palm", "all_hand_parts", "hand_part_size"};
