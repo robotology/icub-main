c254b6173bff12572ef3b3328f77fc48246ffe4b
Ugo Pattacini <ugo.pattacini@iit.it>
2011-11-19 15:14:39 +0000

    No commit message
    svn path=/trunk/iCub/; revision=14569

--- src/modules/wholeBodyDynamics/main.cpp
+++ src/modules/wholeBodyDynamics/main.cpp
@@ -1,9 +1,20 @@
-/*
- * Copyright (C) 2010 RobotCub Consortium
- * Author: Matteo Fumagalli
- * CopyPolicy: Released under the terms of the GNU GPL v2.0.
+/* 
+ * Copyright (C) 2011 RobotCub Consortium, European Commission FP6 Project IST-004370
+ * Author: Marco Randazzo, Matteo Fumagalli
+ * email:  marco.randazzo@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
  *
- */
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
 
 /**
 @ingroup icub_module
@@ -13,14 +24,8 @@
 Estimates the external forces and torques acting at the end effector
 through a model based estimation of the robot dynamics
  
-Copyright (C) 2008 RobotCub Consortium
+\author Matteo Fumagalli, Marco Randazzo
  
-Author: Matteo Fumagalli
- 
-Date: first release 8/07/2010 
-
-CopyPolicy: Released under the terms of the GNU GPL v2.0.
-
 \section intro_sec Description
 
 This module estimates the external wrench acting at the end
