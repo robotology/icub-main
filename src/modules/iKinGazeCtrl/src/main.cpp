7f45827b535e62ae1a8bf6e60d1e9c174709ed0b
Ugo Pattacini <ugo.pattacini@iit.it>
2012-01-10 09:29:14 +0000

    comment fixed
    svn path=/trunk/iCub/; revision=15182

--- src/modules/iKinGazeCtrl/src/main.cpp
+++ src/modules/iKinGazeCtrl/src/main.cpp
@@ -343,9 +343,8 @@ None.
 A configuration file passed through \e --config contains the
 fields required to specify the cameras intrinsic parameters 
 along with a roto-translation matrix appended to the eye 
-kinematic (\ref iKinChain::setHN) in order to achieve the 
-alignment with the optical axes compensating for possible 
-unknown offsets. 
+kinematic (see \ref setHN) in order to achieve the alignment
+with the optical axes compensating for possible unknown offsets. 
  
 \note The final roto-translational matrix is meaningful only as 
 result of the calibration of the cameras extrinsic parameters. 
