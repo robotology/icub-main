dac24e5657200c4e5e60eb7c8f601403ae962ebf
Sean Ryan Fanello <sean.fanello@iit.it>
2012-01-17 10:54:21 +0000

    bug fixes
    svn path=/trunk/iCub/; revision=15281

--- src/tools/stereoCalib/include/stereoCalibThread.h
+++ src/tools/stereoCalib/include/stereoCalibThread.h
@@ -9,10 +9,6 @@
 #include <yarp/os/Stamp.h>
 #include <cv.h>
 #include <highgui.h>
-#include "opencv2/calib3d/calib3d.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
-
 
 using namespace cv;
 using namespace yarp::sig;
