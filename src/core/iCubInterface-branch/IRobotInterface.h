661dccd8a2c22fd933de55463487b99a1e46544e
Lorenzo Natale <lorenzo.natale@iit.it>
2012-02-02 03:50:41 +0000

    branch for ethernet development
    svn path=/trunk/iCub/; revision=15503

---
+++ src/core/iCubInterface-branch/IRobotInterface.h
@@ -0,0 +1,43 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+/*
+ * Copyright (C) 2008 RobotCub Consortium
+ * Author: Lorenzo Natale
+ * CopyPolicy: Released under the terms of the GNU GPL v2.0.
+ *
+ */
+
+#ifndef __IROBOTINTERFACE__
+#define __IROBOTINTERFACE__
+
+#include <yarp/os/Property.h>
+
+#include <string>
+
+class IRobotInterface
+{
+public:
+    virtual ~IRobotInterface(){};
+    virtual bool initialize(const std::string &file)=0;
+    virtual bool initCart(const std::string &file)
+        {return true;}
+    virtual bool finiCart()
+        {return true;}
+
+    /**
+    * Closes all robot devices.
+    */
+    virtual bool detachWrappers()=0;
+    virtual bool closeNetworks()=0;
+
+    /**
+    * Park the robot. This function can be blocking or not depending on 
+    * the value of the parameter wait.
+    * @param wait if true the function blocks and returns only when parking is finished
+    */
+    virtual void park(bool wait=true)=0;
+
+    virtual void abort()=0;
+};
+
+#endif
