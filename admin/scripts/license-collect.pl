3e31c3040a4ab87fe1a386303205f432a67c6509
Lorenzo Natale <lorenzo.natale@iit.it>
2012-05-03 21:40:44 +0000

    No commit message
    svn path=/trunk/iCub/; revision=17374

--- admin/scripts/license-collect.pl
+++ admin/scripts/license-collect.pl
@@ -13,7 +13,7 @@ my %good_licenses= ( # a list of "good" licenses
 			 "This program is free software; you can redistribute it and\/or modify it under the terms of the GNU General Public License version 2 as published by the Free Software Foundation"=>"GPL20",
              "Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met: 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. 3. The name of the author may not be used to endorse or promote products derived from this software without specific prior written permission." => "BSD",
 			 "BSD license" => "BSD",
-             "Intel License Agreement For Open Source Computer Vision Library" => "OPENCV"
+             "Intel License Agreement For Open Source Computer Vision Library" => "OPENCV",
 			 "ARM Limited (ARM) is supplying this software for use with Cortex-M processor based microcontrollers.  This file can be freely distributed within development tools that are supporting such ARM based processors." => "ARM Cortex-M" 
 );
 
