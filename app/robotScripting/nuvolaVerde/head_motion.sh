d3f08b7ae42fd32d3bd41ef9c43a9b92a2b8b099
Ugo Pattacini <ugo.pattacini@iit.it>
2012-06-19 16:20:21 +0000

    No commit message
    svn path=/trunk/iCub/; revision=18719

---
+++ app/robotScripting/nuvolaVerde/head_motion.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+count=1
+while [ "$count" -le "$1" ]
+do
+  azi=$RANDOM
+  ele=$RANDOM
+  ver=$RANDOM
+
+  let "azi %= 60"
+  let "ele %= 10"
+  let "azi -= 30"
+  let "ele -= 5"
+  let "ver %= 15"
+
+  let "count += 1"
+  
+  echo "abs "$azi" "$ele" "$ver"" | yarp write ... /iKinGazeCtrl/head/angles:i
+  sleep "$2"
+done
+
+echo "abs 0.0 0.0 1.0" | yarp write ... /iKinGazeCtrl/head/angles:i
+
+
+
+
