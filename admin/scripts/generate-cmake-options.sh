c66e0b93a450e66b4093b81db2869385ac4b29f1
Lorenzo Natale <lorenzo.natale@iit.it>
2012-08-31 07:25:06 +0000

    add debugInterfaceClient to tests
    svn path=/trunk/iCub/; revision=19945

--- admin/scripts/generate-cmake-options.sh
+++ admin/scripts/generate-cmake-options.sh
@@ -23,6 +23,7 @@ case $2 in
                         -DENABLE_icubmod_icubskinprototype:BOOL=TRUE \
                         -DENABLE_icubmod_fakecan:BOOL=TRUE \
                         -DENABLE_icubmod_canmotioncontrol:BOOL=TRUE\
+                        -DENABLE_icubmod_debugInterfaceClient:BOOL=TRUE\
                         -DICUB_DASHBOARD_SUBMIT:BOOL=TRUE"
 esac
     
