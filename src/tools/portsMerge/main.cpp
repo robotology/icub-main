65a1076ddb0c91e75b960d6b90de1328cb3c2a04
Ugo Pattacini <ugo.pattacini@iit.it>
2012-10-04 12:25:16 +0000

    [portsMerge]: removed tabs :)
    svn path=/trunk/iCub/; revision=20539

--- src/tools/portsMerge/main.cpp
+++ src/tools/portsMerge/main.cpp
@@ -78,86 +78,88 @@ int main(int argc, char *argv[])
 {
     Network yarp;
 
-	BufferedPort<Bottle >   outPort;
+    BufferedPort<Bottle >   outPort;
     BufferedPort<Bottle >*  inPort  = 0;
-	Bottle*                 inData  = 0;
-    yarp::os::Stamp*		inStamp = 0;
-	yarp::os::Stamp         outStamp;
+    Bottle*                 inData  = 0;
+    yarp::os::Stamp*        inStamp = 0;
+    yarp::os::Stamp         outStamp;
 
-	int nPorts = argc-1;
-	if (nPorts == 0)
-	{
-		printf ("No input ports specified! \n");
-		return -1;
-	}
+    int nPorts = argc-1;
+    if (nPorts == 0)
+    {
+        printf ("No input ports specified! \n");
+        return -1;
+    }
 
-	inPort  = new BufferedPort<Bottle > [nPorts];
-	inData  = new Bottle                [nPorts];
-	inStamp = new yarp::os::Stamp       [nPorts];
+    inPort  = new BufferedPort<Bottle > [nPorts];
+    inData  = new Bottle                [nPorts];
+    inStamp = new yarp::os::Stamp       [nPorts];
 
-	//open the ports
-	char buff[255];
-	string s = "/portsMerge";
-	for (int i = 0; i< nPorts; i++)
-	{
-		sprintf(buff,"%s/i%d", s.c_str(), i);
-		inPort[i].open(buff);
-	}
-	sprintf(buff,"%s/o0", s.c_str());
-	outPort.open(buff);
+    //open the ports
+    char buff[255];
+    string s = "/portsMerge";
+    for (int i = 0; i< nPorts; i++)
+    {
+        sprintf(buff,"%s/i%d", s.c_str(), i);
+        inPort[i].open(buff);
+    }
+    sprintf(buff,"%s/o0", s.c_str());
+    outPort.open(buff);
 
-	//makes the connection
-	for (int i=1; i<argc; i++)
-	{
-		string tmp = argv[i];
-		bool b = yarp::os::Network::connect(tmp.c_str(),inPort[i-1].getName().c_str(),"udp",false);		
-		if (b == false) return -1;
-	}
+    //makes the connection
+    for (int i=1; i<argc; i++)
+    {
+        string tmp = argv[i];
+        bool b = yarp::os::Network::connect(tmp.c_str(),inPort[i-1].getName().c_str(),"udp",false);     
+        if (b == false) return -1;
+    }
 
-	printf ("Module running\n");
+    printf ("Module running\n");
     while(true)
     {
-		//read
-		int empty =0;
-		for (int i = 0; i< nPorts; i++)
-		{		
-			Bottle *b = inPort[i].read(false);
-			if (b!=0) 
-			{
-				inPort[i].getEnvelope(inStamp[i]);
-				inData[i] = *b;
-			}
-			else
-			{
-				empty++;
-			}
-		}
-		if (empty == nPorts) 
-		{
-			yarp::os::Time::delay(0.001);
-			continue;
-		}
+        //read
+        int empty =0;
+        for (int i = 0; i< nPorts; i++)
+        {       
+            Bottle *b = inPort[i].read(false);
+            if (b!=0) 
+            {
+                inPort[i].getEnvelope(inStamp[i]);
+                inData[i] = *b;
+            }
+            else
+            {
+                empty++;
+            }
+        }
+        if (empty == nPorts) 
+        {
+            yarp::os::Time::delay(0.001);
+            continue;
+        }
 
-		//write
-		outStamp.update();
-		if (outPort.getOutputCount()>0)
-		{		
-			Bottle &out=outPort.prepare();
-			out.clear();
-			for (int i = 0; i< nPorts; i++)
-			{
-				out.append(inData[i]);
-			}
-			outPort.setEnvelope(outStamp);
-			outPort.write();
-		}
-		
-		//delay
-		yarp::os::Time::delay(0.001);
-	}
+        //write
+        outStamp.update();
+        if (outPort.getOutputCount()>0)
+        {       
+            Bottle &out=outPort.prepare();
+            out.clear();
+            for (int i = 0; i< nPorts; i++)
+            {
+                out.append(inData[i]);
+            }
+            outPort.setEnvelope(outStamp);
+            outPort.write();
+        }
+        
+        //delay
+        yarp::os::Time::delay(0.001);
+    }
 
-	delete [] inPort  ;
-	delete [] inData  ;
-	delete [] inStamp ;
-	return 0;
+    delete [] inPort  ;
+    delete [] inData  ;
+    delete [] inStamp ;
+    return 0;
 }
+
+
