b7c3dbc7bb39dbb05943408e358181bc499042a3
Vadim Tikhanoff <vadim.tikhanoff@iit.it>
2012-11-23 16:00:36 +0000

    added clean for yarp
    svn path=/trunk/iCub/; revision=21602

--- admin/scripts/cluster-clean.sh
+++ admin/scripts/cluster-clean.sh
@@ -12,7 +12,7 @@ ssh -T icub@$1<<END
     pwd
     ls -l --ignore=*.sh --ignore=*.py | grep '^-' | awk '{print `echo '$9'`}' | while read line
     do      
-       ps -ef | grep -i `echo '$line'` | grep -v grep | awk '{print `echo '$2'`}' | xargs kill &>/dev/null
+       ps -ef | grep -i `echo '$line'` | grep -v grep | awk '{print `echo '$2'`}' | xargs kill -9 &>/dev/null
     done
     yarp clean timeout 0.1 &>/dev/null
 END
@@ -20,7 +20,6 @@ END
 echo ""
 echo "You are now clean on $1"
 echo ""
-
 }
 
 function actionFunc(){
@@ -36,29 +35,41 @@ ssh -T icub@$1<<END
     exit
     echo ""
 END
-
-echo ""
-echo "WARNING!!! Should I go ahead and kill these processes?"
-echo ""
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) killFunc $1 '${ICUB_DIR}'; break;;
-        No ) exit;;
-    esac
-done
 }
 
+function askForYarp(){
 
+    echo "WARNING!!! Should I go ahead run this script for yarp?"
+    askForListFunc $1 '${YARP_DIR}'
+}
 
-if [[ -n "$1" ]] ; then
+function askForKillFunc(){
+
     echo ""
-    echo "Are you sure you want to run this script on node "[$1]"?"
+    echo "WARNING!!! Should I go ahead and kill these processes?"
+    echo ""
     select yn in "Yes" "No"; do
         case $yn in
-            Yes ) actionFunc $1 '${ICUB_DIR}'; break;;
+            Yes ) killFunc $1 $2; break;;
             No ) exit;;
         esac
     done
+}
+
+function askForListFunc(){
+
+    echo "Are you sure you want to run this script on node "[$1]"?"
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) actionFunc $1 $2; askForKillFunc $1 $2; askForYarp $1; break;;
+            No ) exit;;
+        esac
+    done
+}
+
+if [[ -n "$1" ]] ; then
+    echo ""
+    askForListFunc $1 '${ICUB_DIR}'
 else
     echo 'node is empty, please select which node you want to clean-up'
 fi
