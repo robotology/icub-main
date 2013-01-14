c89ed3b4a7ed31b490572966b1717b1449088035
Marco Maggiali <marco.maggiali@iit.it>
2013-01-14 13:12:08 +0000

    No commit message
    svn path=/trunk/iCub/; revision=22002

--- app/robots/kneeV2/conf/TESTLAUNCHER_wo_gear.bat
+++ app/robots/kneeV2/conf/TESTLAUNCHER_wo_gear.bat
@@ -1,6 +1,6 @@
 start yarpserver
 start yarp run --server /icubsrv
 start IcubInterface --config iCubMotor_wo_gear.ini
-timeout 3
+timeout 1
 start robotMotorGui --from robotMotorGui.ini
 start simpleClient --robot icub --part right_leg
