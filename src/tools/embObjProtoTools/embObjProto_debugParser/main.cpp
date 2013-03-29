71b9aa9823ab7ff8b1393faff3f54ccf2764f2e5
Valentina Gaggero <valentina.gaggero@iit.it>
2013-03-29 10:27:47 +0000

    added new files for parser cfg and modified cmakelist file
    svn path=/trunk/iCub/; revision=23238

--- src/tools/embObjProtoTools/embObjProto_debugParser/main.cpp
+++ src/tools/embObjProtoTools/embObjProto_debugParser/main.cpp
@@ -75,7 +75,8 @@
 // --------------------------------------------------------------------------------------------------------------------
 // - definition (and initialisation) of extern variables, but better using _get(), _set() 
 // --------------------------------------------------------------------------------------------------------------------
-// empty-section
+//extern const eODeb_eoProtoParser_cfg_t  deb_eoParserCfg;
+extern const eODeb_eoProtoParser_cfg_t *deb_eoParserCfg_ptr;
 
 
 
@@ -90,6 +91,7 @@
 // --------------------------------------------------------------------------------------------------------------------
 static void s_process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
 static void s_ethLowLevelPraser_configure(void);
+static void print_help(void);
 
 
 
@@ -107,6 +109,10 @@ int main(int argc, char *argv[])
 	// char 	errbuf[PCAP_ERRBUF_SIZE];
 	// struct bpf_program fp;					/* The compiled filter expression */
 	char 	filter_exp[64];					/* The filter expression */
+	char    filtersrc[20];
+	bool    filterOnSrc = false;
+	char    filterdst[20];
+	bool    filterOnDst = false;
 	char 	dev[10];
     uint64_t maxacquisition = MAX_ACQUISITION;
 
@@ -116,13 +122,20 @@ int main(int argc, char *argv[])
     
     //1) init data
     sprintf(dev, "eth1");
-    sprintf(filter_exp, "src host 10.0.1.104");
-    
+ //   sprintf(filter_exp, "src host 10.0.1.104");
+    sprintf(filter_exp, " ");
+
     //2) parse arguments
     if(argc>1)
     {
         for(uint8_t i = 0; i<argc; i++)
         {
+            if(strcmp("--help", argv[i]) == 0)
+            {
+				print_help();
+                return(0);
+            }
+
             if(strcmp("--dev", argv[i]) == 0)
             {
                 if(i<(argc-1))
@@ -132,15 +145,27 @@ int main(int argc, char *argv[])
                 continue;
             }
             
-            if(strcmp("--filter", argv[i]) == 0)
+            if(strcmp("--filterSrcAddr", argv[i]) == 0)
             {
                 if(i<(argc-1))
                 {
-                    sprintf(filter_exp, "%s", argv[++i]);
+                	filterOnSrc = true;
+                    sprintf(filtersrc, "%s", argv[++i]);
                 }
                 continue;
             }
+
             
+            if(strcmp("--filterDstAddr", argv[i]) == 0)
+            {
+                if(i<(argc-1))
+                {
+                	filterOnDst = true;
+                	sprintf(filterdst, "%s", argv[++i]);
+                }
+                continue;
+            }
+
             if(strcmp("--maxacquisition", argv[i]) == 0)
             {
                 if(i<(argc-1))
@@ -151,9 +176,25 @@ int main(int argc, char *argv[])
             }
 
         }
-    
     }
-    
+
+    //set filter
+    if((filterOnSrc) &&(filterOnDst))
+    {
+    	sprintf(filter_exp, "src host %s and dst host %s", filtersrc, filterdst);
+
+    }
+    else if(filterOnSrc)
+    {
+    	sprintf(filter_exp, "src host %s ", filtersrc);
+
+    }
+    else if(filterOnDst)
+    {
+    	sprintf(filter_exp, "dst host %s", filterdst);
+    }
+
+
     printf("Debug-tool: Ethernet Low Level Praser start on interface %s\n", dev);
     printf("\t\t Used filter: %s\n", filter_exp);
     
@@ -247,134 +288,134 @@ static void s_process_packet(u_char *args, const struct pcap_pkthdr *header, con
 }
 
 
-static void my_cbk_onErrorSeqNum(eOethLowLevParser_packetInfo_t *pktInfo_ptr, uint32_t rec_seqNum, uint32_t expected_seqNum)
-{
-	
-	printf("ERR in SEQNUM; rec=%d expected=%d\n",rec_seqNum, expected_seqNum );
-return;
-}
-
-static void my_cbk_onNVfound(eOethLowLevParser_packetInfo_t *pktInfo_ptr, eODeb_eoProtoParser_ropAdditionalInfo_t *ropAddInfo_ptr)
-{
-	
-	printf("NV found!!: ep=%x id=%x\n", ropAddInfo_ptr->desc.ep, ropAddInfo_ptr->desc.id);
-	return;
-}
-
-static void my_cbk_onNVsetpointFound(eOethLowLevParser_packetInfo_t *pktInfo_ptr, eODeb_eoProtoParser_ropAdditionalInfo_t *ropAddInfo_ptr)
-{
-    eOmc_setpoint_t * setpoint_ptr = (eOmc_setpoint_t *)ropAddInfo_ptr->desc.data;
-    uint8_t board = 0, j;	
-    float enc_factor, zero, enc_factor_6=182.044 , enc_factor_8=182.044, zero_6=180, zero_8=-180;
-
-	//printf("NV found!!: ep=%x id=%x\n", ropAddInfo_ptr->desc.ep, ropAddInfo_ptr->desc.id);
-	
-    if(ropAddInfo_ptr->desc.ep == 0x18)
-    {
-        board = 8;
-        enc_factor = enc_factor_8;
-        zero = zero_8;
-    }
-    else if(ropAddInfo_ptr->desc.ep == 0x16)
-    {
-        board = 6;
-        enc_factor = enc_factor_6;
-        zero = zero_6;
-    }
-    else
-    {
-        printf("\n\n ERROR: un expected ep!!! %d \n ", ropAddInfo_ptr->desc.ep);
-        return;
-    }
-
-    if(setpoint_ptr->type != eomc_setpoint_position)
-    {
-        printf("ERR: no setpoint position. typse= %d", setpoint_ptr->type);
-        return;
-    }
-
-    switch(ropAddInfo_ptr->desc.id)
-    {
-        case 0xbc1a:
-        {
-            j = 0;
-        }break;
-
-        case 0xbc3a:
-        {
-            j = 1;
-        }break;
-
-        case 0xbc5a:
-        {
-            j = 2;
-        }break;
-
-        case 0xbc7a:
-        {
-            j = 3;
-        }break;
-        default:
-        {
-            printf("ERROR: receiv unexpected nvid %x", ropAddInfo_ptr->desc.id);
-            return;
-        }
-    }
-    float vel, pos;
-
-    pos = (setpoint_ptr->to.position.value/enc_factor)-zero;
-    vel = setpoint_ptr->to.position.withvelocity/fabs(enc_factor);
-
-    printf("board %d  j %d   pos %f (%d)  vel %f (%d)  enc_factor=%f  zero=%f\n", board, j, pos, setpoint_ptr->to.position.value, vel, setpoint_ptr->to.position.withvelocity,enc_factor,zero);
-    
-    return;
-}
+//static void my_cbk_onErrorSeqNum(eOethLowLevParser_packetInfo_t *pktInfo_ptr, uint32_t rec_seqNum, uint32_t expected_seqNum)
+//{
+//
+//	printf("ERR in SEQNUM; rec=%d expected=%d\n",rec_seqNum, expected_seqNum );
+//return;
+//}
+//
+//static void my_cbk_onNVfound(eOethLowLevParser_packetInfo_t *pktInfo_ptr, eODeb_eoProtoParser_ropAdditionalInfo_t *ropAddInfo_ptr)
+//{
+//
+//	printf("NV found!!: ep=%x id=%x\n", ropAddInfo_ptr->desc.ep, ropAddInfo_ptr->desc.id);
+//	return;
+//}
+//
+//static void my_cbk_onNVsetpointFound(eOethLowLevParser_packetInfo_t *pktInfo_ptr, eODeb_eoProtoParser_ropAdditionalInfo_t *ropAddInfo_ptr)
+//{
+//    eOmc_setpoint_t * setpoint_ptr = (eOmc_setpoint_t *)ropAddInfo_ptr->desc.data;
+//    uint8_t board = 0, j;
+//    float enc_factor, zero, enc_factor_6=182.044 , enc_factor_8=182.044, zero_6=180, zero_8=-180;
+//
+//	//printf("NV found!!: ep=%x id=%x\n", ropAddInfo_ptr->desc.ep, ropAddInfo_ptr->desc.id);
+//
+//    if(ropAddInfo_ptr->desc.ep == 0x18)
+//    {
+//        board = 8;
+//        enc_factor = enc_factor_8;
+//        zero = zero_8;
+//    }
+//    else if(ropAddInfo_ptr->desc.ep == 0x16)
+//    {
+//        board = 6;
+//        enc_factor = enc_factor_6;
+//        zero = zero_6;
+//    }
+//    else
+//    {
+//        printf("\n\n ERROR: un expected ep!!! %d \n ", ropAddInfo_ptr->desc.ep);
+//        return;
+//    }
+//
+//    if(setpoint_ptr->type != eomc_setpoint_position)
+//    {
+//        printf("ERR: no setpoint position. typse= %d", setpoint_ptr->type);
+//        return;
+//    }
+//
+//    switch(ropAddInfo_ptr->desc.id)
+//    {
+//        case 0xbc1a:
+//        {
+//            j = 0;
+//        }break;
+//
+//        case 0xbc3a:
+//        {
+//            j = 1;
+//        }break;
+//
+//        case 0xbc5a:
+//        {
+//            j = 2;
+//        }break;
+//
+//        case 0xbc7a:
+//        {
+//            j = 3;
+//        }break;
+//        default:
+//        {
+//            printf("ERROR: receiv unexpected nvid %x", ropAddInfo_ptr->desc.id);
+//            return;
+//        }
+//    }
+//    float vel, pos;
+//
+//    pos = (setpoint_ptr->to.position.value/enc_factor)-zero;
+//    vel = setpoint_ptr->to.position.withvelocity/fabs(enc_factor);
+//
+//    printf("board %d  j %d   pos %f (%d)  vel %f (%d)  enc_factor=%f  zero=%f\n", board, j, pos, setpoint_ptr->to.position.value, vel, setpoint_ptr->to.position.withvelocity,enc_factor,zero);
+//
+//    return;
+//}
 
 
 static void s_ethLowLevelPraser_configure(void)
 {
+
+//    //4.1) init application parser: embObjParser
+//    const eODeb_eoProtoParser_cfg_t  deb_eoParserCfg =
+//    {
+//        EO_INIT(.checks)
+//        {
+//            EO_INIT(.seqNum)
+//            {
+//                EO_INIT(.cbk_onErrSeqNum)           NULL, //my_cbk_onErrorSeqNum,
+//            },
+//
+//            EO_INIT(.nv)
+//            {
+//                EO_INIT(.NVs2searchArray)
+//                {
+//                    EO_INIT(.head)
+//                    {
+//                        EO_INIT(.capacity)       eODeb_eoProtoParser_maxNV2find,
+//                        EO_INIT(.itemsize)       sizeof(eODeb_eoProtoParser_nvidEp_couple_t),
+//                        EO_INIT(.size)           8,
+//                    },
+//                    EO_INIT(.data)
+//                    {
+//                        {0x18, 0xbc1a},
+//                        {0x18, 0xbc3a},
+//                        {0x18, 0xbc5a},
+//                        {0x18, 0xbc7a},
+//                        {0x16, 0xbc1a},
+//                        {0x16, 0xbc3a},
+//                        {0x16, 0xbc5a},
+//                        {0x16, 0xbc7a}
+//                    }
+//
+//                },
+//                EO_INIT(.cbk_onNVfound)            my_cbk_onNVsetpointFound, //my_cbk_onNVfound
+//            },
+//
+//            EO_INIT(.invalidRopFrame)               {0}
+//        }
+//    };
     
-    //4.1) init application parser: embObjParser
-    const eODeb_eoProtoParser_cfg_t  deb_eoParserCfg = 
-    {
-        EO_INIT(.checks)
-        {
-            EO_INIT(.seqNum)
-            {
-                EO_INIT(.cbk_onErrSeqNum)           NULL, //my_cbk_onErrorSeqNum,
-            },
-            
-            EO_INIT(.nv)
-            {
-                EO_INIT(.NVs2searchArray)
-                {
-                    EO_INIT(.head)
-                    {
-                        EO_INIT(.capacity)       eODeb_eoProtoParser_maxNV2find,
-                        EO_INIT(.itemsize)       sizeof(eODeb_eoProtoParser_nvidEp_couple_t),
-                        EO_INIT(.size)           8,
-                    },
-                    EO_INIT(.data)
-                    {
-                        {0x18, 0xbc1a},
-                        {0x18, 0xbc3a},
-                        {0x18, 0xbc5a},
-                        {0x18, 0xbc7a},
-                        {0x16, 0xbc1a},
-                        {0x16, 0xbc3a},
-                        {0x16, 0xbc5a},
-                        {0x16, 0xbc7a}
-                    }
-                
-                },
-                EO_INIT(.cbk_onNVfound)            my_cbk_onNVsetpointFound, //my_cbk_onNVfound
-            },
-            
-            EO_INIT(.invalidRopFrame)               {0}
-        }
-    };
-    
-    eODeb_eoProtoParser_Initialise(&deb_eoParserCfg);
+    eODeb_eoProtoParser_Initialise(/*&deb_eoParserCfg*/deb_eoParserCfg_ptr);
     
     
     //4.2) init low level parser: eOethLowLevParser
@@ -398,6 +439,15 @@ static void s_ethLowLevelPraser_configure(void)
     eo_ethLowLevParser_Initialise(&ethLowLevParserCfg);
 }
 
+static void print_help(void)
+{
+
+	printf("option --help: print this help\n");
+	printf("option --dev: specify device where start to capture (default eth1)\n");
+	printf("option --filterSrcAddr: application will capture packet with source address specified here. Not mandatory\n");
+	printf("option --filterDstAddr: application will capture packet with dest address specified here. Not mandatory\n");
+	printf("option --maxacquisition: max num of captured packets. Not mandatory\n");
+}
 
 // --------------------------------------------------------------------------------------------------------------------
 // - end-of-file (leave a blank line after)
