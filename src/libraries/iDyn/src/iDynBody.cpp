5bd1a79bd076504c8866a57b9b08bc04cb5913e7
Francesco Nori <francesco.nori@iit.it>
2013-03-29 14:31:54 +0000

    corrected an errror on the matrices Hright and Hleft for the neck->arms rototranslation
    svn path=/trunk/iCub/; revision=23244

--- src/libraries/iDyn/src/iDynBody.cpp
+++ src/libraries/iDyn/src/iDynBody.cpp
@@ -2266,13 +2266,13 @@ void iCubUpperTorso::build()
     HRight.resize(4,4); HRight.zero();
 
     double theta = CTRL_DEG2RAD * (180.0-15.0);
-    HLeft(0,0) = cos(theta);    HLeft(0,1) = 0.0;       HLeft(0,2) = sin(theta);    HLeft(0,3) = 0.003066;
-    HLeft(1,0) = 0.0;           HLeft(1,1) = 1.0;       HLeft(1,2) = 0.0;           HLeft(1,3) = -0.049999;
-    HLeft(2,0) = -sin(theta);   HLeft(2,1) = 0.0;       HLeft(2,2) = cos(theta);    HLeft(2,3) = -0.110261;
+    HLeft(0,0) = cos(theta);    HLeft(0,1) = 0.0;       HLeft(0,2) = sin(theta);    HLeft(0,3) = -24.8786e-3;
+    HLeft(1,0) = 0.0;           HLeft(1,1) = 1.0;       HLeft(1,2) = 0.0;           HLeft(1,3) = -50.0000e-3;
+    HLeft(2,0) = -sin(theta);   HLeft(2,1) = 0.0;       HLeft(2,2) = cos(theta);    HLeft(2,3) =  -6.0472e-3;
     HLeft(3,3) = 1.0;   
-    HRight(0,0) = -cos(theta);  HRight(0,1) = 0.0;  HRight(0,2) = -sin(theta);  HRight(0,3) = 0.00294;
-    HRight(1,0) = 0.0;          HRight(1,1) = -1.0; HRight(1,2) = 0.0;          HRight(1,3) = -0.050;
-    HRight(2,0) = -sin(theta);  HRight(2,1) = 0.0;  HRight(2,2) = cos(theta);   HRight(2,3) = 0.10997;
+    HRight(0,0) = -cos(theta);  HRight(0,1) = 0.0;  HRight(0,2) = -sin(theta);  HRight(0,3) = -24.8786e-3;
+    HRight(1,0) = 0.0;          HRight(1,1) = -1.0; HRight(1,2) = 0.0;          HRight(1,3) = -50.0000e-3;
+    HRight(2,0) = -sin(theta);  HRight(2,1) = 0.0;  HRight(2,2) = cos(theta);   HRight(2,3) =   6.0472e-3;
     HRight(3,3) = 1.0;
 
     // order: head - right arm - left arm
