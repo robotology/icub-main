87400e8c4dd20de89602074b0b77b8bce3701695
Valentina Gaggero <valentina.gaggero@iit.it>
2013-05-14 18:01:51 +0000

    use backdoor for ems diagnostics info
    svn path=/trunk/iCub/; revision=23886

---
+++ src/tools/emsBackDoorManager/diagnostics_buttons.cpp
@@ -0,0 +1,246 @@
+/*
+ * Copyright (C) 2011 Department of Robotics Brain and Cognitive Sciences - Istituto Italiano di Tecnologia
+ * Author:  Valentina Gaggero
+ * email:   valentina.gaggero@iit.it
+ * website: www.robotcub.org
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * http://www.robotcub.org/icub/license/gpl.txt
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+/* @file       template_buttons.c
+    @brief
+    @author     valentina.gaggero@iit.it
+    @date       03/22/2013
+**/
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - external dependencies
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of external variables
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of extern public interface
+// --------------------------------------------------------------------------------------------------------------------
+#include "template_buttons.hpp"
+#include "OPCprotocolManager.h"
+#include "OPCprotocolManager_Cfg.h"
+#include "EoDiagnostics.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of extern hidden interface
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - #define with internal scope
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition (and initialisation) of extern variables, but better using _get(), _set()
+// --------------------------------------------------------------------------------------------------------------------
+extern OPCprotocolManager *opcMan_ptr;
+
+eOdgn_commands_t dgnCommands={0};
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - typedef with internal scope
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - declaration of static functions
+// --------------------------------------------------------------------------------------------------------------------
+static uint32_t s_getAddressFromUser(void);
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition (and initialization) of static variables
+// --------------------------------------------------------------------------------------------------------------------
+
+
+// --------------------------------------------------------------------------------------------------------------------
+// - definition of extern public functions
+// --------------------------------------------------------------------------------------------------------------------
+void commands(void)
+{
+	printf("\nq: quit\n");
+	printf("0: ena/disa diagnostics on a ems\n");
+	printf("1: ........\n");
+	printf("2: ........\n");
+	printf("3: ........\n");
+	printf("4: ........\n");
+	printf("5: ........\n");
+	printf("\n");
+}
+
+static uint32_t s_getAddressFromUser(void)
+{
+    char addrstr[20];
+    uint32_t ip1,ip2,ip3,ip4, address=0;
+
+    printf("Insert destination host: x.x.x.x ");
+    scanf("%s", &addrstr[0]);
+    sscanf(addrstr,"%d.%d.%d.%d",&ip1,&ip2,&ip3,&ip4);
+    address = (ip1<<24)|(ip2<<16)|(ip3<<8)|ip4 ;
+
+    return(address);
+}
+
+
+int32_t callback_button_x(uint8_t *payload_ptr, uint32_t payload_size, ACE_INET_Addr *addr, int16_t id)
+{
+	printf("button %d\n", id);
+
+	uint32_t dest_addr = s_getAddressFromUser();
+	uint16_t size = 0;
+	opcprotman_res_t res;
+
+	addr->set(4444, dest_addr);
+
+	if(opcMan_ptr ==  NULL)
+	{
+		printf("opcMan_ptr is NULL\n");
+		return(0);
+	}
+
+	res = opcprotman_Form(opcMan_ptr, opcprotman_opc_ask, id, NULL /*setdata*/, (opcprotman_message_t*)payload_ptr, &size);
+	if(opcprotman_OK  != res)
+	{
+
+		printf("erron in former\n");
+		return(0);
+	}
+	printf("in button %d : size=%d \n", id, size);
+
+     	return(size);
+}
+
+// fill the callback with your code
+uint32_t callback_button_0(uint8_t *payload_ptr, uint32_t payload_size, ACE_INET_Addr	*addr)
+{
+	uint32_t dest_addr;
+	uint16_t size = 0;
+    uint32_t cmdena=0;
+    opcprotman_res_t res;
+
+    if(opcMan_ptr ==  NULL)
+	{
+		printf("opcMan_ptr is NULL\n");
+		return(0);
+	}
+
+
+    printf("enable/disable diagnostics. Press 1 to enable 0 otherwise\n");
+    scanf("%d", &cmdena);
+
+    dest_addr = s_getAddressFromUser();
+    printf("address is 0x %x \n", dest_addr);
+    
+    if(cmdena>1)
+    {
+        cmdena=1;
+    }
+    dgnCommands.enable = 1;
+
+	
+//	addr->set(4444, (10<<24)|(255<<16)|(72<<8)|19 );
+	addr->set(4444, dest_addr);
+
+	res = opcprotman_Form(opcMan_ptr, opcprotman_opc_set, eodgn_nvidbdoor_cmds,  &dgnCommands, (opcprotman_message_t*)payload_ptr, &size);
+	if(opcprotman_OK  != res)
+	{
+
+		printf("erron in former\n");
+		return(0);
+	}
+
+    return(size);
+}
+
+
+
+// fill the callback with your code
+/*uint32_t callback_button_0(uint8_t *payload_ptr, uint32_t payload_size, ACE_INET_Addr	*addr)
+{
+	uint32_t dest_addr = s_getAddressFromUser();
+	uint16_t size = 0;
+
+	
+//	addr->set(4444, (10<<24)|(255<<16)|(72<<8)|19 );
+	addr->set(4444, dest_addr);
+
+	opcprotman_Form(opcMan_ptr, opcprotman_opc_ask, 1, NULL /*setdata, payload_ptr, &size);
+
+	return(size);
+}*/
+uint32_t callback_button_1(uint8_t *payload_ptr, uint32_t payload_size, ACE_INET_Addr *addr)
+{
+	return(callback_button_x(payload_ptr, payload_size, addr, 1));
+}
+
+uint32_t callback_button_2(uint8_t *payload_ptr, uint32_t payload_size, ACE_INET_Addr *addr)
+{
+	return(callback_button_x(payload_ptr, payload_size, addr, 2));
+}
+
+
+uint32_t callback_button_3(uint8_t *payload_ptr, uint32_t payload_size, ACE_INET_Addr *addr)
+{
+	return(0);
+}
+
+uint32_t callback_button_4(uint8_t *payload_ptr, uint32_t payload_size, ACE_INET_Addr *addr)
+{
+	return(0);
+}
+
+uint32_t callback_button_5(uint8_t *payload_ptr, uint32_t payload_size, ACE_INET_Addr *addr)
+{
+	return(0);
+}
+
+uint32_t callback_button_6(uint8_t *payload_ptr, uint32_t payload_size, ACE_INET_Addr *addr)
+{
+	return(0);
+}
+
+uint32_t callback_button_7(uint8_t *payload_ptr, uint32_t payload_size, ACE_INET_Addr *addr)
+{
+	return(0);
+}
+
+uint32_t callback_button_8(uint8_t *payload_ptr, uint32_t payload_size, ACE_INET_Addr *addr)
+{
+	return(0);
+}
+
+uint32_t callback_button_9(uint8_t *payload_ptr, uint32_t payload_size, ACE_INET_Addr *addr)
+{
+	return(0);
+}
+
+
+uint32_t callback_button_10(uint8_t *payload_ptr, uint32_t payload_size, ACE_INET_Addr *addr)
+{
+	return(0);
+}
+
--- src/tools/emsBackDoorManager/opcManager_cfg4pc104.c
+++ src/tools/emsBackDoorManager/opcManager_cfg4pc104.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2013 iCub Facility - Istituto Italiano di Tecnologia
- * Author:  Marco Accame
- * email:   marco.accame@iit.it
+ * Author:  Valentina Gaggero
+ * email:   valentina.gaggero@iit.it
  * website: www.robotcub.org
  * Permission is granted to copy, distribute, and/or modify this program
  * under the terms of the GNU General Public License, version 2 or any
@@ -25,6 +25,7 @@
 #include "stdlib.h" 
 #include "string.h" 
 #include "EoCommon.h"
+#include "EoDiagnostics.h"
 #include "OPCprotocolManager.h"
 #include "OPCprotocolManager_Cfg.h"
 
@@ -40,12 +41,6 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 
-extern void on_rec_ipnet_debug(opcprotman_opc_t opc, opcprotman_var_map_t* map, void* recdata);
-
-extern void on_rec_runner_debug(opcprotman_opc_t opc, opcprotman_var_map_t* map, void* recdata);
-
-extern void on_rec_transceiver_debug(opcprotman_opc_t opc, opcprotman_var_map_t* map, void* recdata);
-
 
 // --------------------------------------------------------------------------------------------------------------------
 // - #define with internal scope
@@ -56,7 +51,7 @@ extern void on_rec_transceiver_debug(opcprotman_opc_t opc, opcprotman_var_map_t*
 // --------------------------------------------------------------------------------------------------------------------
 // - definition (and initialisation) of extern variables, but better using _get(), _set() 
 // --------------------------------------------------------------------------------------------------------------------
-
+extern eOdgn_commands_t dgnCommands;
 
 // --------------------------------------------------------------------------------------------------------------------
 // - typedef with internal scope
@@ -67,32 +62,17 @@ extern void on_rec_transceiver_debug(opcprotman_opc_t opc, opcprotman_var_map_t*
 // --------------------------------------------------------------------------------------------------------------------
 // - declaration of static functions
 // --------------------------------------------------------------------------------------------------------------------
+static void s_print_emsperiph_candata(eOdgn_canstatus_t *canst_ptr);
+static void on_rec_emsperiph(opcprotman_opc_t opc, opcprotman_var_map_t* map, void* recdata);
 
-extern void on_rec_ipnet_debug(opcprotman_opc_t opc, opcprotman_var_map_t* map, void* recdata)
-{   // for the host
+static void on_rec_emsapplcommon(opcprotman_opc_t opc, opcprotman_var_map_t* map, void* recdata);
+static void s_print_emsapplcomm_core(eOdgn_coreapplication_t *appcore_ptr);
+static void s_print_emsapplcomm_ipnet(eOdgn_ipnet_t  *appipnet_ptr);
+static void s_print_emsapplcomm_transceiver(eOdgn_embObjtransceiver_t  *apptrans_ptr);
 
-    EOMtheIPnetDEBUG_t* data = (EOMtheIPnetDEBUG_t*)recdata;
-    
-    switch(opc)
-    {
-        
-        default:
-        case opcprotman_opc_set:
-        {   // nobody can order that to us           
-            // we just dont do it ...         
-        } break;
-        
-        case opcprotman_opc_say:    // someboby has replied to a ask we sent
-        case opcprotman_opc_sig:    // someboby has spontaneously sent some data
-        {   
-        
-            printf("received data of ipnet");
-        
-        } break;
-    }       
-    
-}
-
+static void on_rec_emsapplmc(opcprotman_opc_t opc, opcprotman_var_map_t* map, void* recdata);
+static void s_print_emsapplmc_encoderserror(eOdgn_encoderreads_t *encreads);
+/*
 extern void on_rec_runner_debug(opcprotman_opc_t opc, opcprotman_var_map_t* map, void* recdata)
 {   // for the host
 
@@ -130,29 +110,7 @@ extern void on_rec_runner_debug(opcprotman_opc_t opc, opcprotman_var_map_t* map,
     }       
     
 }
-
-extern void on_rec_transceiver_debug(opcprotman_opc_t opc, opcprotman_var_map_t* map, void* recdata)
-{   // for the host
-    
-    switch(opc)
-    {
-        
-        default:
-        case opcprotman_opc_set:
-        {   // nobody can order that to us           
-            // we just dont do it ...         
-        } break;
-        
-        case opcprotman_opc_say:    // someboby has replied to a ask we sent
-        case opcprotman_opc_sig:    // someboby has spontaneously sent some data
-        {   
-        
-            printf("received data of transceiver");
-        
-        } break;
-    }       
-    
-}
+*/
 
 
 
@@ -171,41 +129,54 @@ extern opcprotman_res_t opcprotman_personalize_database(OPCprotocolManager *p)
 {
     opcprotman_res_t res = opcprotman_OK;
 
-//commented because not used!!!
-///* personalize eom_ipnet_hid_DEBUG_id var*/
-//	res = opcprotman_personalize_var(   p,
-//                                        eom_ipnet_hid_DEBUG_id,
-//                                        (uint8_t*) &eom_ipnet_hid_DEBUG,
-//                                        on_rec_ipnet_debug);
-//
-//    if(opcprotman_OK != res)
-//    {
-//        return(res);
-//    }
-
-/* personalize eom_emsrunner_hid_DEBUG_id var*/
+/* personalize eodgn_nvidbdoor_cmd */
 	res = opcprotman_personalize_var(   p,
-                                        eom_emsrunner_hid_DEBUG_id,
-                                        NULL,  //use NULL because i'd like print received data and not store them!!
-                                        	   //pay attention: see NOTE 1 at the end of this function!!!
-                                        on_rec_runner_debug);
+                                        eodgn_nvidbdoor_cmds,
+                                        (void*)&dgnCommands,
+                                        NULL); //here I'm not interested in callback func on received, becose ems never will send me it
 
     if(opcprotman_OK != res)
     {
         return(res);
     }
 
-//commented because not used!!!
-///* personalize eom_emstransceiver_hid_DEBUG_id var*/
-//	res = opcprotman_personalize_var(   p,
-//                                        eom_emstransceiver_hid_DEBUG_id,
-//                                        (uint8_t*)&eom_emstransceiver_hid_DEBUG,
-//                                        on_rec_transceiver_debug);
-//
-//    if(opcprotman_OK != res)
-//    {
-//        return(res);
-//    }
+/*personalize eodgn_nvidbdoor_emsperiph var*/
+	res = opcprotman_personalize_var(   p,
+                                        eodgn_nvidbdoor_emsperiph,
+                                        NULL,  //use NULL because i'd like print received data and not store them!!
+                                        	   //pay attention: see NOTE 1 at the end of this function!!!
+                                        on_rec_emsperiph);
+
+    if(opcprotman_OK != res)
+    {
+        return(res);
+    }
+
+
+/*personalize eodgn_nvidbdoor_emsperiph var*/
+	res = opcprotman_personalize_var(   p,
+                                        eodgn_nvidbdoor_emsapplcommon,
+                                        NULL,  //use NULL because i'd like print received data and not store them!!
+                                        	   //pay attention: see NOTE 1 at the end of this function!!!
+                                        on_rec_emsapplcommon);
+
+    if(opcprotman_OK != res)
+    {
+        return(res);
+    }
+
+
+/*personalize eodgn_nvidbdoor_emsperiph var*/
+	res = opcprotman_personalize_var(   p,
+                                        eodgn_nvidbdoor_emsapplmc,
+                                        NULL,  //use NULL because i'd like print received data and not store them!!
+                                        	   //pay attention: see NOTE 1 at the end of this function!!!
+                                        on_rec_emsapplmc);
+
+    if(opcprotman_OK != res)
+    {
+        return(res);
+    }
 
 
 
@@ -230,9 +201,154 @@ extern opcprotman_res_t opcprotman_personalize_database(OPCprotocolManager *p)
 // --------------------------------------------------------------------------------------------------------------------
 // - definition of static functions 
 // --------------------------------------------------------------------------------------------------------------------
+static void s_print_emsperiph_candata(eOdgn_canstatus_t *canst_ptr)
+{
+    printf("\t\twarning=%d passive=%d busoff=%d\n", canst_ptr->hw.warning, canst_ptr->hw.passive, canst_ptr->hw.busoff);  
+    printf("\t\thw_rx_queue_is_full=%d\n", canst_ptr->hw.rxqueueisfull);
+    printf("\t\tsw_rx_queue_is_full=%d sw_tx_queue_is_full=%d\n", canst_ptr->sw.rxqueueisfull, canst_ptr->sw.txqueueisfull);
+}
+
+static void on_rec_emsperiph(opcprotman_opc_t opc, opcprotman_var_map_t* map, void* recdata)
+{
+    eOdgn_emsperipheralstatus_t* data = (eOdgn_emsperipheralstatus_t*)recdata;
+    
+    switch(opc)
+    {
+        
+        default:
+        case opcprotman_opc_set:
+        {   // nobody can order that to us           
+            // we just dont do it ...         
+        } break;
+        
+        case opcprotman_opc_say:    // someboby has replied to a ask we sent
+        case opcprotman_opc_sig:    // someboby has spontaneously sent some data
+        {   
+        
+            printf("\n\n-----EMS periph data---\n");
+            printf("\t CAN 1:\n");
+            s_print_emsperiph_candata(&data->can_dev[0]);
+            printf("\t CAN 2:\n");
+            s_print_emsperiph_candata(&data->can_dev[1]);
+                    
+        } break;
+    }       
 
 
+}
 
+static void on_rec_emsapplcommon(opcprotman_opc_t opc, opcprotman_var_map_t* map, void* recdata)
+{
+
+    eOdgn_emsapplication_common_t* data = (eOdgn_emsapplication_common_t*)recdata;
+    
+    switch(opc)
+    {
+        
+        default:
+        case opcprotman_opc_set:
+        {   // nobody can order that to us           
+            // we just dont do it ...         
+        } break;
+        
+        case opcprotman_opc_say:    // someboby has replied to a ask we sent
+        case opcprotman_opc_sig:    // someboby has spontaneously sent some data
+        {   
+        
+            printf("\n\n-----EMS appl common---\n");
+
+            s_print_emsapplcomm_core(&data->core);
+            s_print_emsapplcomm_ipnet(&data->ipnet);
+            s_print_emsapplcomm_transceiver(&data->transceiver);
+
+            fflush(stdout);    
+        } break;
+    }       
+}
+
+static void s_print_emsapplcomm_core(eOdgn_coreapplication_t *appcore_ptr)
+{
+    printf("\tRUNNING ST\n");
+	    printf("\t\tnumberofperiods=%lld\n ", appcore_ptr->runst.numberofperiods);
+	    printf("\t\tcumulativeabsoluteerrorinperiod=%lld\n ", appcore_ptr->runst.cumulativeabsoluteerrorinperiod);
+	    printf("\t\tmeanofabsoluteerrorinperiod=%d\n ", appcore_ptr->runst.meanofabsoluteerrorinperiod);
+
+	    printf("\t\tmovingmeanofabsoluteerrorinperiod=%d\n ", appcore_ptr->runst.movingmeanofabsoluteerrorinperiod);
+	    printf("\t\tmaxabsoluteerrorinperiod=%d\n ", appcore_ptr->runst.maxabsoluteerrorinperiod);
+	    printf("\t\tminabsoluteerrorinperiod=%d\n ", appcore_ptr->runst.minabsoluteerrorinperiod);
+
+	    printf("\t\texecutionoverflows[0]=%d\n ", appcore_ptr->runst.executionoverflows[0]);
+	    printf("\t\texecutionoverflows[1]=%d\n ", appcore_ptr->runst.executionoverflows[1]);
+	    printf("\t\texecutionoverflows[2]=%d\n ", appcore_ptr->runst.executionoverflows[2]);
+	    printf("\t\tdatagrams_failed_to_go_in_txsocket=%d\n ", appcore_ptr->runst.datagrams_failed_to_go_in_txsocket);
+        printf("\t\tcantxfailuretimeoutsemaphore=%d\n ", appcore_ptr->runst.cantxfailuretimeoutsemaphore);
+
+    printf("\tCONFIG ST\n");
+        printf("\t\tcantxfailuretimeoutsemaphore=%d\n ", appcore_ptr->cfgst.cantxfailuretimeoutsemaphore);
+}
+static void s_print_emsapplcomm_ipnet(eOdgn_ipnet_t  *appipnet_ptr)
+{
+    printf("\tIPNET INFO\n");
+	    printf("\t\tdatagrams_failed_to_go_in_rxfifo=%d\n ", appipnet_ptr->datagrams_failed_to_go_in_rxfifo);
+	    printf("\t\tdatagrams_failed_to_go_in_txosalqueue=%d\n ", appipnet_ptr->datagrams_failed_to_go_in_txosalqueue);
+	    printf("\t\tdatagrams_failed_to_be_retrieved_from_txfifo=%d\n ", appipnet_ptr->datagrams_failed_to_be_retrieved_from_txfifo);
+	    printf("\t\tdatagrams_failed_to_be_sent_by_ipal=%d\n ", appipnet_ptr->datagrams_failed_to_be_sent_by_ipal);
+}
+static void s_print_emsapplcomm_transceiver(eOdgn_embObjtransceiver_t  *apptrans_ptr)
+{
+
+    printf("\tTRANSCEIVER INFO\n");
+	    printf("\t\trxinvalidropframes=%d\n ", apptrans_ptr->rxinvalidropframes);
+	    printf("\t\trxseqnumwrong=%d\n ", apptrans_ptr->rxseqnumwrong);
+	    printf("\t\tlostreplies=%d\n ", apptrans_ptr->lostreplies);
+	    printf("\t\tfailuresinloadofreplyropframe=%d\n ", apptrans_ptr->failuresinloadofreplyropframe);
+	    printf("\t\ttxropframeistoobigforthepacket=%d\n ", apptrans_ptr->txropframeistoobigforthepacket);
+	    printf("\t\tcannotloadropinregulars=%d\n ", apptrans_ptr->cannotloadropinregulars);
+	    printf("\t\tcannotloadropinoccasionals=%d\n ", apptrans_ptr->cannotloadropinoccasionals);
+
+
+}
+
+
+static void on_rec_emsapplmc(opcprotman_opc_t opc, opcprotman_var_map_t* map, void* recdata)
+{
+
+    eOdgn_emsapplication_emswithmc_t* data = (eOdgn_emsapplication_emswithmc_t*)recdata;
+    
+    switch(opc)
+    {
+        
+        default:
+        case opcprotman_opc_set:
+        {   // nobody can order that to us           
+            // we just dont do it ...         
+        } break;
+        
+        case opcprotman_opc_say:    // someboby has replied to a ask we sent
+        case opcprotman_opc_sig:    // someboby has spontaneously sent some data
+        {   
+        
+            printf("\n\n-----EMS appl mc---\n");
+
+            s_print_emsapplmc_encoderserror(&(data->encreads));
+
+            fflush(stdout);    
+        } break;
+    }      
+
+}
+
+static void s_print_emsapplmc_encoderserror(eOdgn_encoderreads_t *encreads)
+{
+    uint32_t i;
+    for(i=0; i<6; i++)
+    {
+        printf("Encoder num %d\n", i);
+        printf("\t err_onReadFromSpi=%d   ", encreads->encList[i].err_onReadFromSpi);  
+        printf("err_onParityError=%d  ", encreads->encList[i].err_onParityError);  
+        printf("err_onInvalidValue=%d\n", encreads->encList[i].err_onInvalidValue);  
+    }
+}
 // --------------------------------------------------------------------------------------------------------------------
 // - end-of-file (leave a blank line after)
 // --------------------------------------------------------------------------------------------------------------------
