8f671e4c76b5a35a10f7413ba7e621755f3f8599
Marco Randazzo <marco.randazzo@iit.it>
2013-05-20 12:45:51 +0000

    added canBusSkin, canBusAnalogSensor
    svn path=/trunk/iCub/; revision=23982

---
+++ src/libraries/icubmod/canBusAnalogSensor/CanBusAnalogSensor.cpp
@@ -0,0 +1,508 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+// Copyright: (C) 2010 RobotCub Consortium
+// Authors: Lorenzo Natale
+// CopyPolicy: Released under the terms of the GNU GPL v2.0.
+
+#include <CanBusAnalogSensor.h>
+
+#include <yarp/os/Time.h>
+#include <iostream>
+#include <string.h>
+
+const int CAN_DRIVER_BUFFER_SIZE=2047;
+
+using namespace std;
+
+bool CanBusAnalogSensor::open(yarp::os::Searchable& config)
+{
+    bool correct=true;
+
+    //debug
+    fprintf(stderr, "%s\n", config.toString().c_str());
+
+    correct &= config.check("CanbusDevice");
+    correct &= config.check("CanDeviceNum");
+    correct &= config.check("CanAddress");
+    correct &= config.check("Format");
+    correct &= config.check("Period");
+    correct &= config.check("Channels");
+    
+    if (!correct)
+    {
+        std::cerr<<"Error: insufficient parameters to CanBusAnalogSensor\n"; 
+        return false;
+    }
+
+    int period=config.find("Period").asInt();
+    setRate(period);
+
+    Property prop;
+
+    prop.put("device", config.find("CanbusDevice").asString().c_str());
+    prop.put("physdevice", config.find("physdevice").asString().c_str());
+    prop.put("CanTxTimeout", 500);
+    prop.put("CanRxTimeout", 500);
+    prop.put("CanDeviceNum", config.find("CanDeviceNum").asInt());
+    prop.put("CanMyAddress", 0);
+    prop.put("CanTxQueueSize", CAN_DRIVER_BUFFER_SIZE);
+    prop.put("CanRxQueueSize", CAN_DRIVER_BUFFER_SIZE);
+    pCanBus=0;
+    pCanBufferFactory=0;
+
+    //open the can driver
+    driver.open(prop);
+    if (!driver.isValid())
+    {
+        fprintf(stderr, "Error opening PolyDriver check parameters\n");
+        return false;
+    }
+    driver.view(pCanBus);
+    if (!pCanBus)
+    {
+        fprintf(stderr, "Error opening can device not available\n");
+        return false;
+    }
+    driver.view(pCanBufferFactory);
+    outBuffer=pCanBufferFactory->createBuffer(CAN_DRIVER_BUFFER_SIZE);
+    inBuffer=pCanBufferFactory->createBuffer(CAN_DRIVER_BUFFER_SIZE);
+
+    //select the communication speed
+    pCanBus->canSetBaudRate(0); //default 1MB/s
+
+
+    //set the internal configuration
+    //this->isVirtualSensor   = false;
+    this->boardId           = config.find("CanAddress").asInt();
+    this->dataFormat        = (AnalogDataFormat)(config.find("Format").asInt());
+    this->analogChannels    = config.find("Channels").asInt();
+    this->useCalibration    = (config.find("UseCalibration").asInt()==1);
+    //this->SensorFullScale = config.find("FullScale").asInt();
+
+    //open the can mask for the specific canDeviceId
+    for (int id=0; id<16; ++id)
+    {
+        pCanBus->canIdAdd(0x300+(boardId<<4)+id);
+    }
+
+    //create the data vector:
+    int chan=config.find("Channels").asInt();
+    sensorsNum = chan>=1?chan:1;
+    data.resize(sensorsNum);
+    scaleFactor.resize(sensorsNum);
+
+    //start the sensor broadcast
+    sensor_start(config);
+
+    RateThread::start();
+    return true;
+}
+
+bool CanBusAnalogSensor::readFullScaleAnalog(int ch)
+{
+    scaleFactor[ch]=1e-20;
+
+    unsigned int canMessages=0;
+    unsigned id = 0x200 + boardId;
+    CanMessage &msg=outBuffer[0];
+    msg.setId(id);
+    msg.getData()[0]=0x18;
+    msg.getData()[1]=ch;
+    msg.setLen(2);
+    canMessages=0;
+    pCanBus->canWrite(outBuffer, 1, &canMessages);
+
+    long int timeout=0;
+    bool full_scale_read=false;
+    do 
+    {
+        //pCanBus->canRead();
+        unsigned int num_messages = 0;
+        for (unsigned int i=0; i<num_messages; i++)
+        {
+            /*// CanMessage& m= res._readBuffer[i];
+            unsigned int currId=m.getId();
+            if (currId==(0x0200|sensor_id<<4))
+                if (m.getLen()==4 &&
+                    m.getData()[0]==0x18 &&
+                    m.getData()[1]==ch)
+                    {
+                        scaleFactor[ch]=m.getData()[2]<<8 | m.getData()[3];
+                        full_scale_read=true;
+                        break;
+                    }*/
+        }
+        yarp::os::Time::delay(0.002);
+        timeout++;
+    }
+    while(timeout<32 && full_scale_read==false);
+
+    if (full_scale_read==false) 
+        {                            
+            fprintf(stderr, "*** ERROR: Trying to get fullscale data from sensor: no answer received or message lost (ch:%d)\n", ch);
+            return false;
+        }
+
+    return true;
+}
+
+bool CanBusAnalogSensor::sensor_start(yarp::os::Searchable& analogConfig)
+{
+    fprintf(stderr, "--> Initializing analog device %s\n", analogConfig.find("deviceId").toString().c_str());
+
+    unsigned int canMessages=0;
+    unsigned id = 0x200 + boardId;
+
+    if (analogConfig.check("Period"))
+    {
+        int period=analogConfig.find("Period").asInt();
+        CanMessage &msg=outBuffer[0];
+        msg.setId(id);
+        msg.getData()[0]=0x08;
+        msg.getData()[1]=period; 
+        msg.setLen(2);
+        canMessages=0;
+        pCanBus->canWrite(outBuffer, 1, &canMessages);
+    }
+
+    //init message for mais board
+    if (analogChannels==16 && dataFormat==8)
+    {
+        CanMessage &msg=outBuffer[0];
+        msg.setId(id);
+        msg.getData()[0]=0x07;
+        msg.getData()[1]=0x00; 
+        msg.setLen(2);
+        canMessages=0;
+        pCanBus->canWrite(outBuffer, 1, &canMessages);
+    }
+
+    //init message for strain board
+    else if (analogChannels==6 && dataFormat==16)
+    {
+        //calibrated astrain board
+        if (useCalibration)
+        {
+            //get the full scale values from the strain board
+            for (int ch=0; ch<6; ch++)
+            {
+                bool b=false;
+                int attempts = 0;
+                while(attempts<15) 
+                {
+                    b = readFullScaleAnalog(ch);
+                    if (b==true) 
+                        {
+                            if (attempts>0)    fprintf(stderr, "*** WARNING: Trying to get fullscale data from sensor: channel recovered (ch:%d)\n", ch);
+                            break;
+                        }
+                    attempts++;
+                    yarp::os::Time::delay(0.020);
+                }
+                if (attempts>=15)
+                {
+                    fprintf(stderr, "*** ERROR: Trying to get fullscale data from sensor: all attempts failed (ch:%d)\n", ch);
+                }
+            }
+
+            // debug messages
+            #if 1
+                    fprintf(stderr, "Sensor Fullscale Id %#4X: ",boardId);
+                    fprintf(stderr, " %f ", scaleFactor[0]);
+                    fprintf(stderr, " %f ", scaleFactor[1]);
+                    fprintf(stderr, " %f ", scaleFactor[2]);
+                    fprintf(stderr, " %f ", scaleFactor[3]);
+                    fprintf(stderr, " %f ", scaleFactor[4]);
+                    fprintf(stderr, " %f ", scaleFactor[5]);
+                    fprintf(stderr, " \n ");
+            #endif
+
+            // start the board
+            CanMessage &msg=outBuffer[0];
+            msg.setId(id);
+            msg.getData()[0]=0x07;
+            msg.getData()[1]=0x00; 
+            msg.setLen(2);
+            canMessages=0;
+            pCanBus->canWrite(outBuffer, 1, &canMessages);
+        }
+        //not calibrated strain board
+        else
+        {
+            CanMessage &msg=outBuffer[0];
+            msg.setId(id);
+            msg.getData()[0]=0x07;
+            msg.getData()[1]=0x03; 
+            msg.setLen(2);
+            canMessages=0;
+            pCanBus->canWrite(outBuffer, 1, &canMessages);
+        }
+    }
+    return true;
+}
+
+bool CanBusAnalogSensor::sensor_stop()
+{
+    unsigned int canMessages=0;
+    unsigned id = 0x200 + boardId;
+    CanMessage &msg=outBuffer[0];
+    msg.setId(id);
+    msg.getData()[0]=0x07;
+    msg.getData()[1]=0x01; 
+    msg.setLen(2);
+    canMessages=0;
+    pCanBus->canWrite(outBuffer, 1, &canMessages);
+    return true;
+}
+
+bool CanBusAnalogSensor::close()
+{
+    //stop the thread
+    RateThread::stop();
+
+    //stop the sensor
+    sensor_stop();
+
+    //stop the driver
+    if (pCanBufferFactory)
+    {
+        pCanBufferFactory->destroyBuffer(inBuffer);
+        pCanBufferFactory->destroyBuffer(outBuffer);
+    }
+    driver.close();
+
+    return true;
+}
+
+int CanBusAnalogSensor::read(yarp::sig::Vector &out) 
+{
+    mutex.wait();
+    out=data;
+    mutex.post();
+
+    return yarp::dev::IAnalogSensor::AS_OK;
+}
+
+int CanBusAnalogSensor::getState(int ch)
+{
+    return yarp::dev::IAnalogSensor::AS_OK;
+}
+
+int CanBusAnalogSensor::getChannels()
+{
+    return sensorsNum;
+}
+
+int CanBusAnalogSensor::calibrateSensor()
+{
+    //NOT YET IMPLEMENTED
+    return calibrateSensor();
+}
+
+int CanBusAnalogSensor::calibrateChannel(int ch, double v)
+{
+    //NOT YET IMPLEMENTED
+    return calibrateChannel(ch, 0);
+}
+
+int CanBusAnalogSensor::calibrateSensor(const yarp::sig::Vector& v)
+{
+    //NOT YET IMPLEMENTED
+    return 0;
+}
+
+int CanBusAnalogSensor::calibrateChannel(int ch)
+{
+    //NOT YET IMPLEMENTED
+    return 0;
+}
+
+bool CanBusAnalogSensor::threadInit()
+{
+    unsigned int canMessages=0;
+    unsigned id = 0x200 + boardId;
+       
+    CanMessage &msg=outBuffer[0];
+    msg.setId(id);
+    msg.getData()[0]=0x4C; // message type
+    msg.getData()[1]=0x01; 
+    msg.getData()[2]=0x01; 
+    msg.getData()[3]=0x01;
+    msg.getData()[4]=0;
+    msg.getData()[5]=0x22;
+    msg.getData()[6]=0;
+    msg.getData()[7]=0;
+    msg.setLen(8);
+    canMessages=0;
+    pCanBus->canWrite(outBuffer, 1, &canMessages);
+
+    return true;
+}
+
+bool CanBusAnalogSensor::decode16(const unsigned char *msg, int msg_id, double *data)
+{
+    const char groupId=(msg_id & 0x00f);
+    int baseIndex=0;
+    {
+        switch (groupId)
+        {
+        case 0xA:
+            {
+                for(int k=0;k<3;k++)
+                    {
+                        data[k]=(((unsigned short)(msg[2*k+1]))<<8)+msg[2*k]-0x8000;
+                        if (useCalibration==1)
+                        {
+                            data[k]=data[k]*scaleFactor[k]/float(0x8000);
+                        }
+                    }
+            }
+            break;
+        case 0xB:
+            {
+                for(int k=0;k<3;k++)
+                    {
+                        data[k+3]=(((unsigned short)(msg[2*k+1]))<<8)+msg[2*k]-0x8000;
+                        if (useCalibration==1)
+                        {
+                            data[k+3]=data[k+3]*scaleFactor[k+3]/float(0x8000);
+                        }
+                    }
+            }
+            break;
+        case 0xC:
+            {} //skip these, they are not for us
+            break;
+        case 0xD:
+            {} //skip these, they are not for us
+            break;
+        default:
+            fprintf(stderr, "Warning, got unexpected class 0x3 msg(s): groupId 0x%x\n", groupId);
+            return false;
+            break;
+        }
+        //@@@DEBUG ONLY
+        //fprintf(stderr, "   %+8.1f %+8.1f %+8.1f %+8.1f %+8.1f %+8.1f\n",data[0],data[1],data[2],data[3],data[4],data[5],data[6]);
+    }
+
+    return true;
+}
+
+bool CanBusAnalogSensor::decode8(const unsigned char *msg, int msg_id, double *data)
+{
+    const char groupId=(msg_id & 0x00f);
+    int baseIndex=0;
+    {
+        switch (groupId)
+        {
+        case 0xC:
+            {
+                for(int k=0;k<=6;k++)
+                    data[k]=msg[k];
+            }
+            break;
+        case 0xD:
+            {
+                for(int k=0;k<=7;k++)
+                    data[7+k]=msg[k];
+            }
+            break;
+        case 0xA:
+            {} //skip these, they are not for us
+            break;
+        case 0xB:
+            {} //skip these, they are not for us
+            break;
+        default:
+            fprintf(stderr, "Warning, got unexpected class 0x3 msg(s): groupId 0x%x\n", groupId);
+            return false;
+            break;
+        }
+        //@@@DEBUG ONLY
+        //fprintf(stderr, "   %+8.1f %+8.1f %+8.1f %+8.1f %+8.1f %+8.1f\n",data[0],data[1],data[2],data[3],data[4],data[5],data[6]);
+    }
+    return true;
+}
+
+void CanBusAnalogSensor::run()
+{    
+    mutex.wait();
+
+    unsigned int canMessages=0;
+    bool ret=true; //return true by default
+
+    bool res=pCanBus->canRead(inBuffer,CAN_DRIVER_BUFFER_SIZE,&canMessages);
+    if (!res)
+    {
+        std::cerr<<"canRead failed\n";
+    }
+
+    double timeNow=Time::now();
+    for (unsigned int i=0; i<canMessages; i++)
+    {
+        CanMessage &msg=inBuffer[i];
+
+        unsigned int msgid    = msg.getId();
+        unsigned char *buff   = msg.getData();
+        unsigned int len      = msg.getLen();
+        unsigned int id       = (msgid & 0x00f0)>>4;
+        unsigned int sensorId = msgid&0x000f;
+        //unsigned int type   = buff[0]&0x80;
+        const char   type     = ((msgid&0x700)>>8);
+
+        //parse data here
+        status=IAnalogSensor::AS_OK;
+        
+        if (type==0x03) //analog data
+        {
+            if (id==boardId)
+            {
+                timeStamp=Time::now();
+                switch (dataFormat)
+                {
+                    case ANALOG_FORMAT_8:
+                        ret=decode8(buff, msgid, data.data());
+                        status=IAnalogSensor::AS_OK;
+                        break;
+                    case ANALOG_FORMAT_16:
+                        if (len==6) 
+                        {
+                            ret=decode16(buff, msgid, data.data());
+                            status=IAnalogSensor::AS_OK;
+                        }
+                        else
+                        {
+                            if (len==7 && buff[6] == 1)
+                            {
+                                status=IAnalogSensor::AS_OVF;
+                            }
+                            else
+                            {
+                                status=IAnalogSensor::AS_ERROR;
+                            }
+                            ret=decode16(buff, msgid, data.data());
+                        }
+                        break;
+                    default:
+                        status=IAnalogSensor::AS_ERROR;
+                        ret=false;
+                }
+            }
+        }
+    }
+
+    //if 100ms have passed since the last received message
+    if (timeStamp+0.1<timeNow)
+    {
+        status=IAnalogSensor::AS_TIMEOUT;
+    }
+
+    mutex.post();
+}
+
+void CanBusAnalogSensor::threadRelease()
+{
+    printf("CanBusAnalogSensor Thread releasing...\n");
+    printf("... done.\n");
+}
+
---
+++ src/libraries/icubmod/canBusAnalogSensor/CanBusAnalogSensor.h
@@ -0,0 +1,211 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+// Copyright: (C) 2010 RobotCub Consortium
+// Authors: Lorenzo Natale
+// CopyPolicy: Released under the terms of the GNU GPL v2.0.
+
+#ifndef __SKIN_MESH_THREAD_H__
+#define __SKIN_MESH_THREAD_H__
+
+//#include <stdio.h>
+#include <string>
+
+#include <yarp/os/RateThread.h>
+#include <yarp/os/Semaphore.h>
+#include <yarp/dev/ControlBoardInterfaces.h>
+#include <yarp/dev/IAnalogSensor.h>
+#include <yarp/dev/PolyDriver.h>
+#include <yarp/dev/CanBusInterface.h>
+#include <yarp/sig/Vector.h>
+
+using namespace yarp::os;
+using namespace yarp::dev;
+
+class CanBusAnalogSensor : public RateThread, public yarp::dev::IAnalogSensor, public DeviceDriver 
+{
+    enum AnalogDataFormat
+    {
+        ANALOG_FORMAT_8,
+        ANALOG_FORMAT_16,
+    };
+
+    enum SensorStatus
+    {
+        ANALOG_IDLE=0,
+        ANALOG_OK=1,
+        ANALOG_NOT_RESPONDING=-1,
+        ANALOG_SATURATION=-2,
+        ANALOG_ERROR=-3,
+    };
+
+protected:
+    PolyDriver         driver;
+    ICanBus            *pCanBus;
+    ICanBufferFactory  *pCanBufferFactory;
+    CanBuffer          inBuffer;
+    CanBuffer          outBuffer;
+   
+    yarp::os::Semaphore mutex;
+
+    unsigned int       analogChannels;
+    unsigned short     boardId;
+    int                sensorsNum;
+    short              status;
+    double             timeStamp;
+    AnalogDataFormat   dataFormat;
+    yarp::sig::Vector  data;
+    yarp::sig::Vector  scaleFactor;
+    bool               useCalibration;
+
+public:
+    CanBusAnalogSensor(int period=20) : RateThread(period),mutex(1)
+    {}
+    
+
+    ~CanBusAnalogSensor()
+    {
+    }
+
+    virtual bool open(yarp::os::Searchable& config);
+    virtual bool close();
+   
+    
+    //IAnalogSensor interface
+    virtual int read(yarp::sig::Vector &out);
+    virtual int getState(int ch);
+    virtual int getChannels();
+    int calibrateSensor();
+    virtual int calibrateChannel(int ch, double v);
+
+    virtual int calibrateSensor(const yarp::sig::Vector& v);
+    virtual int calibrateChannel(int ch);
+
+    virtual bool threadInit();
+    virtual void threadRelease();
+    virtual void run();
+
+    //internal methods
+    private:
+    bool decode8 (const unsigned char *msg, int msg_id, double *data);
+    bool decode16(const unsigned char *msg, int msg_id, double *data);
+    bool sensor_start (yarp::os::Searchable& config);
+    bool sensor_stop  ();
+    bool readFullScaleAnalog(int ch);
+};
+
+
+///------------------------------------------------------------------------
+
+
+
+///--------------------------------------------------------
+/*
+
+class TBR_AnalogSensor: public yarp::dev::IAnalogSensor,
+                    public yarp::dev::DeviceDriver
+{
+public:
+    enum AnalogDataFormat
+    {
+        ANALOG_FORMAT_8,
+        ANALOG_FORMAT_16,
+    };
+
+    enum SensorStatus
+    {
+        ANALOG_IDLE=0,
+        ANALOG_OK=1,
+        ANALOG_NOT_RESPONDING=-1,
+        ANALOG_SATURATION=-2,
+        ANALOG_ERROR=-3,
+    };
+
+private:
+    // debug messages
+    unsigned int counterSat;
+    unsigned int counterError;
+    unsigned int counterTimeout;
+    int rate;
+
+    ////////////////////
+    //AnalogData *data;
+    short status;
+    double timeStamp;
+    double* scaleFactor;
+    yarp::os::Semaphore mutex;
+    AnalogDataFormat dataFormat;
+    yarp::os::Bottle initMsg;
+    yarp::os::Bottle speedMsg;
+    yarp::os::Bottle closeMsg;
+    std::string deviceIdentifier;
+    short boardId;
+    short useCalibration;
+    bool  isVirtualSensor; //RANDAZ
+
+    bool decode8(const unsigned char *msg, int id, double *data);
+    bool decode16(const unsigned char *msg, int id, double *data);
+
+public:
+    TBR_CanBackDoor* backDoor; //RANDAZ
+
+    TBR_AnalogSensor();
+    ~TBR_AnalogSensor();
+    bool handleAnalog(void *);
+
+    void resetCounters()
+    {
+        counterSat=0;
+        counterError=0;
+        counterTimeout=0;
+    }
+
+    void getCounters(unsigned int &sat, unsigned int &err, unsigned int &to)
+    {
+        sat=counterSat;
+        err=counterError;
+        to=counterTimeout;
+    }
+
+    void setDeviceId(std::string id)
+    {
+        deviceIdentifier=id;
+    }
+
+    std::string getDeviceId()
+    {
+        return deviceIdentifier;
+    }
+
+    short getId()
+    { return boardId;}
+
+    short getStatus()
+    { return status;}
+
+    bool isOpen()
+    {
+        if (data)
+            return true;
+        else
+            return false;
+    }
+
+    short getUseCalibration()
+        {return useCalibration;}
+    double* getScaleFactor()
+        {return scaleFactor;}
+    double getScaleFactor(int chan)
+        {
+            if (chan>=0 && chan<data->size())
+                return scaleFactor[chan];
+            else
+                return 0;
+        }
+
+    bool open(int channels, AnalogDataFormat f, short bId, short useCalib, bool isVirtualSensor);
+
+    /////////////////////////////////
+};
+*/
+
+#endif
