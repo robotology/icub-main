a74b17ecbfe38ec0540ee048b9cdc6b39660b273
Daniele E. Domenichelli <daniele.domenichelli@iit.it>
2013-05-27 10:32:54 +0000

    Fix pkg-config usage on mac in FindIPOPT.cmake
    svn path=/trunk/iCub/; revision=24134

--- conf/FindIPOPT.cmake
+++ conf/FindIPOPT.cmake
@@ -19,18 +19,39 @@
 
 if(APPLE)
 
-    set(IPOPT_DIR $ENV{IPOPT_DIR} CACHE PATH "Path to IPOPT build directory")
-
-    find_package(PkgConfig)
+    # On APPLE we use PkgConfig to find IPOPT
+    # TODO use it on UNIX as well
+    find_package(PkgConfig QUIET)
     if(PKG_CONFIG_FOUND)
-        pkg_check_modules(IPOPT ipopt)
-        link_directories(${IPOPT_LIBRARY_DIRS}) # vital on Macs, but not many
-                                             # ipopt-using programs do this
 
-        foreach(arg ${IPOPT_LDFLAGS})
-            set(IPOPT_LINK_FLAGS "${IPOPT_LINK_FLAGS} ${arg}")
-        endforeach(arg ${IPOPT_LDFLAGS})
-    endif()
+        if(IPOPT_FIND_VERSION)
+            if(IPOPT_FIND_VERSION_EXACT)
+                pkg_check_modules(_PC_IPOPT QUIET ipopt=${IPOPT_FIND_VERSION})
+            else(IPOPT_FIND_VERSION_EXACT)
+                pkg_check_modules(_PC_IPOPT QUIET ipopt>=${IPOPT_FIND_VERSION})
+            endif(IPOPT_FIND_VERSION_EXACT)
+        else(IPOPT_FIND_VERSION)
+            pkg_check_modules(_PC_IPOPT QUIET ipopt)
+        endif(IPOPT_FIND_VERSION)
+
+
+        if(_PC_IPOPT_FOUND)
+            set(IPOPT_INCLUDE_DIRS ${_PC_IPOPT_INCLUDE_DIRS} CACHE PATH "IPOPT include directory")
+            set(IPOPT_DEFINITIONS ${_PC_IPOPT_CFLAGS_OTHER} CACHE STRING "Additional compiler flags for IPOPT")
+            set(IPOPT_LIBRARIES "" CACHE STRING "IPOPT libraries" FORCE)
+            foreach(_LIBRARY IN ITEMS ${_PC_IPOPT_LIBRARIES})
+                find_library(${_LIBRARY}_PATH
+                            NAMES ${_LIBRARY}
+                            PATHS ${_PC_IPOPT_LIBRARY_DIRS})
+                    list(APPEND IPOPT_LIBRARIES ${${_LIBRARY}_PATH})
+            endforeach(_LIBRARY)
+        endif(_PC_IPOPT_FOUND)
+
+        mark_as_advanced(IPOPT_INCLUDE_DIRS
+                         IPOPT_LIBRARIES
+                         IPOPT_DEFINITIONS)
+
+    endif(PKG_CONFIG_FOUND)
 
 elseif(UNIX)
 
