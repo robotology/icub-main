87f06241ae0e33acc695a3a6f6fe97de7d629c26
Ugo Pattacini <ugo.pattacini@iit.it>
2013-06-24 16:48:45 +0000

    No commit message
    svn path=/trunk/iCub/; revision=24698

--- src/libraries/optimization/src/affine.cpp
+++ src/libraries/optimization/src/affine.cpp
@@ -86,7 +86,7 @@ public:
     /****************************************************************/
     virtual void set_A0(const Matrix &A0)
     {
-        int row_max=std::min(this->A0.rows(),A0.rows());
+        int row_max=std::min(this->A0.rows()-1,A0.rows()-1);
         int col_max=std::min(this->A0.cols(),A0.cols());
         this->A0.setSubmatrix(A0.submatrix(0,row_max,0,col_max),0,0);
     }
@@ -247,11 +247,11 @@ void AffineWithMatchedPoints::setBounds(const Matrix &min,
 {
     int row_max,col_max;
 
-    row_max=std::min(this->min.rows(),min.rows());
+    row_max=std::min(this->min.rows()-1,min.rows()-1);
     col_max=std::min(this->min.cols(),min.cols());
     this->min.setSubmatrix(min.submatrix(0,row_max,0,col_max),0,0);
 
-    row_max=std::min(this->max.rows(),max.rows());
+    row_max=std::min(this->max.rows()-1,max.rows()-1);
     col_max=std::min(this->max.cols(),max.cols());
     this->max.setSubmatrix(max.submatrix(0,row_max,0,col_max),0,0);
 }
@@ -312,7 +312,7 @@ void AffineWithMatchedPoints::clearPoints()
 /****************************************************************/
 bool AffineWithMatchedPoints::setInitialGuess(const Matrix &A)
 {
-    int row_max=std::min(A0.rows(),A.rows());
+    int row_max=std::min(A0.rows()-1,A.rows()-1);
     int col_max=std::min(A0.cols(),A.cols());
     A0.setSubmatrix(A.submatrix(0,row_max,0,col_max),0,0);
 
