7c80f51d7677e3bafb84c821da4e6ff0506938c4
Lorenzo Natale <lorenzo.natale@iit.it>
2013-07-18 16:45:10 +0000

    cleanup
    svn path=/trunk/iCub/; revision=25127

--- src/libraries/icubmod/canBusAnalogSensor/CanBusAnalogSensor.h
+++ src/libraries/icubmod/canBusAnalogSensor/CanBusAnalogSensor.h
@@ -4,15 +4,11 @@
 // Authors: Marco Randazzo <marco.randazzo@iit.it>
 // CopyPolicy: Released under the terms of the GNU GPL v2.0.
 
-#ifndef __SKIN_MESH_THREAD_H__
-#define __SKIN_MESH_THREAD_H__
-
-//#include <stdio.h>
-#include <string>
+#ifndef __CANBUSANALOGSENSOR_H__
+#define __CANBUSANALOGSENSOR_H__
 
 #include <yarp/os/RateThread.h>
 #include <yarp/os/Semaphore.h>
-#include <yarp/dev/ControlBoardInterfaces.h>
 #include <yarp/dev/IAnalogSensor.h>
 #include <yarp/dev/PolyDriver.h>
 #include <yarp/dev/CanBusInterface.h>
@@ -93,119 +89,5 @@ public:
     bool readFullScaleAnalog(int ch);
 };
 
-
-///------------------------------------------------------------------------
-
-
-
-///--------------------------------------------------------
-/*
-
-class TBR_AnalogSensor: public yarp::dev::IAnalogSensor,
-                    public yarp::dev::DeviceDriver
-{
-public:
-    enum AnalogDataFormat
-    {
-        ANALOG_FORMAT_8,
-        ANALOG_FORMAT_16,
-    };
-
-    enum SensorStatus
-    {
-        ANALOG_IDLE=0,
-        ANALOG_OK=1,
-        ANALOG_NOT_RESPONDING=-1,
-        ANALOG_SATURATION=-2,
-        ANALOG_ERROR=-3,
-    };
-
-private:
-    // debug messages
-    unsigned int counterSat;
-    unsigned int counterError;
-    unsigned int counterTimeout;
-    int rate;
-
-    ////////////////////
-    //AnalogData *data;
-    short status;
-    double timeStamp;
-    double* scaleFactor;
-    yarp::os::Semaphore mutex;
-    AnalogDataFormat dataFormat;
-    yarp::os::Bottle initMsg;
-    yarp::os::Bottle speedMsg;
-    yarp::os::Bottle closeMsg;
-    std::string deviceIdentifier;
-    short boardId;
-    short useCalibration;
-    bool  isVirtualSensor; //RANDAZ
-
-    bool decode8(const unsigned char *msg, int id, double *data);
-    bool decode16(const unsigned char *msg, int id, double *data);
-
-public:
-    TBR_CanBackDoor* backDoor; //RANDAZ
-
-    TBR_AnalogSensor();
-    ~TBR_AnalogSensor();
-    bool handleAnalog(void *);
-
-    void resetCounters()
-    {
-        counterSat=0;
-        counterError=0;
-        counterTimeout=0;
-    }
-
-    void getCounters(unsigned int &sat, unsigned int &err, unsigned int &to)
-    {
-        sat=counterSat;
-        err=counterError;
-        to=counterTimeout;
-    }
-
-    void setDeviceId(std::string id)
-    {
-        deviceIdentifier=id;
-    }
-
-    std::string getDeviceId()
-    {
-        return deviceIdentifier;
-    }
-
-    short getId()
-    { return boardId;}
-
-    short getStatus()
-    { return status;}
-
-    bool isOpen()
-    {
-        if (data)
-            return true;
-        else
-            return false;
-    }
-
-    short getUseCalibration()
-        {return useCalibration;}
-    double* getScaleFactor()
-        {return scaleFactor;}
-    double getScaleFactor(int chan)
-        {
-            if (chan>=0 && chan<data->size())
-                return scaleFactor[chan];
-            else
-                return 0;
-        }
-
-    bool open(int channels, AnalogDataFormat f, short bId, short useCalib, bool isVirtualSensor);
-
-    /////////////////////////////////
-};
-*/
 
 #endif
