fee6695e17ec73af74b66484a225c50b0a22b34d
Alberto Cardellino <alberto.cardellino@iit.it>
2013-10-10 10:15:01 +0000

    ControlBoardWrapper2: modified check for input vector size in streaming port
    due to the new group interface, streaming port accepts vector of size smaller than number of joints.
    
    old check "cmdVector.size() != controlledAxes" -> issue error
    has now been changed to
    "cmdVector.size() > controlledAxes", therefore the error is sent only when trying to control more axis than those actually handled by the wrapper
    
    svn path=/trunk/iCub/; revision=26439

--- src/libraries/icubmod/controlBoardWrapper2/ControlBoardWrapper2wip.cpp
+++ src/libraries/icubmod/controlBoardWrapper2/ControlBoardWrapper2wip.cpp
@@ -829,11 +829,11 @@ void ImplementCallbackHelper2::onRead(CommandMessage& v)
     //fprintf(stderr, "Received command %s, %s\n", b.toString().c_str(), cmdVector.toString().c_str());
 
     // some consistency checks
-    if (controlledAxes!=cmdVector.size())
+    if (cmdVector.size() > controlledAxes)
     {
         yarp::os::ConstString str = yarp::os::Vocab::decode(b.get(0).asVocab());
-        fprintf(stderr, "Received command vector with incorrect number of elements (cmd: %s requested jnts: %d received jnts: %d)\n",str.c_str(),controlledAxes,(int)cmdVector.size());
-//        return;
+        fprintf(stderr, "Received command vector with number of elements bigger than axis controlled by this wrapper (cmd: %s requested jnts: %d received jnts: %d)\n",str.c_str(),controlledAxes,(int)cmdVector.size());
+        return;
     }
     if (cmdVector.data()==0)
     {
