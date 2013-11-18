7a45f66a839a451bece5f2169463220e951f30c1
Lorenzo Natale <lorenzo.natale@iit.it>
2013-11-18 23:06:39 +0000

    example for idl usage added
    svn path=/trunk/iCub/; revision=26982

--- src/examples/rpcIdl/include/RpcServerImpl.h
+++ src/examples/rpcIdl/include/RpcServerImpl.h
@@ -1,56 +1,20 @@
-// This is an automatically-generated file.
-// It could get re-generated if the ALLOW_IDL_GENERATION flag is on.
-
-#ifndef YARP_THRIFT_GENERATOR_IRpcServer
-#define YARP_THRIFT_GENERATOR_IRpcServer
-
-#include <yarp/os/Wire.h>
-#include <yarp/os/idl/WireTypes.h>
-
-class IRpcServer;
+#include <IRpcServer.h>
 
 
-/**
- * IRpcServer
- * Interface for an example module.
- */
-class IRpcServer : public yarp::os::Wire {
+class RpcServerImpl : public IRpcServer {
+  int answer;
+  bool running;
+
 public:
-  IRpcServer() { yarp().setOwner(*this); }
-/**
- * Get answer from server
- * @return the answer
- */
+  RpcServerImpl();
+
   virtual int32_t get_answer();
-/**
- * Set value for future answers.
- * @param rightAnswer new answer
- * @return true if connection was successful
- */
   virtual bool set_answer(const int32_t rightAnswer);
-/**
- * Add one integet to future answers.
- * @param x value to add
- * @return new value
- */
   virtual int32_t add_one(const int32_t x);
-/**
- * Start service
- * @return true if service started correctly
- */
   virtual bool start();
-/**
- * Stop service
- * @return true if service stopped correctly
- */
   virtual bool stop();
-/**
- * Check is service is running
- * @return true/false if service is/is not running
- */
   virtual bool is_running();
-  virtual bool read(yarp::os::ConnectionReader& connection);
 };
 
-#endif
+
 
