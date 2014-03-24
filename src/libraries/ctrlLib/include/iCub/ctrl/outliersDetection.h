921b6574b29a9f1327267bc456200500b93100f7
Ugo Pattacini <ugo.pattacini@iit.it>
2014-03-24 10:51:31 +0100

    [ctrlLib/outliersDetection]: comment updated

--- src/libraries/ctrlLib/include/iCub/ctrl/outliersDetection.h
+++ src/libraries/ctrlLib/include/iCub/ctrl/outliersDetection.h
@@ -89,11 +89,11 @@ public:
     *                relative values is skipped. If the property
     *                <i>sorted</i> is provided, then the data are
     *                expected to be sorted either in ascending or
-    *                descending order; further, if the
-    *                <i>check_outlier</i> property is provided with
-    *                a string value equal to "first" ("last"), then
-    *                only the first (last) value is checked during
-    *                detection.
+    *                descending order; further, in case of sorted
+    *                data, if the <i>check_outlier</i> property is
+    *                provided with a string value equal to "first"
+    *                ("last"), then only the first (last) value is
+    *                checked during detection.
     * @return vector containing the outliers indexes.
     */
     yarp::sig::VectorOf<int> detect(const yarp::sig::Vector &data,
