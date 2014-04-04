fee40ac1994dea5309de19403a5698cef072a272
icub <icub@ale.humanoids.iit.it>
2014-04-04 14:54:19 +0200

    added firewirebusreset executable utility to Dragonfly2 library.

--- src/simulators/iCubSimulation/odesdl/OdeLogicalJoint.cpp
+++ src/simulators/iCubSimulation/odesdl/OdeLogicalJoint.cpp
@@ -273,7 +273,7 @@ void OdeLogicalJoint::setVelocityRaw(double target) {
 
 
 void OdeLogicalJoint::controlModeChanged(int cm){
-    if(hinged){
+    if(hinged && joint){
         // only hinge joints can be torque controlled
         if(cm==MODE_TORQUE){
             dJointSetHingeParam(*joint, dParamFMax, dryFriction);
