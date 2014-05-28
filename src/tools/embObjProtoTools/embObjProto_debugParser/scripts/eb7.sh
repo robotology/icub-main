df66d77c51a319c2402c0a3463cb69bdbd5588ef
Valentina Gaggero <valentina.gaggero@iit.it>
2014-05-28 17:06:13 +0200

    modified script to launch debug parser for each ems board

--- src/tools/embObjProtoTools/embObjProto_debugParser/scripts/eb7.sh
+++ src/tools/embObjProtoTools/embObjProto_debugParser/scripts/eb7.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 echo "launch embObjDebugProtoParser for ems 7....."
-sudo /usr/local/src/robot/iCub/main/src/tools/embObjProtoTools/embObjProto_debugParser/build-pc104/embObjProto_debugParser --filterSrcAddr 10.0.1.7
+sudo /usr/local/src/robot/icub-main/src/tools/embObjProtoTools/embObjProto_debugParser/build-pc104/embObjProto_debugParser --filterSrcAddr 10.0.1.7
 
 
 
