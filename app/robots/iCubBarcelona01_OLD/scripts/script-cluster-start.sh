3b2ef9d497bfa25adec81ff2c52157430856a20b
Ugo Pattacini <ugo.pattacini@iit.it>
2014-09-08 12:35:21 +0200

    iCubBarcelona01_OLD removed

--- app/robots/iCubBarcelona01_OLD/scripts/script-cluster-start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo “Starting the cluster...”
-cd $ICUB_ROOT/app/iCubCluster/scripts
-./icub-cluster.py cluster-config.xml
--- app/robots/iCubBarcelona01_OLD/scripts/script-cluster-stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo “Shuting down the cluster...”
-ssh -t icubclient1 'echo icub | sudo -S poweroff'
-ssh -t icubclient2 'echo icub | sudo -S poweroff'
-ssh -t icubclient3 'echo icub | sudo -S poweroff'
-ssh -t pc104 'echo icub | sudo -S poweroff'
-ssh -t ikart 'echo icubAdmin | sudo -S poweroff'
