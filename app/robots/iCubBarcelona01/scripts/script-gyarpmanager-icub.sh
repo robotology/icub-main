47402562e7fc9a0410959f39b8afa9ddaaded807
Marco Randazzo <marco.randazzo@iit.it>
2014-09-10 00:49:44 +0200

    Revert "iCubBarcelona01_OLD removed"
    This reverts commit 3b2ef9d497bfa25adec81ff2c52157430856a20b.
    reverting changes to iCubBarcelona01

---
+++ app/robots/iCubBarcelona01/scripts/script-gyarpmanager-icub.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo “Starting the gyarpmanager for iCub apps...”
+cd $ICUB_ROOT/app
+gyarpmanager
---
+++ app/robots/iCubBarcelona01/scripts/script_for_hammer.sh
@@ -0,0 +1,14 @@
+
+#/bin/bash
+iCub_SIM &
+sleep 10
+iKinGazeCtrl --from configSim.ini &
+sleep 10
+objectsPropertiesCollector --name OPC --empty &
+simCartesianControl &
+iKinCartesianSolver --context simCartesianControl --part right_arm &
+HammerGUI --from hammergui.ini.sim &
+sleep 10
+HAMMER4iCub --from hammer4icub.ini.sim &
+sleep 10
+HammerResponseExample 
