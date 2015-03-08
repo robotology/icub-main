809fdaac44423563a84db098277b5653c42f901e
Marco Randazzo <marco.randazzo@iit.it>
2015-03-08 17:30:34 +0100

    fixed typo

--- app/robots/experimentalSetups/test_utils/test_leg3.sh
+++ app/robots/experimentalSetups/test_utils/test_leg3.sh
@@ -10,8 +10,8 @@ yarp connect /rl_dumper/right_leg/getEncoders      /rl_encs/pos:i
 yarp connect /ll_dumper/left_leg/getMotorEncoders  /ll_motencs/pos:i
 yarp connect /ll_dumper/left_leg/getEncoders       /ll_encs/pos:i
 read -n1 -r -p "press key to continue" key
-yarpscope --xml yarpscope_speeds_la.xml &
-yarpscope --xml yarpscope_speeds_ra.xml &
+yarpscope --xml yarpscope_speeds_ll.xml &
+yarpscope --xml yarpscope_speeds_rl.xml &
 read -n1 -r -p "press key to terminate" key
 killall -9 controlBoardDumper
 killall -9 velocityObserver
