f79b9aebb7ec206b95e2617dca5db12285118078
Marco Randazzo <marco.randazzo@iit.it>
2015-03-23 16:00:19 +0100

    updated matlab script

--- app/robots/experimentalSetups/test_utils/matlab/verify_encoders.m
+++ app/robots/experimentalSetups/test_utils/matlab/verify_encoders.m
@@ -2,10 +2,11 @@
 % the file to be loaded is just a log of the stateExt:o port
 % it can be obtained with the following command:
 % yarp read ... /icub/left_arm/stateExt:o > logarm.txt
-logdata=logarmsign2;
+logdata=logrightleg;
 
 % the following variable defines the number of joints logged by the port
-number_of_joints = 16;
+number_of_joints = 6;
+number_of_joints_to_plot = 6;
 
 i=0;
 jp = logdata(:,i*number_of_joints+1:i*number_of_joints+number_of_joints);i=i+1;i*number_of_joints % 0
@@ -27,8 +28,11 @@ matrix (3,2) = 1.6455;
 matrix (3,3) = 1.6455;
 elseif (number_of_joints==3) %% torso joints
 matrix = eye(3);
-else
+elseif (number_of_joints==6) %%leg joints
 matrix = eye(number_of_joints);
+matrix (1,1) = 60/40; %%check me
+else
+matrix = eye(number_of_joints); 
 end
 inv_matrix =inv(matrix);
 
@@ -39,132 +43,50 @@ ja2m = ((matrix*ja')*100)';
 %%  JOINT POSITIONS vs MOTOR POSITIONS  -> VERIFIED
 close all
 
-figure(1)
+for j=1:1:number_of_joints_to_plot
+figure(j)
 hold on
 grid on
-stairs (jp2m(:,1)-jp2m(1,1),'b')
-stairs (mp(:,1)-mp(1,1),'r')
-xlim ([0 2000])
-
-
-figure(2)
-hold on
-grid on
-stairs (jp2m(:,2)-jp2m(1,2),'b')
-stairs (mp(:,2)-mp(1,2),'r')
-xlim ([0 2000])
-
-
-figure(3)
-hold on
-grid on
-stairs (jp2m(:,3)-jp2m(1,3),'b')
-stairs (mp(:,3)-mp(1,3),'r')
-xlim ([0 2000])
-
-
-figure(4)
-hold on
-grid on
-stairs (jp2m(:,4)-jp2m(1,4),'b')
-stairs (mp(:,4)-mp(1,4),'r')
-xlim ([0 2000])
-
+stairs (jp2m(:,j)-jp2m(1,j),'b')
+stairs (mp(:,j)-mp(1,j),'r')
+%xlim ([0 2000])
+end
 
 %% JOINT VELOCITES vs MOTOR VELOCITIES
 close all;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-figure(11)
+for j=1:1:number_of_joints_to_plot
+figure(10+j)
 hold on
 grid on
-stairs (jv2m(:,1)-jv2m(1,1),'b')
-stairs (mv(:,1)-mv(1,1),'r')
-xlim ([0 2000])
-
-
-figure(12)
-hold on
-grid on
-stairs (jv2m(:,2)-jv2m(1,2),'b')
-stairs (mv(:,2)-mv(1,2),'r')
-xlim ([0 2000])
-
-
-figure(13)
-hold on
-grid on
-stairs (jv2m(:,3)-jv2m(1,3),'b')
-stairs (mv(:,3)-mv(1,3),'r')
-xlim ([0 2000])
-
-
-figure(14)
-hold on
-grid on
-stairs (jv2m(:,4)-jv2m(1,4),'b')
-stairs (mv(:,4)-mv(1,4),'r')
-xlim ([0 2000])
-
+stairs (jv2m(:,j)-jv2m(1,j),'b')
+stairs (mv(:,j)-mv(1,j),'r')
+%xlim ([0 2000])
+end
 
 %% JOINT POSITIONS(DERIVED) vs JOINT SPEED
 close all;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-figure(31)
-hold on
-grid on
-stairs (diff(jp(:,1)/0.010),'b')
-stairs (jv(:,1),'r')
-xlim ([0 2000])
 
-figure(32)
+for j=1:1:number_of_joints_to_plot
+figure(30+j)
 hold on
 grid on
-stairs (diff(jp(:,2)/0.010),'b')
-stairs (jv(:,2),'r')
-xlim ([0 2000])
-
-figure(33)
-hold on
-grid on
-stairs (diff(jp(:,3)/0.010),'b')
-stairs (jv(:,3),'r')
-xlim ([0 2000])
-
-figure(34)
-hold on
-grid on
-stairs (diff(jp(:,4)/0.010),'b')
-stairs (jv(:,4),'r')
-xlim ([0 2000])
+stairs (diff(jp(:,j)/0.010),'b')
+stairs (jv(:,j),'r')
+%xlim ([0 2000])
+end
 
 %% MOTOR POSITIONS(DERIVED) vs MOTOR SPEED -> VERIFIED
 close all;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-figure(41)
-hold on
-grid on
-stairs (diff(mp(:,1)/0.010),'b')
-stairs (mv(:,1),'r')
-xlim ([0 2000])
 
-figure(42)
+for j=1:1:number_of_joints_to_plot
+figure(40+j)
 hold on
 grid on
-stairs (diff(mp(:,2)/0.010),'b')
-stairs (mv(:,2),'r')
-xlim ([0 2000])
-
-figure(43)
-hold on
-grid on
-stairs (diff(mp(:,3)/0.010),'b')
-stairs (mv(:,3),'r')
-xlim ([0 2000])
-
-figure(44)
-hold on
-grid on
-stairs (diff(mp(:,4)/0.010),'b')
-stairs (mv(:,4),'r')
-xlim ([0 2000])
+stairs (diff(mp(:,j)/0.010),'b')
+stairs (mv(:,j),'r')
+%xlim ([0 2000])
+end 
