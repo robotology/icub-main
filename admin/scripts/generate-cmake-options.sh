4d2b7d25f96ac7005558679cb0aed1f56e27ada1
Matteo Brunettini <matteo.brunettini@iit.it>
2015-06-30 14:40:17 +0200

    Update generate-cmake-options.sh

--- admin/scripts/generate-cmake-options.sh
+++ admin/scripts/generate-cmake-options.sh
@@ -1,28 +1,27 @@
 ### parameters are 
 # $1: hostname (could be also buildtype)
 # $2: os type (macos, winxp, lenny, etch, karmic ...)
+# $3: test type: nightly continuous experimental
 
-case $2 in
-# no special condition for macos any more
-#    macos)
-#    CMAKE_OPTIONS="-DCREATE_GUIS:BOOL=FALSE -DCREATE_LIB_MATH:BOOL=TRUE -DENABLE_DASHBOARD_SUBMIT:BOOL=TRUE -DUSE_PORT_AUTHENTICATION:BOOL=TRUE"
-#    ;;
-    *)
-    CMAKE_OPTIONS="-DENABLE_icubmod_cartesiancontrollerclient:BOOL=TRUE \
-                        -DENABLE_icubmod_cartesiancontrollerserver:BOOL=TRUE \
-                        -DENABLE_icubmod_gazecontrollerclient:BOOL=TRUE \
-                        -DENABLE_icubmod_icubarmcalibrator:BOOL=TRUE \
-                        -DENABLE_icubmod_icubarmcalibratorj8:BOOL=TRUE \
-                        -DENABLE_icubmod_icubhandcalibrator:BOOL=TRUE \
-                        -DENABLE_icubmod_icublegscalibrator:BOOL=TRUE \
-                        -DENABLE_icubmod_logpolarclient:BOOL=TRUE \
-                        -DENABLE_icubmod_logpolargrabber:BOOL=TRUE \
-                        -DENABLE_icubmod_skinprototype:BOOL=TRUE \
-                        -DENABLE_icubmod_fakecan:BOOL=TRUE \
-                        -DENABLE_icubmod_canmotioncontrol:BOOL=FALSE\
-                        -DENABLE_icubmod_debugInterfaceClient:BOOL=TRUE\
-                        -DICUB_DASHBOARD_SUBMIT:BOOL=TRUE"
-esac
+
+CMAKE_OPTIONS="\
+   -DTEST_MACHINE_HOSTNAME:STRING=$1 \
+   -DTEST_MACHINE_OS_TYPE:STRING=$2 \
+   -DTEST_MACHINE_TEST_TYPE:STRING=$3 \
+   -DENABLE_icubmod_cartesiancontrollerclient:BOOL=TRUE \
+   -DENABLE_icubmod_cartesiancontrollerserver:BOOL=TRUE \
+   -DENABLE_icubmod_gazecontrollerclient:BOOL=TRUE \
+   -DENABLE_icubmod_icubarmcalibrator:BOOL=TRUE \
+   -DENABLE_icubmod_icubarmcalibratorj8:BOOL=TRUE \
+   -DENABLE_icubmod_icubhandcalibrator:BOOL=TRUE \
+   -DENABLE_icubmod_icublegscalibrator:BOOL=TRUE \
+   -DENABLE_icubmod_logpolarclient:BOOL=TRUE \
+   -DENABLE_icubmod_logpolargrabber:BOOL=TRUE \
+   -DENABLE_icubmod_skinprototype:BOOL=TRUE \
+   -DENABLE_icubmod_fakecan:BOOL=TRUE \
+   -DENABLE_icubmod_canmotioncontrol:BOOL=FALSE\
+   -DENABLE_icubmod_debugInterfaceClient:BOOL=TRUE\
+   -DICUB_DASHBOARD_SUBMIT:BOOL=TRUE"
 
 case $3 in
     experimental)
