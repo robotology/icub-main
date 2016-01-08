f9d541cd2e48eeafa20f6a0d76dcfeba42429df9
Ugo Pattacini <ugo.pattacini@iit.it>
2016-01-08 16:34:18 +0100

    fingersTuner and depth2kin now rely on yarp_add_idl macro removed iCubIDLClients library requires yarp 2.3.64.10

--- src/idl_generated_code/depth2kin_thrift.cmake
+++
@@ -1,5 +0,0 @@
-## This is an automatically-generated file.
-## It could get re-generated if the ALLOW_IDL_GENERATION flag is on
-
-set(headers include/PointReq.h;include/depth2kin_IDL.h)
-set(sources src/PointReq.cpp;src/depth2kin_IDL.cpp)
