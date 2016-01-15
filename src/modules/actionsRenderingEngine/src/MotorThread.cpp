7539a2b2cafada26d66450be6b65e09a0fd207f8
Ugo Pattacini <ugo.pattacini@iit.it>
2016-01-15 17:51:10 +0100

    [ARE]: more suitable pointing heuristics ;)

--- src/modules/actionsRenderingEngine/src/MotorThread.cpp
+++ src/modules/actionsRenderingEngine/src/MotorThread.cpp
@@ -1809,8 +1809,8 @@ bool MotorThread::point(Bottle &options)
     }
 
     arm=checkArm(arm,xd);
-    xd[0]+=0.10;
-    xd[1]+=(arm==LEFT)?-0.05:0.05;
+    xd[0]+=0.06;
+    xd[1]+=(arm==LEFT)?-0.07:0.07;
     xd[2]+=0.08;
 
     Matrix R=zeros(3,3);
