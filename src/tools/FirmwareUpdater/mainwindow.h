4e5a5f8cdf0742864bd686fe1f1c5fd1c58b2d5e
aitek4iit <aitek4iit@aitek.it>
2016-11-22 16:18:53 +0100

    fix for calibration window

--- src/tools/FirmwareUpdater/mainwindow.h
+++ src/tools/FirmwareUpdater/mainwindow.h
@@ -10,86 +10,14 @@
 #include <QLabel>
 #include <QPair>
 #include <QFutureWatcher>
-
+#include "calibrationwindow.h"
+#include "customtreewidgetitem.h"
 namespace Ui {
 class MainWindow;
 }
 
-#define ETH_TREE_NODE   QTreeWidgetItem::UserType + 1
-#define CAN_TREE_NODE   QTreeWidgetItem::UserType + 2
-
-#define ETH_TREE_ROOT_NODE   QTreeWidgetItem::UserType + 3
-#define CAN_TREE_ROOT_NODE   QTreeWidgetItem::UserType + 4
 
 
-class CustomTreeWidgetItem : public QObject, public QTreeWidgetItem
-{
-    Q_OBJECT
-
-public:
-    CustomTreeWidgetItem(QTreeWidgetItem*parent, QStringList fields, int indexOfBoard, FirmwareUpdaterCore *core,int type);
-    
-    int getIndexOfBoard();
-    void setCheckEnabled(bool enable);
-    bool checkIsEnabled();
-    void setCheckSelected(bool selected);
-    bool isCheckSelected();
-    QTreeWidgetItem *getParentNode();
-    void setCanBoards(QList <sBoard> boards);
-    virtual void refresh();
-    QString getBoardType();
-
-
-    QString retrieveCanBoards();
-    EthBoard getBoard();
-    QList <sBoard> getCanBoards();
-    sBoard getCanBoard(int index);
-
-    
-protected:
-    int m_indexOfBoard;
-    FirmwareUpdaterCore *core;
-    SelectionCheckBox *check;
-    QTreeWidgetItem *parentNode;
-    QList <sBoard> canBoards;
-
-signals:
-    void selectedChanged(bool);
-    void needLoading(bool,bool);
-    void selectionCheckDestroy(QObject*);
-
-private slots:
-    void onSelectedChanged(bool);
-    
-};
-
-
-class EthTreeWidgetItem : public CustomTreeWidgetItem
-{
-    Q_OBJECT
-
-public:
-    EthTreeWidgetItem(QTreeWidgetItem*parent, FirmwareUpdaterCore *core, int indexOfBoard);
-    void setSelectedBoard(bool, int);
-    void refresh();
-
-
-
-
-
-};
-
-class CanTreeWidgetItem : public CustomTreeWidgetItem
-{
-    Q_OBJECT
-
-public:
-    CanTreeWidgetItem(QTreeWidgetItem*,FirmwareUpdaterCore *core,int indexOfBoard);
-    
-    sBoard getBoard();
-    void refresh();
-
-};
 
 class MainWindow : public QMainWindow
 {
@@ -121,8 +49,10 @@ private:
      void uploadLoader(QString);
      void uploadUpdater(QString);
 
+
 private:
     Ui::MainWindow *ui;
+    CalibrationWindow *calibDlg;
     FirmwareUpdaterCore *core;
     QProgressBar *progress;
     QLabel *infoResult;
@@ -144,6 +74,7 @@ signals:
     void needSetRestartOnSelected();
     void refreshCanBoardsFromEth(QTreeWidgetItem*);
     void needLoading(bool,bool,QString = "",bool = true);
+
 
 
 private slots:
@@ -174,6 +105,7 @@ private slots:
     void onSelectionCheckDestroy(QObject*);
     void onChangeAddress(bool);
     void onRestartBoards(bool click);
+
     //void onRestartBoards5Secs(bool click);
     void onSetInfoRes(QString);
 };
