979d1c669b734bc40e74add167f39f9b2e4e7f29
Marco Randazzo <marco.randazzo@iit.it>
2017-08-09 16:55:17 +0200

    Added check on port.open() open result

--- src/tools/joystickCtrl/main.cpp
+++ src/tools/joystickCtrl/main.cpp
@@ -305,7 +305,7 @@ public:
             }
             else
             {
-                fprintf ( stderr, "Unknown [OUTPUT] property\n");
+                yError() << "Unknown [OUTPUT] property";
                 return false;
             }
         }
@@ -322,11 +322,17 @@ public:
             output_port_name = "/joystickCtrl:o";
             yWarning ( "outputPortName not found, using %s \n", output_port_name.c_str());
         }
-        port_command.open(output_port_name.c_str());
+        bool ret=true;
+        ret &= port_command.open(output_port_name.c_str());
         //@@@ TO BE COMPLETED: port name prefix to be set in the ini file
-        port_axis_only.open("/joystickCtrl/raw_axis:o");
-        port_buttons_only.open("/joystickCtrl/raw_buttons:o");
-        
+        ret &= port_axis_only.open("/joystickCtrl/raw_axis:o");
+        ret &= port_buttons_only.open("/joystickCtrl/raw_buttons:o");
+        if (ret==false)
+        {
+            yError() << "Unable to open module ports";
+            return false;
+        }
+
         //get the list of the commands to be executed with the buttons
         Bottle& exec_comm_bottle = rf.findGroup("BUTTONS_EXECUTE");
         int joystick_actions_count = 0;
