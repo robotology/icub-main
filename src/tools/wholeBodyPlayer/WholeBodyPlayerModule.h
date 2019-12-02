eebd5098571f88f50c1eaa31e8bda2c4eff4bba5
Nicolo <nicolo.genesio@iit.it>
2019-12-02 17:45:48 +0100

    wholeBodyPlayer: add control of yarpdataplayer through rpc ports.
    Once the security guard is violated, the yarpdataplayer is paused, the current
    step is move in position control for safety reason, and then a play rpc command
    is send again to yarpdataplayer.

--- src/tools/wholeBodyPlayer/WholeBodyPlayerModule.h
+++ src/tools/wholeBodyPlayer/WholeBodyPlayerModule.h
@@ -46,6 +46,7 @@
 #include <yarp/os/LogStream.h>
 #include <yarp/os/RFModule.h>
 #include <yarp/dev/IPositionDirect.h>
+#include <yarp/dev/IPositionControl.h>
 #include <yarp/dev/IControlMode.h>
 #include <yarp/dev/IEncoders.h>
 #include <yarp/dev/PolyDriver.h>
@@ -54,57 +55,127 @@
 #include <vector>
 #include <cmath>
 #include <memory>
+#include <mutex>
+#include <yarp/os/RpcClient.h>
 
-constexpr double tolerance = 200.0; //degrees
+constexpr double tolerance = 5.0; //degrees
+
+enum class state : std::uint8_t {
+    ok = 0,
+    error,
+    fatal_error
+};
 
 class ReplayPort : public  yarp::os::BufferedPort<yarp::os::Bottle>
 {
 public:
-    ReplayPort(std::string partName, yarp::dev::IPositionDirect* iPosDir, yarp::dev::IEncoders* iEnc) :
+    ReplayPort(std::string partName, yarp::dev::IPositionDirect* iPosDir, yarp::dev::IEncoders* iEnc,
+               yarp::dev::IControlMode* iCM, yarp::dev::IPositionControl* iPosControl) :
                                                                             m_partName(std::move(partName)),
                                                                             m_posDir(iPosDir),
-                                                                            m_enc(iEnc)
+                                                                            m_enc(iEnc),
+                                                                            m_CM(iCM),
+                                                                            m_posControl(iPosControl)
+
     {
-        if (m_posDir && m_enc) {
+        if (m_posDir && m_enc && m_CM) {
+            // Get num of axes
             m_posDir->getAxes(&m_numAxes);
+            // Set in position direct
+            std::vector<int> cms (m_numAxes, VOCAB_CM_POSITION_DIRECT);
+            m_CM->setControlModes(cms.data());
+            // Allocate vectors
             m_currState.resize(m_numAxes);
             m_nextState.resize(m_numAxes);
         }
         yarp::os::BufferedPort<yarp::os::Bottle>::useCallback();
+        yarp::os::BufferedPort<yarp::os::Bottle>::setStrict(true);
     }
 
     using yarp::os::TypedReaderCallback<yarp::os::Bottle>::onRead;
     void onRead(yarp::os::Bottle& datum) override
     {
-        if (!interrupted && m_posDir && !datum.isNull()) {
+        if (m_state==state::ok && m_posDir && !datum.isNull()) {
 
             bool ok = m_enc->getEncoders(m_currState.data());
+            m_mutex.lock();
             for (size_t i=0; i<datum.size(); i++) {
                 m_nextState[i] = datum.get(i).asDouble();
                 auto delta = std::fabs(m_nextState[i] - m_currState[i]);
                 ok &= delta < tolerance; // TODO improve the check calculating the distance between the 2 vector !!!
                 if (!ok) {
-                    yError()<<"ReplayPort: joint"<<i<<"of"<<m_partName<<"is too far to the target position";
-                    yError()<<"Desired: "<<datum.get(i).asDouble()<<"current: "<<m_currState[i]<<delta<<"the port"
-                    <<this->getName()<<"will be closed for safety reason";
-                    interrupted =true;
-                    return;
+                    yWarning()<<"ReplayPort: joint"<<i<<"of"<<m_partName<<"is too far to the target position";
+                    yWarning()<<"Desired: "<<datum.get(i).asDouble()<<"current: "<<m_currState[i]<<delta<<"Trying to reach "
+                                                                                                        "it in Position Control, "
+                                                                                                        "the playback will be paused";
+                    m_state = state::error;
+                    ok = true;
 
                 }
             }
-
-            m_posDir->setPositions(m_nextState.data());
+            m_mutex.unlock();
+            if (m_state == state::ok) {
+                m_posDir->setPositions(m_nextState.data());
+            }
 //            yDebug()<<"ReplayPort: part"<<m_partName;
 //            yDebug()<<"Desired: "<<m_nextState<<"current:"<<m_currState;
         }
     }
 
-    std::atomic_bool interrupted{false};
+    bool positionMoveFallback(){
+        bool ok = true;
+        std::vector<int> cms (m_numAxes, VOCAB_CM_POSITION);
+        ok &= m_CM->setControlModes(cms.data());
+
+        if (!ok) {
+            m_state = state::fatal_error;
+            return ok;
+        }
+
+        m_mutex.lock();
+        ok &= m_posControl->positionMove(m_nextState.data());
+        m_mutex.unlock();
+
+        if (!ok) {
+            m_state = state::fatal_error;
+            return ok;
+        }
+        bool done{false};
+        ok &= m_posControl->checkMotionDone(&done);
+
+        if (!ok) {
+            m_state = state::fatal_error;
+            return ok;
+        }
+
+        while (!done) {
+            ok &= m_posControl->checkMotionDone(&done);
+            if (!ok) {
+                m_state = state::fatal_error;
+                return ok;
+            }
+            yarp::os::Time::delay(0.01);
+        }
+
+        std::vector<int> cmDir (m_numAxes, VOCAB_CM_POSITION_DIRECT);
+        ok &= m_CM->setControlModes(cmDir.data());
+        if (!ok) {
+            m_state = state::fatal_error;
+            return ok;
+        }
+        return ok;
+    }
+
+    std::atomic<state> m_state{state::ok};
 private:
     double now{0.0};
     std::string m_partName{};
     yarp::dev::IPositionDirect* m_posDir{nullptr};
     yarp::dev::IEncoders* m_enc{nullptr};
+    yarp::dev::IPositionControl* m_posControl{nullptr};
+    yarp::dev::IControlMode* m_CM{nullptr};
+    std::mutex m_mutex;
+
 
     std::vector<double> m_currState, m_nextState;
     int m_numAxes{0};
@@ -120,8 +191,9 @@ struct Replayer {
                                  {"local",  yarp::os::Value("/"+moduleName+"/"+part+"/remoteControlBoard")}};
 
         yarp::dev::IControlMode* iCM{nullptr};
-        yarp::dev::IPositionDirect* m_iPosDir{nullptr};
-        yarp::dev::IEncoders* m_enc{nullptr};
+        yarp::dev::IPositionDirect* iPosDir{nullptr};
+        yarp::dev::IPositionControl* iPosControl{nullptr};
+        yarp::dev::IEncoders* iEnc{nullptr};
 
         m_remoteControlBoard = std::make_unique<yarp::dev::PolyDriver>();
 
@@ -130,20 +202,15 @@ struct Replayer {
             yError()<<"Replayer: failed to open the remote control board for part"<<part;
             return false;
         }
-        ok &= m_remoteControlBoard->view(m_iPosDir);
-        ok &= m_remoteControlBoard->view(m_enc);
+        ok &= m_remoteControlBoard->view(iPosDir);
+        ok &= m_remoteControlBoard->view(iEnc);
         ok &= m_remoteControlBoard->view(iCM);
+        ok &= m_remoteControlBoard->view(iPosControl);
 
         if (ok)
         {
-            int nJoints;
-            ok &= m_iPosDir->getAxes(&nJoints);
-            std::vector<int> cms (nJoints, VOCAB_CM_POSITION_DIRECT);
-            ok &= iCM->setControlModes(cms.data());
-            if (ok) {
-                m_replayPort = std::make_unique<ReplayPort>(part, m_iPosDir, m_enc);
-                ok &= m_replayPort->open("/"+moduleName+"/"+part+"/state:i");
-            }
+            m_replayPort = std::make_unique<ReplayPort>(part, iPosDir, iEnc, iCM, iPosControl);
+            ok &= m_replayPort->open("/"+moduleName+"/"+part+"/state:i");
         }
         return ok;
     }
@@ -173,5 +240,8 @@ public:
 
 private:
     std::vector<Replayer> m_replayerVec;
+    yarp::os::RpcClient   m_rpcPort;
+    yarp::os::Bottle reqPause{"pause"}, reqPlay{"play"}, response;
+
 
 };
