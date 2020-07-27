db26009cd4e975bc229aaea864146801e7a29069
Nicolò Genesio (二コゲネ) <nicolo.genesio@iit.it>
2020-07-27 16:06:42 +0200

    Update iCubFindDependencies.cmake
    Fix icub_firmware_shared error message

--- conf/iCubFindDependencies.cmake
+++ conf/iCubFindDependencies.cmake
@@ -35,7 +35,7 @@ find_package(icub_firmware_shared QUIET)
 if(icub_firmware_shared_FOUND)
   # icub-firmware-shared 4.0.7 was actually a wrong version exported by icub-firmware-shared <= 1.15
   if(icub_firmware_shared_VERSION VERSION_GREATER 4 OR icub_firmware_shared_VERSION VERSION_LESS 1.16.100)
-    message(FATAL_ERROR "An old version of icub-firmware-shared has been detected, but at least 1.16.0 is required")
+    message(FATAL_ERROR "An old version of icub-firmware-shared has been detected, but at least 1.16.100 is required")
   endif()
 endif()
 
