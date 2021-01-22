a5b8e752001320e35b1fea76db6e760a9f6d9ebd
Marco Accame <marco.accame@iit.it>
2021-01-22 14:20:13 +0100

    Movement of the fingers of the new hand in open/close mode (#714)

--- src/libraries/icubmod/embObjMotionControl/embObjMotionControl.cpp
+++ src/libraries/icubmod/embObjMotionControl/embObjMotionControl.cpp
@@ -16,7 +16,7 @@
 #include "embObjMotionControl.h"
 #include <ethManager.h>
 #include <FeatureInterface.h>
-
+#include <yarp/conf/environment.h>
 
 #include <yarp/os/LogStream.h>
 
@@ -253,7 +253,7 @@ embObjMotionControl::embObjMotionControl() :
     behFlags.useRawEncoderData = false;
     behFlags.pwmIsLimited     = false;
 
-    std::string tmp = NetworkBase::getEnvironment("ETH_VERBOSEWHENOK");
+    std::string tmp = yarp::conf::environment::getEnvironment("ETH_VERBOSEWHENOK");
     if (tmp != "")
     {
         behFlags.verbosewhenok = (bool)NetType::toInt(tmp);
@@ -603,7 +603,12 @@ bool embObjMotionControl::saveCouplingsData(void)
             jc_dest = &(serviceConfig.ethservice.configuration.data.mc.mc2pluspsc.jomocoupling);
 
         } break;
-         case eomn_serv_MC_mc4:
+        case eomn_serv_MC_mc4plusfaps:
+        {
+            jc_dest = &(serviceConfig.ethservice.configuration.data.mc.mc4plusfaps.jomocoupling);
+
+        } break;
+        case eomn_serv_MC_mc4:
         {
             return true;
         } break;
@@ -4726,7 +4731,8 @@ bool embObjMotionControl::iNeedCouplingsInfo(void)
     if( (mc_serv_type == eomn_serv_MC_foc) ||
         (mc_serv_type == eomn_serv_MC_mc4plus) ||
         (mc_serv_type == eomn_serv_MC_mc4plusmais) ||
-        (mc_serv_type == eomn_serv_MC_mc2pluspsc)
+        (mc_serv_type == eomn_serv_MC_mc2pluspsc) ||
+        (mc_serv_type == eomn_serv_MC_mc4plusfaps)
       )
         return true;
     else
