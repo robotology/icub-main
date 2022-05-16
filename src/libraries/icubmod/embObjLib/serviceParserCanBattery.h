4e9a547067758e595e204ad3b585a7de686972bd
triccyx <triccyx@gmail.com>
2022-05-16 15:25:14 +0200

    first step

---
+++ src/libraries/icubmod/embObjLib/serviceParserCanBattery.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2022 Istituto Italiano di Tecnologia (IIT)
+ * All rights reserved.
+ * Author: Luca Tricerri
+ * This software may be modified and distributed under the terms of the
+ * BSD-3-Clause license. See the accompanying LICENSE file for details.
+ */
+
+#ifndef _ServiceParserCanBattery_H_
+#define _ServiceParserCanBattery_H_
+
+#include <yarp/os/Bottle.h>
+
+#include <map>
+#include <string>
+
+#include "EoAnalogSensors.h"
+#include "EoBoards.h"
+#include "EoManagement.h"
+#include "EoMotionControl.h"
+#include "ftInfo.h"
+
+using namespace yarp::os;
+
+class BatteryInfo
+{
+   public:
+	uint8_t acquisitionRate{0};
+	eObrd_type_t board{eobrd_unknown};
+	int port{0};
+	int address{0};
+	int majorProtocol{0};
+	int minorProtocol{0};
+	int majorFirmware{0};
+	int minorFirmware{0};
+	int buildFirmware{0};
+
+	bool toEomn(eOas_battery_sensordescriptor_t& out) const;
+};
+
+class ServiceParserCanBattery
+{
+   public:
+	ServiceParserCanBattery();
+	bool parse(const yarp::os::Searchable& config);
+	bool toEomn(eOmn_serv_config_data_as_canbattery_t& out) const;
+	BatteryInfo &getBatteryInfo()
+
+   protected:
+	virtual bool checkPropertyCanBoards(const Bottle& bPropertiesCanBoards);  // OK
+	virtual bool checkPropertySensors(const Bottle& property);				  // OK
+	virtual bool checkSettings(const Bottle& settings);						  // OK
+	virtual bool checkServiceType(const Bottle& service);					  // OK
+	virtual eObrd_type_t checkBoardType(const std::string& boardType);
+
+	BatteryInfo batteryInfo_;
+};
+
+#endif
