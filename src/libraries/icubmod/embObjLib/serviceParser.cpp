ec5396a21c5ea51348a67d2d7e67db0437388621
Mattia Fussi <38140169+mfussi66@users.noreply.github.com>
2022-12-05 15:24:04 +0100

    Fix boolean expression when checking type of strain2 board (#846)

--- src/libraries/icubmod/embObjLib/serviceParser.cpp
+++ src/libraries/icubmod/embObjLib/serviceParser.cpp
@@ -1528,7 +1528,7 @@ bool ServiceParser::parseService(Searchable &config, servConfigFTsensor_t &ftcon
     servAnalogSensor_t thestrain_sensor = as_service.settings.enabledsensors.at(0);
     
     // first check we do is about thestrain_props.type
-    if(eobrd_cantype_strain2 != thestrain_props.type || eobrd_cantype_strain2c != thestrain_props.type)
+    if((eobrd_cantype_strain2 != thestrain_props.type) && (eobrd_cantype_strain2c != thestrain_props.type))
     {
         yError() << "ServiceParser::parseService() for embObjFTsensor has detected an invalid type of board. it should be a eobrd_strain2 or a eobrd_strain2c but is a:" << eoboards_type2string2(eoboards_cantype2type(thestrain_props.type), eobool_false);
         return false;
