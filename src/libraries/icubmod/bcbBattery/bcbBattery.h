fa805721f0aa89e8fa9101eda904f75f2f46cdc3
Ugo Pattacini <ugo.pattacini@iit.it>
2023-12-18 14:39:40 +0100

    reinstated the previous fix for BATTERY_OK_STANDBY

--- src/libraries/icubmod/bcbBattery/bcbBattery.h
+++ src/libraries/icubmod/bcbBattery/bcbBattery.h
@@ -45,11 +45,7 @@ class batteryReaderThread : public PeriodicThread
     double             battery_current = 0;
     std::string        battery_info = "icub battery system v1.0";
     int                backpack_status = 0;
-#if YARP_VERSION_MAJOR == 3 && YARP_VERSION_MINOR <= 8
-    IBattery::Battery_status     battery_status = IBattery::Battery_status::BATTERY_OK_STANBY; 
-#else
     IBattery::Battery_status     battery_status = IBattery::Battery_status::BATTERY_OK_STANDBY;    
-#endif
 
     batteryReaderThread (ISerialDevice *_iSerial, double period) :
     PeriodicThread((double)period),
