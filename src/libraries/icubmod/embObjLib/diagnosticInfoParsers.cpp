613b4e8a246c5cb06efbc78776576bcb2660e68b
SanLordKevin <kevin.sangalli@iit.it>
2025-07-07 11:27:45 +0200

    wip on message parsing4

--- src/libraries/icubmod/embObjLib/diagnosticInfoParsers.cpp
+++ src/libraries/icubmod/embObjLib/diagnosticInfoParsers.cpp
@@ -531,7 +531,7 @@ void ConfigParser::parseInfo()
             int boardtype =   (m_dnginfo.param64 & 0x0000ff0000000000) >> 40;
 
             eObrd_type_t  general_brd_type = eoboards_cantype2type((eObrd_cantype_t )boardtype);
-                                       
+            std::string board_type_str = eoboards_type2string(general_brd_type);                           
             uint64_t val = invalidmask & 0x0f;
             if(0 != val)
             {
