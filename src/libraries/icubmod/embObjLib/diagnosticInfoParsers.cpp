de3ffe48194e3f41734a1e2667034217b1076b3e
SanLordKevin <kevin.sangalli@iit.it>
2025-07-07 15:49:09 +0200

    c

--- src/libraries/icubmod/embObjLib/diagnosticInfoParsers.cpp
+++ src/libraries/icubmod/embObjLib/diagnosticInfoParsers.cpp
@@ -535,7 +535,7 @@ void ConfigParser::parseInfo()
             location.bus = BUS;
             location.adr = address;
             std::string location_str(64, '\0');
-            ServiceParser::convert(location, &location_str,location_str.size());
+            ServiceParser::convert(location, &location_str[0],location_str.size());
             eObrd_type_t  general_brd_type = eoboards_cantype2type((eObrd_cantype_t )boardtype);
             std::string board_type_str = eoboards_type2string(general_brd_type);                           
             uint64_t val = invalidmask & 0x0f;
