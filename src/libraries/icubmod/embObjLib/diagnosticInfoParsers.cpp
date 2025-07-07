c41bdfac16cdd5f29e760d93deab4e8cc3e7655c
SanLordKevin <kevin.sangalli@iit.it>
2025-07-07 15:16:31 +0200

    add bus parsing

--- src/libraries/icubmod/embObjLib/diagnosticInfoParsers.cpp
+++ src/libraries/icubmod/embObjLib/diagnosticInfoParsers.cpp
@@ -528,6 +528,8 @@ void ConfigParser::parseInfo()
             int proto_minor = (m_dnginfo.param64 & 0x00000000ff000000) >> 24;
             int proto_major = (m_dnginfo.param64 & 0x000000ff00000000) >> 32;
             int boardtype =   (m_dnginfo.param64 & 0x0000ff0000000000) >> 40;
+            int address =     (m_dnginfo.param64 & 0x00ff000000000000) >> 48;
+            int BUS =         (m_dnginfo.param64 & 0xff00000000000000) >> 56;
 
             eObrd_type_t  general_brd_type = eoboards_cantype2type((eObrd_cantype_t )boardtype);
             std::string board_type_str = eoboards_type2string(general_brd_type);                           
@@ -544,25 +546,25 @@ void ConfigParser::parseInfo()
                 else
                 {
                     snprintf(str, sizeof(str), " error on ICCdiscovery because it has: %s %s %s %s ",
-                                                ((val & 0x1) == 0x1) ? (wrongtype) : (empty),
-                                                ((val & 0x2) == 0x2) ? (wrongappl) : (empty),
-                                                ((val & 0x4) == 0x4) ? (wrongprot) : (empty),
+                                                ((val & 0x1) == 0x1) ? (wrongtype)   : (empty),
+                                                ((val & 0x2) == 0x2) ? (wrongappl)   : (empty),
+                                                ((val & 0x4) == 0x4) ? (wrongprot)   : (empty),
                                                 ((val & 0x8) == 0x8) ? (wronchannel) : (empty)
                     );
                     m_dnginfo.baseInfo.finalMessage.append(str);
 
-                    snprintf(str, sizeof(str), "Found on other core: %s Fw ver is %d.%d.%d. Proto ver is %d.%d \n", 
+                    snprintf(str, sizeof(str), "Found on other core: %s FW ver is %d.%d.%d. Protocol ver is %d.%d  BUS:%d address:%d. \n", 
                                             board_type_str.c_str(),
-                                            fw_major, fw_minor, fw_build, proto_major, proto_minor 
+                                            fw_major, fw_minor, fw_build, proto_major, proto_minor, BUS, address
                     );
                     m_dnginfo.baseInfo.finalMessage.append(str);
                 }
             }
             else
             {
-                snprintf(str, sizeof(str), "%s ICCdiscovery successful. Found on other core: %s Fw ver is %d.%d.%d. Proto ver is %d.%d \n", 
+                snprintf(str, sizeof(str), "%s ICCdiscovery successful. Found on other core: %s FW ver is %d.%d.%d. Protocol ver is %d.%d BUS:%d address:%d. \n", 
                                         m_dnginfo.baseMessage.c_str(), board_type_str.c_str(),
-                                        fw_major, fw_minor, fw_build, proto_major, proto_minor  );
+                                        fw_major, fw_minor, fw_build, proto_major, proto_minor, BUS, address  );
 
                 m_dnginfo.baseInfo.finalMessage.append(str);
             }
