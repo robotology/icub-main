ba52de2a23fff77e71108fb6f45ab8822f9510f4
Kevin <kevin.sangalli@iit.it>
2025-08-12 14:27:33 +0200

    FU board parsing

--- src/tools/FirmwareUpdater/firmwareupdatercore.cpp
+++ src/tools/FirmwareUpdater/firmwareupdatercore.cpp
@@ -328,15 +328,15 @@ QString FirmwareUpdaterCore::getProcessFromUint(uint8_t id, bool isMultiCore, eO
     case uprot_proc_Updater:
         return "eUpdater";
     case uprot_proc_Application00:
-        if (isMultiCore)
+        if (eoboards_is_MC_board(eoboards_ethtype2type(boardtype)) && isMultiCore)
             return "app.yri" ;
         else
             return "eApplication";
     case uprot_proc_Application01:
-        if (isMultiCore)
+        if (eoboards_is_MC_board(eoboards_ethtype2type(boardtype)) && isMultiCore)
             return "app.mot" ;
         else
-            return "error";
+            return "eApplication_core_1";
     case uprot_proc_ApplPROGupdater:
         return "eApplPROGupdater";
     default:
